7cb1675538f390a26828a18bbc30678705b54098
Dino Music <dino.music@htecgroup.com>
2022-12-04 14:11:31 +0100

    EXSWHTEC-193 - Implement positive and negative tests for hipGraphMemsetNodeSetParams

--- catch/unit/graph/hipGraphAddMemsetNode.cc
+++ catch/unit/graph/hipGraphAddMemsetNode.cc
@@ -1,19 +1,22 @@
 /*
 Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANNTY OF ANY KIND, EXPRESS OR
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER INN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR INN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
@@ -24,41 +27,30 @@ THE SOFTWARE.
 #include <resource_guards.hh>
 #include <utils.hh>
 
+#include "graph_memset_node_test_common.hh"
 #include "graph_tests_common.hh"
 
 TEMPLATE_TEST_CASE("Unit_hipGraphAddMemsetNode_Positive_Basic", "", uint8_t, uint16_t, uint32_t) {
-  const size_t width = GENERATE(1, 64, kPageSize / sizeof(TestType) + 1);
-  const size_t height = GENERATE(1, 2, 1024);
+  const auto f = [](hipMemsetParams* params) {
+    hipGraph_t graph = nullptr;
+    HIP_CHECK(hipGraphCreate(&graph, 0));
 
-  LinearAllocGuard2D<TestType> alloc(width, height);
+    hipGraphNode_t node = nullptr;
+    HIP_CHECK(hipGraphAddMemsetNode(&node, graph, nullptr, 0, params));
 
-  hipGraph_t graph = nullptr;
-  HIP_CHECK(hipGraphCreate(&graph, 0));
+    hipGraphExec_t graph_exec = nullptr;
+    HIP_CHECK(hipGraphInstantiate(&graph_exec, graph, nullptr, nullptr, 0));
 
-  hipGraphNode_t node = nullptr;
-  constexpr TestType set_value = 42;
-  hipMemsetParams params = {};
-  params.dst = alloc.ptr();
-  params.elementSize = sizeof(TestType);
-  params.width = width;
-  params.height = height;
-  params.pitch = alloc.pitch();
-  params.value = set_value;
-  HIP_CHECK(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params));
+    HIP_CHECK(hipGraphLaunch(graph_exec, hipStreamPerThread));
+    HIP_CHECK(hipStreamSynchronize(hipStreamPerThread));
 
-  hipGraphExec_t graph_exec = nullptr;
-  HIP_CHECK(hipGraphInstantiate(&graph_exec, graph, nullptr, nullptr, 0));
+    HIP_CHECK(hipGraphExecDestroy(graph_exec));
+    HIP_CHECK(hipGraphDestroy(graph));
 
-  HIP_CHECK(hipGraphLaunch(graph_exec, hipStreamPerThread));
-  HIP_CHECK(hipStreamSynchronize(hipStreamPerThread));
+    return hipSuccess;
+  };
 
-  HIP_CHECK(hipGraphExecDestroy(graph_exec));
-  HIP_CHECK(hipGraphDestroy(graph));
-
-  LinearAllocGuard<TestType> buffer(LinearAllocs::hipHostMalloc, width * sizeof(TestType) * height);
-  HIP_CHECK(hipMemcpy2D(buffer.ptr(), width * sizeof(TestType), alloc.ptr(), alloc.pitch(),
-                        width * sizeof(TestType), height, hipMemcpyDeviceToHost));
-  ArrayFindIfNot(buffer.ptr(), set_value, width * height);
+  GraphMemsetNodeCommonPositive<TestType>(f);
 }
 
 TEST_CASE("Unit_hipGraphAddMemsetNode_Negative_Parameters") {
@@ -77,49 +69,7 @@ TEST_CASE("Unit_hipGraphAddMemsetNode_Negative_Parameters") {
   GraphAddNodeCommonNegativeTests(std::bind(hipGraphAddMemsetNode, _1, _2, _3, _4, &params), graph);
 
   hipGraphNode_t node = nullptr;
-
-  SECTION("pMemsetParams == nullptr") {
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, nullptr), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.dst == nullptr") {
-    params.dst = nullptr;
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.elementSize != 1, 2, 4") {
-    params.elementSize = GENERATE(0, 3, 5);
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.width == 0") {
-    params.width = 0;
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.width > allocation size") {
-    params.width = 5;
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.height == 0") {
-    params.height = 0;
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.pitch < width when height > 1") {
-    params.width = 2;
-    params.height = 2;
-    params.pitch = 1 * params.elementSize;
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
-
-  SECTION("pMemsetParams.pitch * height > allocation size") {
-    params.width = 2;
-    params.height = 2;
-    params.pitch = 3 * params.elementSize;
-    HIP_CHECK_ERROR(hipGraphAddMemsetNode(&node, graph, nullptr, 0, &params), hipErrorInvalidValue);
-  }
+  MemsetCommonNegative(std::bind(hipGraphAddMemsetNode, &node, graph, nullptr, 0, _1), params);
 
   HIP_CHECK(hipGraphDestroy(graph));
 }

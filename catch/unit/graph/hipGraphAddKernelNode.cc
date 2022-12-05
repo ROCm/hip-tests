a9fad2d20fc231dd81bb57e13674f87e92f871f0
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2022-12-05 09:23:14 +0100

    EXSWHTEC-169 - Implement additional tests for Kernel Graph Node APIs - Tidy up hipGraphAddKernelNode tests - Tidy up hipGraphKernelNodeGetParams tests - Tidy up hipGraphKernelNodeSetParams tests - Tidy up hipGraphExecKernelNodeSetParams tests.

--- catch/unit/graph/hipGraphAddKernelNode.cc
+++ catch/unit/graph/hipGraphAddKernelNode.cc
@@ -1,13 +1,16 @@
 /*
 Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -22,7 +25,6 @@ THE SOFTWARE.
 
 /* Test verifies hipGraphAddKernelNode API Negative scenarios.
  */
-
 TEST_CASE("Unit_hipGraphAddKernelNode_Negative") {
   constexpr int N = 1024;
   size_t NElem{N};
@@ -31,7 +33,6 @@ TEST_CASE("Unit_hipGraphAddKernelNode_Negative") {
   unsigned blocks = HipTest::setNumBlocks(blocksPerCU, threadsPerBlock, N);
   int *A_d, *B_d, *C_d;
   hipGraph_t graph;
-  hipError_t ret;
   hipGraphNode_t kNode;
   hipKernelNodeParams kNodeParams{};
   std::vector<hipGraphNode_t> dependencies;
@@ -41,61 +42,65 @@ TEST_CASE("Unit_hipGraphAddKernelNode_Negative") {
   HIP_CHECK(hipMalloc(&C_d, sizeof(int) * N));
   HIP_CHECK(hipGraphCreate(&graph, 0));
 
-  void* kernelArgs[] = {&A_d, &B_d, &C_d, reinterpret_cast<void *>(&NElem)};
-  kNodeParams.func = reinterpret_cast<void *>(HipTest::vectorADD<int>);
+  void* kernelArgs[] = {&A_d, &B_d, &C_d, reinterpret_cast<void*>(&NElem)};
+  kNodeParams.func = reinterpret_cast<void*>(HipTest::vectorADD<int>);
   kNodeParams.gridDim = dim3(blocks);
   kNodeParams.blockDim = dim3(threadsPerBlock);
-  kNodeParams.sharedMemBytes = 0;
-  kNodeParams.kernelParams = reinterpret_cast<void **>(kernelArgs);
-  kNodeParams.extra = nullptr;
+  kNodeParams.kernelParams = reinterpret_cast<void**>(kernelArgs);
 
   SECTION("Pass pGraphNode as nullptr") {
-    ret = hipGraphAddKernelNode(nullptr, graph, nullptr, 0, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(nullptr, graph, nullptr, 0, &kNodeParams),
+                    hipErrorInvalidValue);
   }
+
   SECTION("Pass Graph as nullptr") {
-    ret = hipGraphAddKernelNode(&kNode, nullptr, nullptr, 0, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(&kNode, nullptr, nullptr, 0, &kNodeParams),
+                    hipErrorInvalidValue);
   }
+
   SECTION("Pass invalid numDependencies") {
-    ret = hipGraphAddKernelNode(&kNode, graph, nullptr, 11, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(&kNode, graph, nullptr, 11, &kNodeParams),
+                    hipErrorInvalidValue);
   }
+
   SECTION("Pass invalid numDependencies and valid list for dependencies") {
     HIP_CHECK(hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams));
     dependencies.push_back(kNode);
-    ret = hipGraphAddKernelNode(&kNode, graph,
-              dependencies.data(), dependencies.size()+1, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(&kNode, graph, dependencies.data(),
+                                          dependencies.size() + 1, &kNodeParams),
+                    hipErrorInvalidValue);
   }
+
   SECTION("Pass NodeParams as nullptr") {
-    ret = hipGraphAddKernelNode(&kNode, graph,
-              dependencies.data(), dependencies.size(), nullptr);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(
+        hipGraphAddKernelNode(&kNode, graph, dependencies.data(), dependencies.size(), nullptr),
+        hipErrorInvalidValue);
   }
-  SECTION("Pass NodeParams func datamember as nullptr") {
+
+  SECTION("Pass NodeParams func data member as nullptr") {
     kNodeParams.func = nullptr;
-    ret = hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams);
-    REQUIRE(hipSuccess != ret);
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams),
+                    hipErrorInvalidDeviceFunction);
   }
-  SECTION("Pass kernelParams datamember as nullptr") {
-    kNodeParams.func = reinterpret_cast<void *>(HipTest::vectorADD<int>);
+
+  SECTION("Pass kernelParams data member as nullptr") {
     kNodeParams.kernelParams = nullptr;
-    ret = hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams),
+                    hipErrorInvalidValue);
   }
-#if HT_AMD
-// On Cuda setup this test case getting failed
+
+#if HT_AMD  // On Cuda setup this test case getting failed
   SECTION("Try adding kernel node after destroy the already created graph") {
-    kNodeParams.kernelParams = reinterpret_cast<void **>(kernelArgs);
-    HIP_CHECK(hipGraphDestroy(graph));
-    ret = hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    hipGraph_t destroyed_graph;
+    HIP_CHECK(hipGraphCreate(&destroyed_graph, 0));
+    HIP_CHECK(hipGraphDestroy(destroyed_graph));
+    HIP_CHECK_ERROR(hipGraphAddKernelNode(&kNode, destroyed_graph, nullptr, 0, &kNodeParams),
+                    hipErrorInvalidValue);
   }
 #endif
 
   HIP_CHECK(hipFree(A_d));
   HIP_CHECK(hipFree(B_d));
   HIP_CHECK(hipFree(C_d));
+  HIP_CHECK(hipGraphDestroy(graph));
 }
-
--- catch/unit/graph/hipGraphExecKernelNodeSetParams.cc
+++ catch/unit/graph/hipGraphExecKernelNodeSetParams.cc
@@ -1,30 +1,33 @@
 /*
 Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANNTY OF ANY KIND, EXPRESS OR
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER INN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR INN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
 /**
-Testcase Scenarios :
+Test Case Scenarios :
 Negative -
 1) Pass hGraphExec as nullptr and verify api returns error code.
 2) Pass node as nullptr and verify api returns error code.
 3) Pass NodeParams as un-initialized structure object and verify api returns error code.
 4) Pass pNodeParams as nullptr and verify api returns error code.
-5) Pass NodeParams:func datamember as nullptr and verify api returns error code.
+5) Pass NodeParams:func data member as nullptr and verify api returns error code.
 Functional -
 1) Instantiate a graph with kernel node, obtain executable graph and update
    the kernel node params with set and check it is taking effect.
@@ -39,12 +42,11 @@ Functional -
  */
 TEST_CASE("Unit_hipGraphExecKernelNodeSetParams_Negative") {
   constexpr size_t N = 1024;
-  constexpr size_t Nbytes = N * sizeof(int);
   constexpr auto blocksPerCU = 6;  // to hide latency
   constexpr auto threadsPerBlock = 256;
+  unsigned blocks = HipTest::setNumBlocks(blocksPerCU, threadsPerBlock, N);
   hipGraph_t graph;
-  hipError_t ret;
-  hipGraphNode_t memcpyNode, kNode{};
+  hipGraphNode_t kNode{};
   hipKernelNodeParams kNodeParams{};
   hipStream_t streamForGraph;
   int *A_d, *B_d, *C_d;
@@ -55,57 +57,61 @@ TEST_CASE("Unit_hipGraphExecKernelNodeSetParams_Negative") {
 
   HIP_CHECK(hipStreamCreate(&streamForGraph));
   HipTest::initArrays(&A_d, &B_d, &C_d, &A_h, &B_h, &C_h, N, false);
-  unsigned blocks = HipTest::setNumBlocks(blocksPerCU, threadsPerBlock, N);
-
   HIP_CHECK(hipGraphCreate(&graph, 0));
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, nullptr, 0, A_d, A_h,
-                                   Nbytes, hipMemcpyHostToDevice));
-  dependencies.push_back(memcpyNode);
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, nullptr, 0, B_d, B_h,
-                                   Nbytes, hipMemcpyHostToDevice));
-  dependencies.push_back(memcpyNode);
 
-  void* kernelArgs[] = {&A_d, &B_d, &C_d, reinterpret_cast<void *>(&NElem)};
-  kNodeParams.func = reinterpret_cast<void *>(HipTest::vectorADD<int>);
+  void* kernelArgs[] = {&A_d, &B_d, &C_d, reinterpret_cast<void*>(&NElem)};
+  kNodeParams.func = reinterpret_cast<void*>(HipTest::vectorADD<int>);
   kNodeParams.gridDim = dim3(blocks);
   kNodeParams.blockDim = dim3(threadsPerBlock);
-  kNodeParams.sharedMemBytes = 0;
   kNodeParams.kernelParams = reinterpret_cast<void**>(kernelArgs);
-  kNodeParams.extra = nullptr;
+  HIP_CHECK(hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams));
+
+  hipGraphNode_t empty_node;
+  HIP_CHECK(hipGraphAddEmptyNode(&empty_node, graph, &kNode, 1));
 
   // Instantiate and launch the graph
-  HIP_CHECK(hipGraphInstantiate(&graphExec, graph, NULL, NULL, 0));
+  HIP_CHECK(hipGraphInstantiate(&graphExec, graph, nullptr, nullptr, 0));
 
   SECTION("Pass hipGraphExec as nullptr") {
-    ret = hipGraphExecKernelNodeSetParams(nullptr, kNode, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(nullptr, kNode, &kNodeParams),
+                    hipErrorInvalidValue);
   }
+
   SECTION("Pass Node as nullptr") {
-    ret = hipGraphExecKernelNodeSetParams(graphExec, nullptr, &kNodeParams);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(graphExec, nullptr, &kNodeParams),
+                    hipErrorInvalidValue);
   }
+
 #if HT_AMD
-  /* NodeParams null check is disabled on Nvedia as
+  /* NodeParams null check is disabled on Nvidia as
    * this call gives SIGSEGV error in CUDA setup */
   SECTION("Pass NodeParams as nullptr") {
-    ret = hipGraphExecKernelNodeSetParams(graphExec, kNode, nullptr);
-    REQUIRE(hipErrorInvalidValue == ret);
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(graphExec, kNode, nullptr),
+                    hipErrorInvalidValue);
   }
 #endif
-/* For below 2 scenarios -
-   In AMD setup this API return - hipErrorInvalidValue and
-   In CUDA setup this API return - hipErrorInvalidDeviceFunction
-   As per Cuda spec API can only return "cudaSuccess, cudaErrorInvalidValue".
-*/
-  SECTION("Pass NodeParams as un-initialized structure object") {
-    hipKernelNodeParams kNodeParams1{};
-    ret = hipGraphExecKernelNodeSetParams(graphExec, kNode, &kNodeParams1);
-    REQUIRE(hipSuccess != ret);
-  }
-  SECTION("Pass NodeParams func datamember as nullptr") {
+
+  SECTION("Pass NodeParams func data member as nullptr") {
     kNodeParams.func = nullptr;
-    ret = hipGraphExecKernelNodeSetParams(graphExec, kNode, &kNodeParams);
-    REQUIRE(hipSuccess != ret);
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(graphExec, kNode, &kNodeParams),
+                    hipErrorInvalidDeviceFunction);
+  }
+
+  SECTION("Pass kernelParams data member as nullptr") {
+    kNodeParams.kernelParams = nullptr;
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(graphExec, kNode, &kNodeParams),
+                    hipErrorInvalidValue);
+  }
+
+  SECTION("node is not a kernel node") {
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(graphExec, empty_node, &kNodeParams),
+                    hipErrorInvalidValue);
+  }
+
+  SECTION("node is not instantiated") {
+    HIP_CHECK(hipGraphAddKernelNode(&kNode, graph, nullptr, 0, &kNodeParams));
+    HIP_CHECK_ERROR(hipGraphExecKernelNodeSetParams(graphExec, kNode, &kNodeParams),
+                    hipErrorInvalidValue);
   }
 
   HipTest::freeArrays(A_d, B_d, C_d, A_h, B_h, C_h, false);
@@ -114,16 +120,15 @@ TEST_CASE("Unit_hipGraphExecKernelNodeSetParams_Negative") {
   HIP_CHECK(hipStreamDestroy(streamForGraph));
 }
 
-
 /**
  * Functional Test for API Exec Kernel Params
  */
-
 TEST_CASE("Unit_hipGraphExecKernelNodeSetParams_Functional") {
   constexpr size_t N = 1024;
   constexpr size_t Nbytes = N * sizeof(int);
   constexpr auto blocksPerCU = 6;  // to hide latency
   constexpr auto threadsPerBlock = 256;
+  unsigned blocks = HipTest::setNumBlocks(blocksPerCU, threadsPerBlock, N);
   hipGraph_t graph;
   hipGraphNode_t memcpyNode, kNode;
   hipKernelNodeParams kNodeParams{}, kNodeParams1{};
@@ -136,43 +141,36 @@ TEST_CASE("Unit_hipGraphExecKernelNodeSetParams_Functional") {
 
   HIP_CHECK(hipStreamCreate(&streamForGraph));
   HipTest::initArrays(&A_d, &B_d, &C_d, &A_h, &B_h, &C_h, N, false);
-  unsigned blocks = HipTest::setNumBlocks(blocksPerCU, threadsPerBlock, N);
-
   HIP_CHECK(hipGraphCreate(&graph, 0));
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, nullptr, 0, A_d, A_h,
-                                   Nbytes, hipMemcpyHostToDevice));
+
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, nullptr, 0, A_d, A_h, Nbytes,
+                                    hipMemcpyHostToDevice));
   dependencies.push_back(memcpyNode);
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, nullptr, 0, B_d, B_h,
-                                   Nbytes, hipMemcpyHostToDevice));
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, nullptr, 0, B_d, B_h, Nbytes,
+                                    hipMemcpyHostToDevice));
   dependencies.push_back(memcpyNode);
 
-  void* kernelArgs[] = {&A_d, &B_d, &C_d, reinterpret_cast<void *>(&NElem)};
-  kNodeParams.func = reinterpret_cast<void *>(HipTest::vectorADD<int>);
+  void* kernelArgs[] = {&A_d, &B_d, &C_d, reinterpret_cast<void*>(&NElem)};
+  kNodeParams.func = reinterpret_cast<void*>(HipTest::vectorADD<int>);
   kNodeParams.gridDim = dim3(blocks);
   kNodeParams.blockDim = dim3(threadsPerBlock);
-  kNodeParams.sharedMemBytes = 0;
   kNodeParams.kernelParams = reinterpret_cast<void**>(kernelArgs);
-  kNodeParams.extra = nullptr;
-  HIP_CHECK(hipGraphAddKernelNode(&kNode, graph, dependencies.data(),
-                                  dependencies.size(), &kNodeParams));
+  HIP_CHECK(
+      hipGraphAddKernelNode(&kNode, graph, dependencies.data(), dependencies.size(), &kNodeParams));
 
-  memset(&kNodeParams1, 0, sizeof(kNodeParams1));
-  kNodeParams1.func = reinterpret_cast<void *>(HipTest::vectorSUB<int>);
+  kNodeParams1.func = reinterpret_cast<void*>(HipTest::vectorSUB<int>);
   kNodeParams1.gridDim = dim3(blocks);
   kNodeParams1.blockDim = dim3(threadsPerBlock);
-  kNodeParams1.sharedMemBytes = 0;
   kNodeParams1.kernelParams = reinterpret_cast<void**>(kernelArgs);
-  kNodeParams1.extra = nullptr;
 
   dependencies.clear();
   dependencies.push_back(kNode);
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, dependencies.data(),
-                                    dependencies.size(), C_h, C_d,
-                                    Nbytes, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyNode, graph, dependencies.data(), dependencies.size(),
+                                    C_h, C_d, Nbytes, hipMemcpyDeviceToHost));
+
   // Instantiate and launch the graph
   HIP_CHECK(hipGraphInstantiate(&graphExec, graph, NULL, NULL, 0));
-  REQUIRE(hipSuccess == hipGraphExecKernelNodeSetParams(graphExec, kNode,
-                                      &kNodeParams1));
+  HIP_CHECK(hipGraphExecKernelNodeSetParams(graphExec, kNode, &kNodeParams1));
   HIP_CHECK(hipGraphLaunch(graphExec, streamForGraph));
   HIP_CHECK(hipStreamSynchronize(streamForGraph));
 

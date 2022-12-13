e3a535dc3aa8ed5b43098b7e1cbd71c9bde660af
Milos Mozetic <milos.mozetic@htecgroup.com>
2022-12-13 11:28:53 +0100

    EXSWHTEC-223 - Adjust table size for HIP API test cases

--- utils/coverage/hipAPI.cpp
+++ utils/coverage/hipAPI.cpp
@@ -143,8 +143,12 @@ std::string HipAPI::createHTMLReport() const {
 
   html_report << one_tab << "<center>";
   // Add info about test cases
-  html_report << one_tab << "<table width=\"50%\" cellpadding=1 cellspacing=1 border=0>";
+  html_report << one_tab << "<table width=\"20%\" cellpadding=1 cellspacing=1 border=0>";
   if (!test_cases.empty()) {
+    html_report << two_tabs << "<tr>";
+    html_report << three_tabs << "<td class=\"tableHead\"><br></td>";
+    html_report << two_tabs << "</tr>";
+
     html_report << two_tabs << "<tr>";
     html_report << three_tabs << "<td class=\"tableHead\">Test case ID</td>";
     html_report << two_tabs << "</tr>";

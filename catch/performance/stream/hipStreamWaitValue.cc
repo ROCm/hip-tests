acf3f631ea34ca6216f0f01ca5c8e7429a2ed4c7
Miloš Mozetić <milos.mozetic@htecgroup.com>
2023-01-24 12:05:28 +0000

    EXSWHTEC-251 - Add performance tests for CU mask, Callback, get Flags and Priority, wait Event and Value and write Value

---
+++ catch/performance/stream/hipStreamWaitValue.cc
@@ -0,0 +1,97 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+#include <performance_common.hh>
+
+std::string GetFlagWaitSectionName(unsigned int flag) {
+  if (flag == hipStreamWaitValueGte) {
+    return "greater than";
+  } else if (flag == hipStreamWaitValueEq) {
+    return "equal";
+  } else if (flag == hipStreamWaitValueAnd) {
+    return "logical and";
+  } else if (flag == hipStreamWaitValueNor) {
+    return "logical nor";
+  } else {
+    return "unknown flag";
+  }
+}
+
+class StreamWaitValue32Benchmark : public Benchmark<StreamWaitValue32Benchmark> {
+ public:
+  void operator()(const size_t array_size, unsigned int flag) {
+    const StreamGuard stream_guard{Streams::created};
+    const hipStream_t stream = stream_guard.stream();
+    uint32_t* value_ptr;
+    uint32_t value{0};
+    if (flag == hipStreamWaitValueAnd) {
+      value = 1;
+    }
+    HIP_CHECK(hipMalloc(&value_ptr, sizeof(uint32_t) * array_size));
+    HIP_CHECK(hipMemset(value_ptr, value, sizeof(uint32_t) * array_size));
+
+    TIMED_SECTION(kTimerTypeCpu) {
+      HIP_CHECK(hipStreamWaitValue32(stream, value_ptr, value, flag));
+    }
+    HIP_CHECK(hipFree(value_ptr));
+  }
+};
+
+class StreamWaitValue64Benchmark : public Benchmark<StreamWaitValue64Benchmark> {
+ public:
+  void operator()(const size_t array_size, unsigned int flag) {
+    const StreamGuard stream_guard{Streams::created};
+    const hipStream_t stream = stream_guard.stream();
+    uint64_t* value_ptr;
+    uint64_t value{0};
+    if (flag == hipStreamWaitValueAnd) {
+      value = 1;
+    }
+    HIP_CHECK(hipMalloc(&value_ptr, sizeof(uint64_t) * array_size));
+    HIP_CHECK(hipMemset(value_ptr, value, sizeof(uint64_t) * array_size));
+
+    TIMED_SECTION(kTimerTypeCpu) {
+      HIP_CHECK(hipStreamWaitValue64(stream, value_ptr, value, flag));
+    }
+    HIP_CHECK(hipFree(value_ptr));
+  }
+};
+
+template <typename WaitValueBenchmark>
+static void RunBenchmark(const size_t array_size, unsigned int flag) {
+  WaitValueBenchmark benchmark;
+  benchmark.AddSectionName(std::to_string(array_size));
+  benchmark.AddSectionName(GetFlagWaitSectionName(flag));
+  benchmark.Run(array_size, flag);
+}
+
+TEST_CASE("Performance_hipStreamWaitValue32") {
+  size_t array_size = GENERATE(4_KB, 4_MB, 16_MB);
+  unsigned int flag = GENERATE(hipStreamWaitValueGte, hipStreamWaitValueEq,
+                               hipStreamWaitValueAnd, hipStreamWaitValueNor);
+  RunBenchmark<StreamWaitValue32Benchmark>(array_size, flag);
+}
+
+TEST_CASE("Performance_hipStreamWaitValue64") {
+  size_t array_size = GENERATE(4_KB, 4_MB, 16_MB);
+  unsigned int flag = GENERATE(hipStreamWaitValueGte, hipStreamWaitValueEq,
+                               hipStreamWaitValueAnd, hipStreamWaitValueNor);
+  RunBenchmark<StreamWaitValue64Benchmark>(array_size, flag);
+}
---
+++ catch/performance/stream/hipStreamWriteValue.cc
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+#include <performance_common.hh>
+
+class StreamWriteValue32Benchmark : public Benchmark<StreamWriteValue32Benchmark> {
+ public:
+  void operator()(const size_t array_size) {
+    const StreamGuard stream_guard{Streams::created};
+    const hipStream_t stream = stream_guard.stream();
+    uint32_t* value_ptr;
+    uint32_t value{0};
+    HIP_CHECK(hipMalloc(&value_ptr, sizeof(uint32_t) * array_size));
+    HIP_CHECK(hipMemset(value_ptr, value, sizeof(uint32_t) * array_size));
+
+    TIMED_SECTION(kTimerTypeCpu) {
+      HIP_CHECK(hipStreamWriteValue32(stream, value_ptr, value, 0));
+    }
+    HIP_CHECK(hipFree(value_ptr));
+  }
+};
+
+class StreamWriteValue64Benchmark : public Benchmark<StreamWriteValue64Benchmark> {
+ public:
+  void operator()(const size_t array_size) {
+    const StreamGuard stream_guard{Streams::created};
+    const hipStream_t stream = stream_guard.stream();
+    uint64_t* value_ptr;
+    uint64_t value{0};
+    HIP_CHECK(hipMalloc(&value_ptr, sizeof(uint64_t) * array_size));
+    HIP_CHECK(hipMemset(value_ptr, value, sizeof(uint64_t) * array_size));
+
+    TIMED_SECTION(kTimerTypeCpu) {
+      HIP_CHECK(hipStreamWriteValue64(stream, value_ptr, value, 0));
+    }
+    HIP_CHECK(hipFree(value_ptr));
+  }
+};
+
+template <typename WriteValueBenchmark>
+static void RunBenchmark(const size_t array_size) {
+  WriteValueBenchmark benchmark;
+  benchmark.AddSectionName(std::to_string(array_size));
+  benchmark.Run(array_size);
+}
+
+TEST_CASE("Performance_hipStreamWriteValue32") {
+  size_t array_size = GENERATE(4_KB, 4_MB, 16_MB);
+  RunBenchmark<StreamWriteValue32Benchmark>(array_size);
+}
+
+TEST_CASE("Performance_hipStreamWriteValue64") {
+  size_t array_size = GENERATE(4_KB, 4_MB, 16_MB);
+  RunBenchmark<StreamWriteValue64Benchmark>(array_size);
+}

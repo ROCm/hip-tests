332d96694ec37f5119c4af5638de6445cbb016eb
SrinivasaRao <Srinivasarao.Gollamandala@amd.com>
2023-02-20 15:21:07 +0530

    SWDEV-380340 - [catch2][dtest] DynamicLoading tests migrated from direct to catch2
    Change-Id: I5f82ddd565d4e86270498d2d8b155200ce568849

--- catch/include/hip_test_defgroups.hh
+++ catch/include/hip_test_defgroups.hh
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 - 2022 Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c) 2021 - 2023 Advanced Micro Devices, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -91,5 +91,11 @@ THE SOFTWARE.
  * @defgroup StreamTest Stream Management
  * @{
  * This section describes the stream management types & functions of HIP runtime API.
+ * @}
+ */
+/**
+ * @defgroup DynamicLoadingTest Kernel Loading Management
+ * @{
+ * This section describes the different kernel launch approaches.
  * @}
  */

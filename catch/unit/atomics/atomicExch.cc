3d545177b26a2ee8dea29268ef8fc0496f3f29c6
Dino Musić <dino.music@htecgroup.com>
2023-02-28 11:51:49 +0000

    EXSWHTEC-265 - Refactor test harness to add concurrent kernel support

--- catch/unit/atomics/atomicExch.cc
+++ catch/unit/atomics/atomicExch.cc
@@ -83,6 +83,15 @@ void AtomicExchSameAddressTest(const dim3 blocks, const dim3 threads,
                       hipMemcpyDeviceToHost));
   HIP_CHECK(hipDeviceSynchronize());
 
+  // Every thread will exchange its grid-wide linear id into a target location within mem_dev,
+  // receiving back the value previously present therein. This previous value is written to
+  // old_vals_dev.
+  // old_vals_dev will not contain values that the final scheduled warp exchanged into mem_dev, but
+  // mem_dev obviously will.
+  // Given that mem_dev initially contains values in the range [0, width) and that the maximum value
+  // the final thread shall write is thread_count + width - 1, presuming correct operation of
+  // atomicExch, the union of mem_dev and old_vals_dev shall contain values in the range
+  //[0, thread_count + width)
   std::sort(old_vals.begin(), old_vals.end());
   for (auto i = 0u; i < old_vals.size(); ++i) {
     REQUIRE(i == old_vals[i]);
@@ -143,96 +152,11 @@ __global__ void atomicExchMultiDestKernel(T* const global_mem, T* const old_vals
   }
 }
 
-template <typename TestType, bool use_shared_mem>
+template <typename TestType, bool use_shared_mem, unsigned int kernel_count = 1u>
 void AtomicExchMultiDestWithScatter(const dim3 blocks, const dim3 threads,
                                     const LinearAllocs alloc_type, const unsigned int width,
                                     const unsigned int pitch) {
-  const auto mem_alloc_size = width * pitch;
-  LinearAllocGuard<TestType> mem_dev(alloc_type, mem_alloc_size);
-
-  const auto thread_count = blocks.x * blocks.y * blocks.z * threads.x * threads.y * threads.z;
-  const auto old_vals_alloc_size = thread_count * sizeof(TestType);
-  LinearAllocGuard<TestType> old_vals_dev(LinearAllocs::hipMalloc, old_vals_alloc_size);
-  std::vector<TestType> old_vals(thread_count + width);
-  std::iota(old_vals.begin(), old_vals.begin() + width, 0);
-
-  HIP_CHECK(hipMemcpy2D(mem_dev.ptr(), pitch, old_vals.data(), sizeof(TestType), sizeof(TestType),
-                        width, hipMemcpyHostToDevice));
-  const auto shared_mem_size = use_shared_mem ? mem_alloc_size : 0u;
-  atomicExchMultiDestKernel<TestType, use_shared_mem>
-      <<<blocks, threads, shared_mem_size>>>(mem_dev.ptr(), old_vals_dev.ptr(), width, pitch);
-  HIP_CHECK(hipGetLastError());
-
-  HIP_CHECK(
-      hipMemcpy(old_vals.data(), old_vals_dev.ptr(), old_vals_alloc_size, hipMemcpyDeviceToHost));
-  HIP_CHECK(hipMemcpy2D(old_vals.data() + thread_count, sizeof(TestType), mem_dev.ptr(), pitch,
-                        sizeof(TestType), width, hipMemcpyDeviceToHost));
-  HIP_CHECK(hipDeviceSynchronize());
-
-  // Every thread will exchange its grid-wide linear id into a target location within mem_dev,
-  // receiving back the value previously present therein. This previous value is written to
-  // old_vals_dev.
-  // old_vals_dev will not contain values that the final scheduled warp exchanged into mem_dev, but
-  // mem_dev obviously will.
-  // Given that mem_dev initially contains values in the range [0, width) and that the maximum value
-  // the final thread shall write is thread_count + width - 1, presuming correct operation of
-  // atomicExch, the union of mem_dev and old_vals_dev shall contain values in the range
-  //[0, thread_count + width)
-  std::sort(old_vals.begin(), old_vals.end());
-  for (auto i = 0u; i < old_vals.size(); ++i) {
-    REQUIRE(i == old_vals[i]);
-  }
-}
-
-template <typename TestType>
-void AtomicExchMultiDestWithScatterTest(const unsigned int width, const unsigned int pitch) {
-  const auto threads = GENERATE(dim3(1024), dim3(1023), dim3(511), dim3(17), dim3(31));
-
-  SECTION("Global memory") {
-    const auto blocks = GENERATE(dim3(40));
-    using LA = LinearAllocs;
-    for (const auto alloc_type :
-         {LA::hipMalloc, LA::hipHostMalloc, LA::hipMallocManaged, LA::mallocAndRegister}) {
-      DYNAMIC_SECTION("Allocation type: " << to_string(alloc_type)) {
-        AtomicExchMultiDestWithScatter<TestType, false>(blocks, threads, LinearAllocs::hipMalloc,
-                                                        width, pitch);
-      }
-    }
-  }
-
-  SECTION("Shared memory") {
-    const auto blocks = GENERATE(dim3(1));
-    AtomicExchMultiDestWithScatter<TestType, true>(blocks, threads, LinearAllocs::hipMalloc, width,
-                                                   pitch);
-  }
-}
-
-TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Same_Address_Runtime", "", int, unsigned int,
-                   unsigned long long, float) {
-  AtomicExchMultiDestWithScatterTest<TestType>(1, sizeof(TestType));
-}
-
-TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Adjacent_Addresses", "", int, unsigned int,
-                   unsigned long long, float) {
-  int warp_size = 0;
-  HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
-
-  AtomicExchMultiDestWithScatterTest<TestType>(warp_size, sizeof(TestType));
-}
-
-TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Scattered_Addresses", "", int, unsigned int,
-                   unsigned long long, float) {
-  int warp_size = 0;
-  HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
-  const auto cache_line_size = 128u;
-
-  AtomicExchMultiDestWithScatterTest<TestType>(warp_size, cache_line_size);
-}
-
-template <typename TestType, unsigned int kernel_count, bool use_shared_mem>
-void Foo(const dim3 blocks, const dim3 threads, const LinearAllocs alloc_type,
-            const unsigned int width, const unsigned int pitch) {
-  static_assert(kernel_count > 1 && !use_shared_mem,
+  static_assert(!(kernel_count > 1 && use_shared_mem),
                 "Shared memory should not be used for a multiple kernel launch");
 
   if constexpr (kernel_count > 1) {
@@ -278,8 +202,69 @@ void Foo(const dim3 blocks, const dim3 threads, const LinearAllocs alloc_type,
   }
 }
 
-TEST_CASE("Foo") {
+template <typename TestType, unsigned int kernel_count = 1>
+void AtomicExchMultiDestWithScatterTest(const unsigned int width, const unsigned int pitch) {
   const auto threads = GENERATE(dim3(1024), dim3(1023), dim3(511), dim3(17), dim3(31));
-  const auto blocks = GENERATE(dim3(40));
-  Foo<int, 2, false>(blocks, threads, LinearAllocs::hipMallocManaged, 32, 128);
+
+  SECTION("Global memory") {
+    const auto blocks = GENERATE(dim3(40));
+    using LA = LinearAllocs;
+    for (const auto alloc_type :
+         {LA::hipMalloc, LA::hipHostMalloc, LA::hipMallocManaged, LA::mallocAndRegister}) {
+      DYNAMIC_SECTION("Allocation type: " << to_string(alloc_type)) {
+        AtomicExchMultiDestWithScatter<TestType, false, kernel_count>(
+            blocks, threads, LinearAllocs::hipMalloc, width, pitch);
+      }
+    }
+  }
+
+  SECTION("Shared memory") {
+    const auto blocks = GENERATE(dim3(1));
+    AtomicExchMultiDestWithScatter<TestType, true, 1>(blocks, threads, LinearAllocs::hipMalloc,
+                                                      width, pitch);
+  }
+}
+
+TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Same_Address_Runtime", "", int, unsigned int,
+                   unsigned long long, float) {
+  AtomicExchMultiDestWithScatterTest<TestType>(1, sizeof(TestType));
+}
+
+TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Adjacent_Addresses", "", int, unsigned int,
+                   unsigned long long, float) {
+  int warp_size = 0;
+  HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
+
+  AtomicExchMultiDestWithScatterTest<TestType>(warp_size, sizeof(TestType));
+}
+
+TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Scattered_Addresses", "", int, unsigned int,
+                   unsigned long long, float) {
+  int warp_size = 0;
+  HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
+  const auto cache_line_size = 128u;
+
+  AtomicExchMultiDestWithScatterTest<TestType>(warp_size, cache_line_size);
+}
+
+TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Same_Address_Runtime_Multi_Kernel", "", int,
+                   unsigned int, unsigned long long, float) {
+  AtomicExchMultiDestWithScatterTest<TestType, 2>(1, sizeof(TestType));
+}
+
+TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Adjacent_Addresses_Multi_Kernel", "", int,
+                   unsigned int, unsigned long long, float) {
+  int warp_size = 0;
+  HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
+
+  AtomicExchMultiDestWithScatterTest<TestType, 2>(warp_size, sizeof(TestType));
+}
+
+TEMPLATE_TEST_CASE("Unit_atomicExch_Positive_Basic_Scattered_Addresses_Multi_Kernel", "", int,
+                   unsigned int, unsigned long long, float) {
+  int warp_size = 0;
+  HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
+  const auto cache_line_size = 128u;
+
+  AtomicExchMultiDestWithScatterTest<TestType, 2>(warp_size, cache_line_size);
 }

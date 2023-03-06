2345d35ce66fbafb728ab96b458352002fe342e0
Nives Vukovic <nives.vukovic@htecgroup.com>
2023-03-06 13:03:09 +0100

    EXSWHTEC-256 - Further code cleanup

--- catch/unit/cooperativeGrps/hipCGThreadBlockType.cc
+++ catch/unit/cooperativeGrps/hipCGThreadBlockType.cc
@@ -1,13 +1,16 @@
 /*
-Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c) 2020 - 2021 Advanced Micro Devices, Inc. All rights reserved.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -17,117 +20,145 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#pragma once
 
-#include "cpu_grid.h"
-
-#include <optional>
+/* HIT_START
+ * BUILD: %t %s ../../test_common.cpp
+ * TEST: %t
+ * HIT_END
+ */
 
 #include <hip_test_common.hh>
 #include <hip/hip_cooperative_groups.h>
-#include <resource_guards.hh>
-#include <utils.hh>
+#include <cstdlib>
 
-static __device__ bool deactivate_thread(const uint64_t* const active_masks) {
-  const auto warp =
-      cooperative_groups::tiled_partition(cooperative_groups::this_thread_block(), warpSize);
-  const auto block = cooperative_groups::this_thread_block();
-  const auto warps_per_block = (block.size() + warpSize - 1) / warpSize;
-  const auto block_rank = (blockIdx.z * gridDim.y + blockIdx.y) * gridDim.x + blockIdx.x;
-  const auto idx = block_rank * warps_per_block + block.thread_rank() / warpSize;
+#define ASSERT_EQUAL(lhs, rhs) HIPASSERT(lhs == rhs)
 
-  return !(active_masks[idx] & (static_cast<uint64_t>(1) << warp.thread_rank()));
+using namespace cooperative_groups;
+
+static __global__
+void kernel_cg_thread_block_type(int *sizeTestD,
+                                 int *thdRankTestD,
+                                 int *syncTestD,
+                                 dim3 *groupIndexTestD,
+                                 dim3 *thdIndexTestD)
+{
+  thread_block tb = this_thread_block();
+  int gIdx = (blockIdx.x * blockDim.x) + threadIdx.x;
+  // Test size
+  sizeTestD[gIdx] = tb.size();
+
+  // Test thread_rank
+  thdRankTestD[gIdx] = tb.thread_rank();
+
+  // Test sync
+  __shared__ int sm[2];
+  if (threadIdx.x == 0)
+    sm[0] = 10;
+  else if (threadIdx.x == 1)
+    sm[1] = 20;
+  tb.sync();
+  syncTestD[gIdx] = sm[1] * sm[0];
+
+  // Test group_index
+  groupIndexTestD[gIdx] = tb.group_index();
+
+  // Test thread_index
+  thdIndexTestD[gIdx] = tb.thread_index();
 }
 
-static inline std::mt19937& GetRandomGenerator() {
-  static std::mt19937 mt(11);
-  return mt;
-}
+static void test_cg_thread_block_type(int blockSize)
+{
+  int nBytes = sizeof(int) * 2 * blockSize;
+  int nDim3Bytes = sizeof(dim3) * 2 * blockSize;
+  int *sizeTestD, *sizeTestH;
+  int *thdRankTestD, *thdRankTestH;
+  int *syncTestD, *syncTestH;
+  dim3 *groupIndexTestD, *groupIndexTestH;
+  dim3 *thdIndexTestD, *thdIndexTestH;
 
-template <typename T> static inline T GenerateRandomInteger(const T min, const T max) {
-  std::uniform_int_distribution<T> dist(min, max);
-  return dist(GetRandomGenerator());
-}
+  // Allocate device memory
+  HIPCHECK(hipMalloc(&sizeTestD, nBytes));
+  HIPCHECK(hipMalloc(&thdRankTestD, nBytes));
+  HIPCHECK(hipMalloc(&syncTestD, nBytes));
+  HIPCHECK(hipMalloc(&groupIndexTestD, nDim3Bytes));
+  HIPCHECK(hipMalloc(&thdIndexTestD, nDim3Bytes));
 
-inline uint64_t get_predicate_mask(unsigned int test_case, unsigned int warp_size) {
-  uint64_t predicate_mask = 0;
-  switch (test_case) {
-    case 0:  // no thread
-      predicate_mask = 0;
-      break;
-    case 1:  // 1st thread
-      predicate_mask = 1;
-      break;
-    case 2:  // last thread
-      predicate_mask = static_cast<uint64_t>(1) << (warp_size - 1);
-      break;
-    case 3:  // all threads
-      predicate_mask = 0xFFFFFFFFFFFFFFFF;
-      break;
-    default:  // random
-      static std::mt19937_64 mt(test_case);
-      std::uniform_int_distribution<uint64_t> dist(0, std::numeric_limits<uint64_t>::max());
-      predicate_mask = dist(mt);
-  }
-  return predicate_mask;
-}
+  // Allocate host memory
+  HIPCHECK(hipHostMalloc(&sizeTestH, nBytes));
+  HIPCHECK(hipHostMalloc(&thdRankTestH, nBytes));
+  HIPCHECK(hipHostMalloc(&syncTestH, nBytes));
+  HIPCHECK(hipHostMalloc(&groupIndexTestH, nDim3Bytes));
+  HIPCHECK(hipHostMalloc(&thdIndexTestH, nDim3Bytes));
 
-inline uint64_t get_active_predicate(uint64_t predicate, size_t partition_size) {
-  uint64_t active_predicate = predicate;
-  for (int i = partition_size; i < 64; i++) {
-    active_predicate &= ~(static_cast<uint64_t>(1) << i);
-  }
-  return active_predicate;
-}
+  // Launch Kernel
+  hipLaunchKernelGGL(kernel_cg_thread_block_type,
+                     2,
+                     blockSize,
+                     0,
+                     0,
+                     sizeTestD,
+                     thdRankTestD,
+                     syncTestD,
+                     groupIndexTestD,
+                     thdIndexTestD);
 
-template <typename Derived, typename T> class WarpTest {
- public:
-  WarpTest() : warp_size_{get_warp_size()} {}
+  // Copy result from device to host
+  HIPCHECK(hipMemcpy(sizeTestH, sizeTestD, nBytes, hipMemcpyDeviceToHost));
+  HIPCHECK(hipMemcpy(thdRankTestH, thdRankTestD, nBytes, hipMemcpyDeviceToHost));
+  HIPCHECK(hipMemcpy(syncTestH, syncTestD, nBytes, hipMemcpyDeviceToHost));
+  HIPCHECK(hipMemcpy(groupIndexTestH, groupIndexTestD, nDim3Bytes, hipMemcpyDeviceToHost));
+  HIPCHECK(hipMemcpy(thdIndexTestH, thdIndexTestD, nDim3Bytes, hipMemcpyDeviceToHost));
 
-  void run() {
-    const auto blocks = GenerateBlockDimensionsForShuffle();
-    INFO("Grid dimensions: x " << blocks.x << ", y " << blocks.y << ", z " << blocks.z);
-    const auto threads = GenerateThreadDimensionsForShuffle();
-    INFO("Block dimensions: x " << threads.x << ", y " << threads.y << ", z " << threads.z);
-    grid_ = CPUGrid(blocks, threads);
-
-    const auto alloc_size = grid_.thread_count_ * sizeof(T);
-    LinearAllocGuard<T> arr_dev(LinearAllocs::hipMalloc, alloc_size);
-    LinearAllocGuard<T> arr(LinearAllocs::hipHostMalloc, alloc_size);
-    HIP_CHECK(hipMemset(arr_dev.ptr(), 0, alloc_size));
-
-    warps_in_block_ = (grid_.threads_in_block_count_ + warp_size_ - 1) / warp_size_;
-    const auto warps_in_grid = warps_in_block_ * grid_.block_count_;
-    LinearAllocGuard<uint64_t> active_masks_dev(LinearAllocs::hipMalloc,
-                                                warps_in_grid * sizeof(uint64_t));
-    active_masks_.resize(warps_in_grid);
-    std::generate(active_masks_.begin(), active_masks_.end(),
-                  [] { return GenerateRandomInteger(0ul, std::numeric_limits<uint64_t>().max()); });
-
-    HIP_CHECK(hipMemcpy(active_masks_dev.ptr(), active_masks_.data(),
-                        warps_in_grid * sizeof(uint64_t), hipMemcpyHostToDevice));
-    cast_to_derived().launch_kernel(arr_dev.ptr(), active_masks_dev.ptr());
-    HIP_CHECK(hipGetLastError());
-    HIP_CHECK(hipMemcpy(arr.ptr(), arr_dev.ptr(), alloc_size, hipMemcpyDeviceToHost));
-    HIP_CHECK(hipDeviceSynchronize());
-
-    cast_to_derived().validate(arr.ptr());
-  }
-
- private:
-  int get_warp_size() const {
-    int current_dev = -1;
-    HIP_CHECK(hipGetDevice(&current_dev));
-    int warp_size = 0u;
-    HIP_CHECK(hipDeviceGetAttribute(&warp_size, hipDeviceAttributeWarpSize, 0));
-    return warp_size;
+  // Validate results for both blocks together
+  for (int i = 0; i < 2 * blockSize; ++i) {
+    ASSERT_EQUAL(sizeTestH[i], blockSize);
+    ASSERT_EQUAL(thdRankTestH[i], i % blockSize);
+    ASSERT_EQUAL(syncTestH[i], 200);
+    ASSERT_EQUAL(groupIndexTestH[i].x, (uint) i / blockSize);
+    ASSERT_EQUAL(groupIndexTestH[i].y, 0);
+    ASSERT_EQUAL(groupIndexTestH[i].z, 0);
+    ASSERT_EQUAL(thdIndexTestH[i].x, (uint) i % blockSize);
+    ASSERT_EQUAL(thdIndexTestH[i].y, 0);
+    ASSERT_EQUAL(thdIndexTestH[i].z, 0);
   }
 
-  Derived& cast_to_derived() { return reinterpret_cast<Derived&>(*this); }
+  // Free device memory
+  HIPCHECK(hipFree(sizeTestD));
+  HIPCHECK(hipFree(thdRankTestD));
+  HIPCHECK(hipFree(syncTestD));
+  HIPCHECK(hipFree(groupIndexTestD));
+  HIPCHECK(hipFree(thdIndexTestD));
 
- protected:
-  const int warp_size_;
-  CPUGrid grid_;
-  unsigned int warps_in_block_;
-  std::vector<uint64_t> active_masks_;
-};
+  //Free host memory
+  HIPCHECK(hipHostFree(sizeTestH));
+  HIPCHECK(hipHostFree(thdRankTestH));
+  HIPCHECK(hipHostFree(syncTestH));
+  HIPCHECK(hipHostFree(groupIndexTestH));
+  HIPCHECK(hipHostFree(thdIndexTestH));
+}
+
+TEST_CASE("Unit_hipCGThreadBlockType") {
+  // Use default device for validating the test
+  int deviceId;
+  hipDeviceProp_t deviceProperties;
+  HIPCHECK(hipGetDevice(&deviceId));
+  HIPCHECK(hipGetDeviceProperties(&deviceProperties, deviceId));
+
+  if (!deviceProperties.cooperativeLaunch) {
+    HipTest::HIP_SKIP_TEST("Device doesn't support cooperative launch!");
+    return;
+  }
+
+  // Test for blockSizes in powers of 2
+  int maxThreadsPerBlock = deviceProperties.maxThreadsPerBlock;
+  for (int blockSize = 2; blockSize <= maxThreadsPerBlock; blockSize = blockSize*2) {
+    test_cg_thread_block_type(blockSize);
+  }
+
+  // Test for random blockSizes, but the sequence is the same every execution
+  srand(0);
+  for (int i = 0; i < 10; i++) {
+    // Test fails for only 1 thread per block
+    test_cg_thread_block_type(max(2, rand() % maxThreadsPerBlock));
+  }
+}

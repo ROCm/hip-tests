cbf14e4eb4e23c15f1cb3d28281179c98b6f4d09
Jaydeep Patel <jaydeepkumar.patel@amd.com>
2023-03-06 04:24:15 +0000

    SWDEV-362365 - Correct error code.
    Change-Id: I329a3296ce56ae741118b4f66294001025d48443

--- catch/unit/device/hipDeviceSetGetSharedMemConfig.cc
+++ catch/unit/device/hipDeviceSetGetSharedMemConfig.cc
@@ -43,13 +43,8 @@ TEST_CASE("Unit_hipDeviceSetSharedMemConfig_Positive_Basic") {
 }
 
 TEST_CASE("Unit_hipDeviceSetSharedMemConfig_Negative_Parameters") {
-#if HT_AMD
-  HIP_CHECK_ERROR(hipDeviceSetSharedMemConfig(static_cast<hipSharedMemConfig>(-1)),
-                  hipErrorNotSupported);
-#elif HT_NVIDIA
   HIP_CHECK_ERROR(hipDeviceSetSharedMemConfig(static_cast<hipSharedMemConfig>(-1)),
                   hipErrorInvalidValue);
-#endif
 }
 
 TEST_CASE("Unit_hipDeviceGetSharedMemConfig_Positive_Default") {

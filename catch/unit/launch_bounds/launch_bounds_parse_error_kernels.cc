67ded312f8268826c81831b0db74c9fd5118c90b
Miloš Mozetić <milos.mozetic@htecgroup.com>
2023-03-15 13:31:14 +0000

    EXSWHTEC-280 - Make separate .cc files for kernels that cause compiler error and parse error

--- catch/unit/launch_bounds/launch_bounds_parse_error_kernels.cc
+++ catch/unit/launch_bounds/launch_bounds_parse_error_kernels.cc
@@ -19,27 +19,12 @@ THE SOFTWARE.
 
 #include <hip_test_common.hh>
 
-__launch_bounds__(0) __global__ void MaxThreadsZero(int* sum) {
-  const int tid = threadIdx.x + blockIdx.x * blockDim.x;
-  atomicAdd(sum, tid);
-}
-
 __launch_bounds__(-1) __global__ void MaxThreadsNegative(int* sum) {
   const int tid = threadIdx.x + blockIdx.x * blockDim.x;
   atomicAdd(sum, tid);
 }
 
 __launch_bounds__(128, -1) __global__ void MinWarpsNegative(int* sum) {
-  const int tid = threadIdx.x + blockIdx.x * blockDim.x;
-  atomicAdd(sum, tid);
-}
-
-__launch_bounds__(1.5) __global__ void MaxThreadsNotInt(int* sum) {
-  const int tid = threadIdx.x + blockIdx.x * blockDim.x;
-  atomicAdd(sum, tid);
-}
-
-__launch_bounds__(128, 1.5) __global__ void MinWarpsNotInt(int* sum) {
   const int tid = threadIdx.x + blockIdx.x * blockDim.x;
   atomicAdd(sum, tid);
 }

e77ed4f79048f6fb03e36c00418b12e2ce807d12
Miloš Mozetić <milos.mozetic@htecgroup.com>
2023-03-21 09:06:59 +0000

    EXSWHTEC-282 - Refactor channel descriptor tests

--- catch/unit/channelDescriptor/channel_descriptor.cc
+++ catch/unit/channelDescriptor/channel_descriptor.cc
@@ -17,187 +17,38 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <hip_test_common.hh>
-
-static void CompareChannelDescriptors(const hipChannelFormatDesc& lchannel_desc, const hipChannelFormatDesc& rchannel_desc) {
-  REQUIRE(lchannel_desc.x == rchannel_desc.x);
-  REQUIRE(lchannel_desc.y == rchannel_desc.y);
-  REQUIRE(lchannel_desc.z == rchannel_desc.z);
-  REQUIRE(lchannel_desc.w == rchannel_desc.w);
-  REQUIRE(lchannel_desc.f == rchannel_desc.f);
-}
+#include "channel_descriptor_common.hh"
 
 TEMPLATE_TEST_CASE("Unit_ChannelDescriptor_Positive_Basic_1D", "", char, unsigned char, uchar1,
                    signed char, char1, unsigned short, ushort1, signed short, short1,
                    unsigned int, uint1, signed int, int1, float, float1, unsigned long, ulong1,
                    signed long, long1) {
-  hipChannelFormatDesc channel_desc{};
-  int size{};
-  hipChannelFormatKind kind{};
-
-  if (std::is_same<char, TestType>::value) {
-    size = static_cast<int>(sizeof(char) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<unsigned char, TestType>::value || std::is_same<uchar1, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned char) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<signed char, TestType>::value || std::is_same<char1, TestType>::value) {
-    size = static_cast<int>(sizeof(signed char) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<unsigned short, TestType>::value || std::is_same<ushort1, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned short) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<signed short, TestType>::value || std::is_same<short1, TestType>::value) {
-    size = static_cast<int>(sizeof(signed short) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<unsigned int, TestType>::value || std::is_same<uint1, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned int) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<signed int, TestType>::value || std::is_same<int1, TestType>::value) {
-    size = static_cast<int>(sizeof(signed int) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<float, TestType>::value || std::is_same<float1, TestType>::value) {
-    size = static_cast<int>(sizeof(float) * 8);
-    kind = hipChannelFormatKindFloat;
-  } else if (std::is_same<unsigned long, TestType>::value || std::is_same<ulong1, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned long) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<signed long, TestType>::value || std::is_same<long1, TestType>::value) {
-    size = static_cast<int>(sizeof(signed long) * 8);
-    kind = hipChannelFormatKindSigned;
-  }
-
-  const hipChannelFormatDesc referent_channel_desc{size, 0, 0, 0, kind};
-  channel_desc = hipCreateChannelDesc<TestType>();
-  CompareChannelDescriptors(channel_desc, referent_channel_desc);
+  ChannelDescriptorTest1D<TestType> channel_desc_test;
+  channel_desc_test.Run();
 }
 
 TEMPLATE_TEST_CASE("Unit_ChannelDescriptor_Positive_Basic_2D", "", uchar2, char2, ushort2, short2,
                    uint2, int2, float2, ulong2, long2) {
-  hipChannelFormatDesc channel_desc{};
-  int size{};
-  hipChannelFormatKind kind{};
-
-  if (std::is_same<uchar2, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned char) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<char2, TestType>::value) {
-    size = static_cast<int>(sizeof(signed char) * 8);
-    kind = hipChannelFormatKindSigned;  
-  } else if (std::is_same<ushort2, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned short) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<short2, TestType>::value) {
-    size = static_cast<int>(sizeof(signed short) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<uint2, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned int) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<int2, TestType>::value) {
-    size = static_cast<int>(sizeof(signed int) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<float2, TestType>::value) {
-    size = static_cast<int>(sizeof(float) * 8);
-    kind = hipChannelFormatKindFloat;
-  } else if (std::is_same<ulong2, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned long) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<long2, TestType>::value) {
-    size = static_cast<int>(sizeof(signed long) * 8);
-    kind = hipChannelFormatKindSigned;
-  }
-
-  const hipChannelFormatDesc referent_channel_desc{size, size, 0, 0, kind};
-  channel_desc = hipCreateChannelDesc<TestType>();
-  CompareChannelDescriptors(channel_desc, referent_channel_desc);
+  ChannelDescriptorTest2D<TestType> channel_desc_test;
+  channel_desc_test.Run();
 }
 
 #ifndef __GNUC__
 TEMPLATE_TEST_CASE("Unit_ChannelDescriptor_Positive_Basic_3D", "", uchar3, char3, ushort3, short3,
                    uint3, int3, float3, ulong3, long3) {
-  hipChannelFormatDesc channel_desc{};
-  int size{};
-  hipChannelFormatKind kind{};
-
-  if (std::is_same<uchar3, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned char) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<char3, TestType>::value) {
-    size = static_cast<int>(sizeof(signed char) * 8);
-    kind = hipChannelFormatKindSigned;  
-  } else if (std::is_same<ushort3, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned short) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<short3, TestType>::value) {
-    size = static_cast<int>(sizeof(signed short) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<uint3, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned int) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<int3, TestType>::value) {
-    size = static_cast<int>(sizeof(signed int) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<float3, TestType>::value) {
-    size = static_cast<int>(sizeof(float) * 8);
-    kind = hipChannelFormatKindFloat;
-  } else if (std::is_same<ulong3, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned long) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<long3, TestType>::value) {
-    size = static_cast<int>(sizeof(signed long) * 8);
-    kind = hipChannelFormatKindSigned;
-  }
-
-  const hipChannelFormatDesc referent_channel_desc{size, size, size, 0, kind};
-  channel_desc = hipCreateChannelDesc<TestType>();
-  CompareChannelDescriptors(channel_desc, referent_channel_desc);
+  ChannelDescriptorTest3D<TestType> channel_desc_test;
+  channel_desc_test.Run();
 }
 #endif
 
 TEMPLATE_TEST_CASE("Unit_ChannelDescriptor_Positive_Basic_4D", "", uchar4, char4, ushort4, short4,
                    uint4, int4, float4, ulong4, long4) {
-  hipChannelFormatDesc channel_desc{};
-  int size{};
-  hipChannelFormatKind kind{};
-
-  if (std::is_same<uchar4, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned char) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<char4, TestType>::value) {
-    size = static_cast<int>(sizeof(signed char) * 8);
-    kind = hipChannelFormatKindSigned;  
-  } else if (std::is_same<ushort4, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned short) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<short4, TestType>::value) {
-    size = static_cast<int>(sizeof(signed short) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<uint4, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned int) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<int4, TestType>::value) {
-    size = static_cast<int>(sizeof(signed int) * 8);
-    kind = hipChannelFormatKindSigned;
-  } else if (std::is_same<float4, TestType>::value) {
-    size = static_cast<int>(sizeof(float) * 8);
-    kind = hipChannelFormatKindFloat;
-  } else if (std::is_same<ulong4, TestType>::value) {
-    size = static_cast<int>(sizeof(unsigned long) * 8);
-    kind = hipChannelFormatKindUnsigned;
-  } else if (std::is_same<long4, TestType>::value) {
-    size = static_cast<int>(sizeof(signed long) * 8);
-    kind = hipChannelFormatKindSigned;
-  }
-
-  const hipChannelFormatDesc referent_channel_desc{size, size, size, size, kind};
-  channel_desc = hipCreateChannelDesc<TestType>();
-  CompareChannelDescriptors(channel_desc, referent_channel_desc);
+  ChannelDescriptorTest4D<TestType> channel_desc_test;
+  channel_desc_test.Run();
 }
 
 TEMPLATE_TEST_CASE("Unit_ChannelDescriptor_Positive_FormatNone", "",short, int, long, long long,
                    signed long long, unsigned long long) {
-  hipChannelFormatDesc channel_desc{};
-  const hipChannelFormatDesc referent_channel_desc{0, 0, 0, 0, hipChannelFormatKindNone};
-  channel_desc = hipCreateChannelDesc<TestType>();
-  CompareChannelDescriptors(channel_desc, referent_channel_desc);
+  ChannelDescriptorTestNone<TestType> channel_desc_test;
+  channel_desc_test.Run();
 }
---
+++ catch/unit/channelDescriptor/channel_descriptor_common.hh
@@ -0,0 +1,223 @@
+/*
+Copyright (c) 2021 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+
+enum class ChannelDimension {
+  OneDim,
+  TwoDim,
+  ThreeDim,
+  FourDim
+};
+
+template<typename T> class ChannelDescriptorTestShell { 
+ protected:
+  int size;
+  hipChannelFormatKind kind;
+  ChannelDimension dimension;
+  virtual void SetSizeAndKind() = 0;
+
+ public:
+  void Run() {
+    hipChannelFormatDesc channel_desc{};
+    SetSizeAndKind();
+    hipChannelFormatDesc referent_channel_desc{0, 0, 0, 0, kind};
+    switch (dimension) {
+      case ChannelDimension::FourDim:
+        referent_channel_desc.w = size;
+      case ChannelDimension::ThreeDim:
+        referent_channel_desc.z = size;
+      case ChannelDimension::TwoDim:
+        referent_channel_desc.y = size;
+      default:
+        referent_channel_desc.x = size;
+    }
+    channel_desc = hipCreateChannelDesc<T>();
+    REQUIRE(channel_desc.x == referent_channel_desc.x);
+    REQUIRE(channel_desc.y == referent_channel_desc.y);
+    REQUIRE(channel_desc.z == referent_channel_desc.z);
+    REQUIRE(channel_desc.w == referent_channel_desc.w);
+    REQUIRE(channel_desc.f == referent_channel_desc.f);
+  }
+
+  ChannelDescriptorTestShell(const ChannelDimension dimension): size(0), kind(hipChannelFormatKindNone), dimension(dimension) {}
+  ChannelDescriptorTestShell(const ChannelDescriptorTestShell&) = delete;
+  ChannelDescriptorTestShell(ChannelDescriptorTestShell&&) = delete;
+};
+
+template<typename T> class ChannelDescriptorTest1D : public ChannelDescriptorTestShell<T> {
+ public:
+  ChannelDescriptorTest1D() : ChannelDescriptorTestShell<T>(ChannelDimension::OneDim) {}
+
+ protected:
+  void SetSizeAndKind() {
+    if (std::is_same<char, T>::value) {
+      this->size = static_cast<int>(sizeof(char) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<unsigned char, T>::value || std::is_same<uchar1, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned char) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<signed char, T>::value || std::is_same<char1, T>::value) {
+      this->size = static_cast<int>(sizeof(signed char) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<unsigned short, T>::value || std::is_same<ushort1, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned short) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<signed short, T>::value || std::is_same<short1, T>::value) {
+      this->size = static_cast<int>(sizeof(signed short) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<unsigned int, T>::value || std::is_same<uint1, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned int) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<signed int, T>::value || std::is_same<int1, T>::value) {
+      this->size = static_cast<int>(sizeof(signed int) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<float, T>::value || std::is_same<float1, T>::value) {
+      this->size = static_cast<int>(sizeof(float) * 8);
+      this->kind = hipChannelFormatKindFloat;
+    } else if (std::is_same<unsigned long, T>::value || std::is_same<ulong1, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned long) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<signed long, T>::value || std::is_same<long1, T>::value) {
+      this->size = static_cast<int>(sizeof(signed long) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    }
+  }
+};
+
+template<typename T> class ChannelDescriptorTest2D : public ChannelDescriptorTestShell<T> {
+ public:
+  ChannelDescriptorTest2D() : ChannelDescriptorTestShell<T>(ChannelDimension::TwoDim) {}
+
+ protected:
+  void SetSizeAndKind() {
+    if (std::is_same<uchar2, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned char) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<char2, T>::value) {
+      this->size = static_cast<int>(sizeof(signed char) * 8);
+      this->kind = hipChannelFormatKindSigned;  
+    } else if (std::is_same<ushort2, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned short) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<short2, T>::value) {
+      this->size = static_cast<int>(sizeof(signed short) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<uint2, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned int) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<int2, T>::value) {
+      this->size = static_cast<int>(sizeof(signed int) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<float2, T>::value) {
+      this->size = static_cast<int>(sizeof(float) * 8);
+      this->kind = hipChannelFormatKindFloat;
+    } else if (std::is_same<ulong2, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned long) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<long2, T>::value) {
+      this->size = static_cast<int>(sizeof(signed long) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    }
+  }
+};
+
+#ifndef __GNUC__
+template<typename T> class ChannelDescriptorTest3D : public ChannelDescriptorTestShell<T> {
+ public:
+  ChannelDescriptorTest3D() : ChannelDescriptorTestShell<T>(ChannelDimension::ThreeDim) {}
+
+ protected:
+  void SetSizeAndKind() {
+    if (std::is_same<uchar3, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned char) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<char3, T>::value) {
+      this->size = static_cast<int>(sizeof(signed char) * 8);
+      this->kind = hipChannelFormatKindSigned;  
+    } else if (std::is_same<ushort3, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned short) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<short3, T>::value) {
+      this->size = static_cast<int>(sizeof(signed short) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<uint3, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned int) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<int3, T>::value) {
+      this->size = static_cast<int>(sizeof(signed int) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<float3, T>::value) {
+      this->size = static_cast<int>(sizeof(float) * 8);
+      this->kind = hipChannelFormatKindFloat;
+    } else if (std::is_same<ulong3, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned long) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<long3, T>::value) {
+      this->size = static_cast<int>(sizeof(signed long) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    }
+  }
+};
+#endif
+
+template<typename T> class ChannelDescriptorTest4D : public ChannelDescriptorTestShell<T> {
+ public:
+  ChannelDescriptorTest4D() : ChannelDescriptorTestShell<T>(ChannelDimension::FourDim) {}
+
+ protected:
+  void SetSizeAndKind() {
+    if (std::is_same<uchar4, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned char) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<char4, T>::value) {
+      this->size = static_cast<int>(sizeof(signed char) * 8);
+      this->kind = hipChannelFormatKindSigned;  
+    } else if (std::is_same<ushort4, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned short) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<short4, T>::value) {
+      this->size = static_cast<int>(sizeof(signed short) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<uint4, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned int) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<int4, T>::value) {
+      this->size = static_cast<int>(sizeof(signed int) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    } else if (std::is_same<float4, T>::value) {
+      this->size = static_cast<int>(sizeof(float) * 8);
+      this->kind = hipChannelFormatKindFloat;
+    } else if (std::is_same<ulong4, T>::value) {
+      this->size = static_cast<int>(sizeof(unsigned long) * 8);
+      this->kind = hipChannelFormatKindUnsigned;
+    } else if (std::is_same<long4, T>::value) {
+      this->size = static_cast<int>(sizeof(signed long) * 8);
+      this->kind = hipChannelFormatKindSigned;
+    }
+  }
+};
+
+template<typename T> class ChannelDescriptorTestNone : public ChannelDescriptorTestShell<T> {
+ public:
+  ChannelDescriptorTestNone() : ChannelDescriptorTestShell<T>(ChannelDimension::OneDim) {}
+
+ protected:
+  void SetSizeAndKind() {}
+};

873bb1a1824bd6d7a2accfeb2fca546d4e77cec2
Dino Music <dino.music@htecgroup.com>
2023-03-22 10:27:05 +0100

    Implement math test harness for N arguments

--- catch/unit/math/math_common.hh
+++ catch/unit/math/math_common.hh
@@ -24,44 +24,70 @@ THE SOFTWARE.
 #include <hip_test_common.hh>
 #include <resource_guards.hh>
 
-template <typename T, typename RT, typename V>
-void MathTest(void (*kernel)(T*, size_t, T*), RT (*ref_func)(RT), size_t num_args, T* xs,
-              V validator, size_t grid_dim, size_t block_dim) {
-  LinearAllocGuard<T> xs_dev{LinearAllocs::hipMalloc, num_args * sizeof(T)};
-  HIP_CHECK(hipMemcpy(xs_dev.ptr(), xs, num_args * sizeof(T), hipMemcpyHostToDevice));
+#define MATH_SINGLE_ARG_KERNEL_DEF(func_name)                                                      \
+  template <typename T>                                                                            \
+  __global__ void func_name##_kernel(T* const ys, const size_t num_xs, T* const xs) {              \
+    const auto tid = cg::this_grid().thread_rank();                                                \
+                                                                                                   \
+    if (tid < num_xs) {                                                                            \
+      if constexpr (std::is_same_v<float, T>) {                                                    \
+        ys[tid] = func_name##f(xs[tid]);                                                           \
+      } else if constexpr (std::is_same_v<double, T>) {                                            \
+        ys[tid] = func_name(xs[tid]);                                                              \
+      }                                                                                            \
+    }                                                                                              \
+  }
 
-  LinearAllocGuard<T> ys_dev{LinearAllocs::hipMalloc, num_args * sizeof(T)};
-  kernel<<<grid_dim, block_dim>>>(ys_dev.ptr(), num_args, xs_dev.ptr());
+#define MATH_DOUBLE_ARG_KERNEL_DEF(func_name)                                                      \
+  template <typename T>                                                                            \
+  __global__ void func_name##_kernel(T* const ys, const size_t num_xs, T* const x1s,               \
+                                     T* const x2s) {                                               \
+    const auto tid = cg::this_grid().thread_rank();                                                \
+                                                                                                   \
+    if (tid < num_xs) {                                                                            \
+      if constexpr (std::is_same_v<float, T>) {                                                    \
+        ys[tid] = func_name##f(x1s[tid], x2s[tid]);                                                \
+      } else if constexpr (std::is_same_v<double, T>) {                                            \
+        ys[tid] = func_name(x1s[tid], x2s[tid]);                                                   \
+      }                                                                                            \
+    }                                                                                              \
+  }
+
+template <typename T, typename RT, typename Validator, typename... Ts, typename... RTs, size_t... I>
+void MathTestImpl(Validator validator, const size_t grid_dim, const size_t block_dim,
+                  void (*const kernel)(T*, const size_t, Ts*...), RT (*const ref_func)(RTs...),
+                  const size_t num_args, std::index_sequence<I...> is, Ts*... xss) {
+  struct LAWrapper {
+    LAWrapper(const size_t size, T* const init_vals) : la_{LinearAllocs::hipMalloc, size, 0u} {
+      HIP_CHECK(hipMemcpy(ptr(), init_vals, size, hipMemcpyHostToDevice));
+    }
+
+    T* ptr() { return la_.ptr(); }
+
+    LinearAllocGuard<T> la_;
+  };
+
+  std::array xss_dev{LAWrapper(num_args * sizeof(T), xss)...};
+
+  LinearAllocGuard<T> y_dev{LinearAllocs::hipMalloc, num_args * sizeof(T)};
+  kernel<<<grid_dim, block_dim>>>(y_dev.ptr(), num_args, xss_dev[I].ptr()...);
   HIP_CHECK(hipGetLastError());
 
-  std::vector<T> ys(num_args);
-  HIP_CHECK(hipMemcpy(ys.data(), ys_dev.ptr(), num_args * sizeof(T), hipMemcpyDeviceToHost));
+  std::vector<T> y(num_args);
+  HIP_CHECK(hipMemcpy(y.data(), y_dev.ptr(), num_args * sizeof(T), hipMemcpyDeviceToHost));
 
   for (auto i = 0u; i < num_args; ++i) {
-    validator(ys[i], static_cast<T>(ref_func(static_cast<RT>(xs[i]))));
+    validator(y[i], static_cast<T>(ref_func(static_cast<RT>(xss[i])...)));
   }
 }
 
-template <typename T, typename RT, typename V>
-void MathTest(void (*kernel)(T*, size_t, T*, T*), RT (*ref_func)(RT, RT), size_t num_args, T* x1s,
-              T* x2s, V validator, size_t grid_dim, size_t block_dim) {
-  LinearAllocGuard<T> x1s_dev{LinearAllocs::hipMalloc, num_args * sizeof(T)};
-  HIP_CHECK(hipMemcpy(x1s_dev.ptr(), x1s, num_args * sizeof(T), hipMemcpyHostToDevice));
-  LinearAllocGuard<T> x2s_dev{LinearAllocs::hipMalloc, num_args * sizeof(T)};
-  HIP_CHECK(hipMemcpy(x2s_dev.ptr(), x2s, num_args * sizeof(T), hipMemcpyHostToDevice));
-
-  LinearAllocGuard<T> ys_dev{LinearAllocs::hipMalloc, num_args * sizeof(T)};
-  kernel<<<grid_dim, block_dim>>>(ys_dev.ptr(), num_args, x1s_dev.ptr(), x2s_dev.ptr());
-  HIP_CHECK(hipGetLastError());
-
-  std::vector<T> ys(num_args);
-  HIP_CHECK(hipMemcpy(ys.data(), ys_dev.ptr(), num_args * sizeof(T), hipMemcpyDeviceToHost));
-
-  for (auto i = 0u; i < num_args; ++i) {
-    validator(ys[i], static_cast<T>(ref_func(static_cast<RT>(x1s[i]), static_cast<RT>(x2s[i]))));
-  }
+template <typename T, typename RT, typename Validator, typename... Ts, typename... RTs>
+void MathTest(Validator validator, const size_t grid_dims, const size_t block_dims,
+              void (*const kernel)(T*, const size_t, Ts*...), RT (*const ref_func)(RTs...),
+              const size_t num_args, Ts*... xss) {
+  MathTestImpl(validator, grid_dims, block_dims, kernel, ref_func, num_args,
+               std::index_sequence_for<Ts...>{}, xss...);
 }
-
 
 struct ULPValidator {
   template <typename T> void operator()(const T actual_val, const T ref_val) const {

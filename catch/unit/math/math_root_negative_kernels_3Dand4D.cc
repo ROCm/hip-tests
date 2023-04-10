b2e5e76f6937a3d34c90b42fa04fb26752d9e142
Milos Mozetic <milos.mozetic@htecgroup.com>
2023-04-10 10:26:47 +0200

    EXSWHTEC-284 - Split negative tests into two files, as NVCC has 100 error limit that cannot be changed

--- catch/unit/math/math_root_negative_kernels_3Dand4D.cc
+++ catch/unit/math/math_root_negative_kernels_3Dand4D.cc
@@ -25,78 +25,6 @@ class Dummy {
   __device__ ~Dummy() {}
 };
 
-#define NEGATIVE_KERNELS_SHELL_ONE_ARG(func_name)                                                  \
-  __global__ void func_name##_kernel_v1(double* x) { double result = func_name(x); }               \
-  __global__ void func_name##_kernel_v2(Dummy x) { double result = func_name(x); }                 \
-  __global__ void func_name##f_kernel_v1(float* x) { float result = func_name##f(x); }             \
-  __global__ void func_name##f_kernel_v2(Dummy x) { float result = func_name##f(x); }
-
-#define NEGATIVE_KERNELS_SHELL_TWO_ARGS(func_name)                                                 \
-  __global__ void func_name##_kernel_v1(double* x, double y) { double result = func_name(x, y); }  \
-  __global__ void func_name##_kernel_v2(double x, double* y) { double result = func_name(x, y); }  \
-  __global__ void func_name##_kernel_v3(Dummy x, double y) { double result = func_name(x, y); }    \
-  __global__ void func_name##_kernel_v4(double x, Dummy y) { double result = func_name(x, y); }    \
-  __global__ void func_name##f_kernel_v1(float* x, float y) { float result = func_name##f(x, y); } \
-  __global__ void func_name##f_kernel_v2(float x, float* y) { float result = func_name##f(x, y); } \
-  __global__ void func_name##f_kernel_v3(Dummy x, float y) { float result = func_name##f(x, y); }  \
-  __global__ void func_name##f_kernel_v4(float x, Dummy y) { float result = func_name##f(x, y); }
-
-#define NEGATIVE_KERNELS_SHELL_ARRAY_ARG(func_name)                                                \
-  __global__ void func_name##_kernel_v1(int* dim, const double* a) {                               \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v2(Dummy dim, const double* a) {                              \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v3(int dim, const int* a) {                                   \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v4(int dim, const char* a) {                                  \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v5(int dim, const short* a) {                                 \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v6(int dim, const long* a) {                                  \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v7(int dim, const long long* a) {                             \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v8(int dim, const float* a) {                                 \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##_kernel_v9(int dim, const Dummy* a) {                                 \
-    double result = func_name(dim, a);                                                             \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v1(int* dim, const float* a) {                               \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v2(Dummy dim, const float* a) {                              \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v3(int dim, const int* a) {                                  \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v4(int dim, const char* a) {                                 \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v5(int dim, const short* a) {                                \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v6(int dim, const long* a) {                                 \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v7(int dim, const long long* a) {                            \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v8(int dim, const double* a) {                               \
-    float result = func_name##f(dim, a);                                                           \
-  }                                                                                                \
-  __global__ void func_name##f_kernel_v9(int dim, const Dummy* a) {                                \
-    double result = func_name##f(dim, a);                                                          \
-  }
-
 #define NEGATIVE_KERNELS_SHELL_THREE_ARGS(func_name)                                               \
   __global__ void func_name##_kernel_v1(double* x, double y, double z) {                           \
     double result = func_name(x, y, z);                                                            \
@@ -185,15 +113,7 @@ class Dummy {
     float result = func_name##f(x, y, z, w);                                                       \
   }
 
-NEGATIVE_KERNELS_SHELL_ONE_ARG(sqrt)
-NEGATIVE_KERNELS_SHELL_ONE_ARG(rsqrt)
-NEGATIVE_KERNELS_SHELL_ONE_ARG(cbrt)
-NEGATIVE_KERNELS_SHELL_ONE_ARG(rcbrt)
-NEGATIVE_KERNELS_SHELL_TWO_ARGS(hypot)
-NEGATIVE_KERNELS_SHELL_TWO_ARGS(rhypot)
 NEGATIVE_KERNELS_SHELL_THREE_ARGS(norm3d)
 NEGATIVE_KERNELS_SHELL_THREE_ARGS(rnorm3d)
 NEGATIVE_KERNELS_SHELL_FOUR_ARGS(norm4d)
 NEGATIVE_KERNELS_SHELL_FOUR_ARGS(rnorm4d)
-NEGATIVE_KERNELS_SHELL_ARRAY_ARG(norm)
-NEGATIVE_KERNELS_SHELL_ARRAY_ARG(rnorm)

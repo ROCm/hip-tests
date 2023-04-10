0d3bb345efc73a60fc2c14918035a40e2e3275a6
Dino Musić <dino.music@htecgroup.com>
2023-04-10 13:33:33 +0000

    EXSWHTEC-287 - Implement negative RTC tests

--- catch/unit/math/trig_funcs.cc
+++ catch/unit/math/trig_funcs.cc
@@ -19,6 +19,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#include "trig_negative_kernels_rtc.hh"
+
 #include "unary_common.hh"
 #include "binary_common.hh"
 
@@ -26,34 +28,49 @@ THE SOFTWARE.
 
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(sin, std::sin, 2, 2);
+TEST_CASE("Unit_Device_sin_sinf_Negative_RTC") { NegativeTestRTCWrapper<4>(kSin); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(cos, std::cos, 2, 2)
+TEST_CASE("Unit_Device_cos_cosf_Negative_RTC") { NegativeTestRTCWrapper<4>(kCos); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(tan, std::tan, 4, 2)
+TEST_CASE("Unit_Device_tan_tanf_Negative_RTC") { NegativeTestRTCWrapper<4>(kTan); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(asin, std::asin, 2, 2)
+TEST_CASE("Unit_Device_asin_asinf_Negative_RTC") { NegativeTestRTCWrapper<4>(kAsin); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(acos, std::acos, 2, 2)
+TEST_CASE("Unit_Device_acos_acosf_Negative_RTC") { NegativeTestRTCWrapper<4>(kAcos); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(atan, std::atan, 2, 2)
+TEST_CASE("Unit_Device_atan_atanf_Negative_RTC") { NegativeTestRTCWrapper<4>(kAtan); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(sinh, std::sinh, 3, 2)
+TEST_CASE("Unit_Device_sinh_sinhf_Negative_RTC") { NegativeTestRTCWrapper<4>(kSinh); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(cosh, std::cosh, 2, 1)
+TEST_CASE("Unit_Device_cosh_coshf_Negative_RTC") { NegativeTestRTCWrapper<4>(kCosh); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(tanh, std::tanh, 2, 1)
+TEST_CASE("Unit_Device_tanh_tanhf_Negative_RTC") { NegativeTestRTCWrapper<4>(kTanh); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(asinh, std::asinh, 3, 2)
+TEST_CASE("Unit_Device_asinh_asinhf_Negative_RTC") { NegativeTestRTCWrapper<4>(kAsinh); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(acosh, std::acosh, 4, 2)
+TEST_CASE("Unit_Device_acosh_acoshf_Negative_RTC") { NegativeTestRTCWrapper<4>(kAcosh); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(atanh, std::atanh, 3, 2)
+TEST_CASE("Unit_Device_atanh_atanhf_Negative_RTC") { NegativeTestRTCWrapper<4>(kAtanh); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(sinpi, boost::math::sin_pi, 2, 2);
+TEST_CASE("Unit_Device_sinpi_sinpif_Negative_RTC") { NegativeTestRTCWrapper<4>(kSinpi); }
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(cospi, boost::math::cos_pi, 2, 2);
+TEST_CASE("Unit_Device_cospi_cospif_Negative_RTC") { NegativeTestRTCWrapper<4>(kCospi); }
 
 MATH_BINARY_WITHIN_ULP_TEST_DEF(atan2, std::atan2, 3, 2);
+TEST_CASE("Unit_Device_atan2_atan2f_Negative_RTC") { NegativeTestRTCWrapper<8>(kAtan2); }
 
 
 template <typename T>
@@ -83,6 +100,8 @@ TEST_CASE("Unit_Device_sincos_Accuracy_Positive - double") {
       PairValidatorBuilderFactory<double>(ULPValidatorBuilderFactory<double>(2));
   UnaryDoublePrecisionTest(sincos_kernel<double>, sincos<long double>, validator_builder);
 }
+
+TEST_CASE("Unit_Device_sincos_sincosf_Negative_RTC") { NegativeTestRTCWrapper<36>(kSincos); }
 
 
 template <typename T>
@@ -114,3 +133,5 @@ TEST_CASE("Unit_Device_sincospi_Accuracy_Positive - double") {
       PairValidatorBuilderFactory<double>(ULPValidatorBuilderFactory<double>(2));
   UnaryDoublePrecisionTest(sincospi_kernel<double>, sincospi<long double>, validator_builder);
 }
+
+TEST_CASE("Unit_Device_sincospi_sincospif_Negative_RTC") { NegativeTestRTCWrapper<36>(kSincospi); }

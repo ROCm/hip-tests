30fa116a0896122b1abdb8e20538e54f1658c02a
Dino Musić <dino.music@htecgroup.com>
2023-04-10 16:04:52 +0000

    EXSWHTEC-290 - Implement negative RTC tests

--- catch/unit/math/misc_funcs.cc
+++ catch/unit/math/misc_funcs.cc
@@ -19,21 +19,31 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#include "misc_negative_kernels_rtc.hh"
+
 #include "unary_common.hh"
 #include "binary_common.hh"
 #include "ternary_common.hh"
 
 MATH_UNARY_WITHIN_ULP_TEST_DEF(fabs, std::fabs, 0, 0)
+TEST_CASE("Unit_Device_fabs_fabsf_Negative_RTC") { NegativeTestRTCWrapper<4>(kFabs); }
 
 MATH_BINARY_WITHIN_ULP_TEST_DEF(copysign, std::copysign, 0, 0)
+TEST_CASE("Unit_Device_copysign_copysignf_Negative_RTC") { NegativeTestRTCWrapper<8>(kCopySign); }
 
 MATH_BINARY_WITHIN_ULP_TEST_DEF(fmax, std::fmax, 0, 0)
+TEST_CASE("Unit_Device_fmax_fmaxf_Negative_RTC") { NegativeTestRTCWrapper<8>(kFmax); }
 
 MATH_BINARY_WITHIN_ULP_TEST_DEF(fmin, std::fmin, 0, 0)
+TEST_CASE("Unit_Device_fmin_fminf_Negative_RTC") { NegativeTestRTCWrapper<8>(kFmin); }
 
 MATH_BINARY_WITHIN_ULP_TEST_DEF(nextafter, std::nextafter, 0, 0)
+TEST_CASE("Unit_Device_nextafter_nextafterf_Negative_RTC") {
+  NegativeTestRTCWrapper<8>(kNextAfter);
+}
 
 MATH_TERNARY_WITHIN_ULP_TEST_DEF(fma, std::fma, 0, 0)
+TEST_CASE("Unit_Device_fma_fmaf_Negative_RTC") { NegativeTestRTCWrapper<12>(kFma); }
 
 __global__ void fdividef_kernel(float* const ys, const size_t num_xs, float* const x1s,
                                 float* const x2s) {
@@ -49,6 +59,8 @@ TEST_CASE("Unit_Device_fdividef_Accuracy_Positive") {
   double (*ref)(double, double) = [](double x1, double x2) { return x1 / x2; };
   BinaryFloatingPointTest(fdividef_kernel, ref, ULPValidatorBuilderFactory<float>(0));
 }
+
+TEST_CASE("Unit_Device_fdividef_Negative_RTC") { NegativeTestRTCWrapper<4>(kFdividef); }
 
 #define MATH_BOOL_RETURNING_FUNCTION_TEST_DEF(kern_name, ref_func)                                 \
   template <typename T>                                                                            \
@@ -72,9 +84,13 @@ TEST_CASE("Unit_Device_fdividef_Accuracy_Positive") {
   }
 
 MATH_BOOL_RETURNING_FUNCTION_TEST_DEF(isfinite, std::isfinite)
+TEST_CASE("Unit_Device_isfinite_Negative_RTC") { NegativeTestRTCWrapper<4>(kIsFinite); }
 
 MATH_BOOL_RETURNING_FUNCTION_TEST_DEF(isinf, std::isinf)
+TEST_CASE("Unit_Device_isinf_Negative_RTC") { NegativeTestRTCWrapper<4>(kIsInf); }
 
 MATH_BOOL_RETURNING_FUNCTION_TEST_DEF(isnan, std::isnan)
+TEST_CASE("Unit_Device_isnan_Negative_RTC") { NegativeTestRTCWrapper<4>(kIsNan); }
 
 MATH_BOOL_RETURNING_FUNCTION_TEST_DEF(signbit, std::signbit)
+TEST_CASE("Unit_Device_signbit_Negative_RTC") { NegativeTestRTCWrapper<4>(kSignBit); }
--- catch/unit/math/misc_negative_kernels.cc
+++ catch/unit/math/misc_negative_kernels.cc
@@ -42,13 +42,13 @@ class Dummy {
 
 #define MISC_BINARY_NEGATIVE_KERNELS(func_name)                                                    \
   __global__ void func_name##f_kernel_v1(float* x, float y) { float result = func_name##f(x, y); } \
-  __global__ void func_name##f_kernel_v2(Dummy x, float y) { float result = func_name##f(x); }     \
-  __global__ void func_name##f_kernel_v1(float x, float* y) { float result = func_name##f(x); }    \
-  __global__ void func_name##f_kernel_v2(float x, Dummy y) { float result = func_name##f(x); }     \
+  __global__ void func_name##f_kernel_v2(Dummy x, float y) { float result = func_name##f(x, y); }  \
+  __global__ void func_name##f_kernel_v3(float x, float* y) { float result = func_name##f(x, y); } \
+  __global__ void func_name##f_kernel_v4(float x, Dummy y) { float result = func_name##f(x, y); }  \
   __global__ void func_name##_kernel_v1(double* x, double y) { double result = func_name(x, y); }  \
-  __global__ void func_name##_kernel_v2(Dummy x, double y) { double result = func_name(x); }       \
-  __global__ void func_name##_kernel_v1(double x, double* y) { double result = func_name(x); }     \
-  __global__ void func_name##_kernel_v2(double x, Dummy y) { double result = func_name(x); }
+  __global__ void func_name##_kernel_v2(Dummy x, double y) { double result = func_name(x, y); }    \
+  __global__ void func_name##_kernel_v3(double x, double* y) { double result = func_name(x, y); }  \
+  __global__ void func_name##_kernel_v4(double x, Dummy y) { double result = func_name(x, y); }
 
 /*Expecting 4 errors*/
 MISC_UNARY_NEGATIVE_KERNELS(fabs)
@@ -63,11 +63,25 @@ MISC_BINARY_NEGATIVE_KERNELS(fma)
 /*Expecting 4 errors*/
 __global__ void fdividef_kernel_v1(float* x, float y) { float result = fdividef(x, y); }
 __global__ void fdividef_kernel_v2(Dummy x, float y) { float result = fdivide(x); }
-__global__ void fdividef_kernel_v1(float x, float* y) { float result = fdivide(x); }
-__global__ void fdividef_kernel_v2(float x, Dummy y) { float result = fdivide(x); }
+__global__ void fdividef_kernel_v3(float x, float* y) { float result = fdivide(x); }
+__global__ void fdividef_kernel_v4(float x, Dummy y) { float result = fdivide(x); }
 
 /*Expecting 4 errors per macro invocation - 16 total*/
 MISC_UNARY_BOOL_RET_NEGATIVE_KERNELS(isfinite)
 MISC_UNARY_BOOL_RET_NEGATIVE_KERNELS(isinf)
 MISC_UNARY_BOOL_RET_NEGATIVE_KERNELS(isnan)
 MISC_UNARY_BOOL_RET_NEGATIVE_KERNELS(signbit)
+
+/*Expecting 12 errors*/
+__global__ void fmaf_kernel_v1(float* x, float y, float z) { float result = fmaf(x, y, z); }
+__global__ void fmaf_kernel_v2(Dummy x, float y, float z) { float result = fmaf(x, y, z); }
+__global__ void fmaf_kernel_v3(float x, float* y, float z) { float result = fmaf(x, y, z); }
+__global__ void fmaf_kernel_v4(float x, Dummy y, float z) { float result = fmaf(x, y, z); }
+__global__ void fmaf_kernel_v5(float x, float y, float* z) { float result = fmaf(x, y, z); }
+__global__ void fmaf_kernel_v6(float x, float y, Dummy z) { float result = fmaf(x, y, z); }
+__global__ void fma_kernel_v1(double* x, double y, double z) { double result = fmaf(x, y, z); }
+__global__ void fma_kernel_v2(Dummy x, double y, double z) { double result = fmaf(x, y, z); }
+__global__ void fma_kernel_v3(double x, double* y, double z) { double result = fmaf(x, y, z); }
+__global__ void fma_kernel_v4(double x, Dummy y, double z) { double result = fmaf(x, y, z); }
+__global__ void fma_kernel_v5(double x, double y, double* z) { double result = fmaf(x, y, z); }
+__global__ void fma_kernel_v6(double x, double y, Dummy z) { double result = fmaf(x, y, z); }

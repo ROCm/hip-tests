34981d67af667dad91f014095039863ee071d1ca
Milos Mozetic <milos.mozetic@htecgroup.com>
2023-05-23 14:04:34 +0200

    EXSWHTEC-263 - Address comments from the defect EXSWHTEC-278

--- catch/unit/atomics/atomicXor_negative_kernels.cc
+++ catch/unit/atomics/atomicXor_negative_kernels.cc
@@ -60,6 +60,14 @@ __global__ void atomicXor_int_v8(long long* address, int* result) {
   *result = atomicXor(address, 1234);
 }
 
+__global__ void atomicXor_int_v9(float* address, int* result) {
+  *result = atomicXor(address, 1234);
+}
+
+__global__ void atomicXor_int_v10(double* address, int* result) {
+  *result = atomicXor(address, 1234);
+}
+
 /* unsigned int atomicXor(unsigned int* address, unsigned int val) */
 __global__ void atomicXor_uint_v1(unsigned int* address, unsigned int* result) {
   *result = atomicXor(&address, 1234);
@@ -90,6 +98,14 @@ __global__ void atomicXor_uint_v7(long* address, unsigned int* result) {
 }
 
 __global__ void atomicXor_uint_v8(long long* address, unsigned int* result) {
+  *result = atomicXor(address, 1234);
+}
+
+__global__ void atomicXor_int_v9(float* address, unsigned int* result) {
+  *result = atomicXor(address, 1234);
+}
+
+__global__ void atomicXor_int_v10(double* address, unsigned int* result) {
   *result = atomicXor(address, 1234);
 }
 
@@ -126,6 +142,14 @@ __global__ void atomicXor_ulong_v8(long long* address, unsigned long* result) {
   *result = atomicXor(address, 1234);
 }
 
+__global__ void atomicXor_ulong_v9(float* address, unsigned long* result) {
+  *result = atomicOr(address, 1234);
+}
+
+__global__ void atomicXor_ulong_v10(double* address, unsigned long* result) {
+  *result = atomicOr(address, 1234);
+}
+
 /* atomicXor(unsigned long long* address, unsigned long long val) */
 __global__ void atomicXor_ulonglong_v1(unsigned long long* address, unsigned long long* result) {
   *result = atomicXor(&address, 1234);
@@ -159,68 +183,10 @@ __global__ void atomicXor_ulonglong_v8(long long* address, unsigned long long* r
   *result = atomicXor(address, 1234);
 }
 
-/* atomicXor(float* address, float val) */
-__global__ void atomicXor_float_v1(float* address, float* result) {
-  *result = atomicXor(&address, 1234.f);
+__global__ void atomicOr_ulonglong_v9(float* address, unsigned long long* result) {
+  *result = atomicOr(address, 1234);
 }
 
-__global__ void atomicXor_float_v2(float* address, float* result) {
-  *result = atomicXor(address, address);
-}
-
-__global__ void atomicXor_float_v3(float* address, float* result) {
-  *result = atomicXor(1234.f, 1234.f);
-}
-
-__global__ void atomicXor_float_v4(Dummy* address, float* result) {
-  *result = atomicXor(address, 1234.f);
-}
-
-__global__ void atomicXor_float_v5(char* address, float* result) {
-  *result = atomicXor(address, 1234.f);
-}
-
-__global__ void atomicXor_float_v6(short* address, float* result) {
-  *result = atomicXor(address, 1234.f);
-}
-
-__global__ void atomicXor_float_v7(long* address, float* result) {
-  *result = atomicXor(address, 1234.f);
-}
-
-__global__ void atomicXor_float_v8(long long* address, float* result) {
-  *result = atomicXor(address, 1234);
-}
-
-/* atomicXor(double* address, double val) */
-__global__ void atomicXor_double_v1(double* address, double* result) {
-  *result = atomicXor(&address, 1234.0);
-}
-
-__global__ void atomicXor_double_v2(double* address, double* result) {
-  *result = atomicXor(address, address);
-}
-
-__global__ void atomicXor_double_v3(double* address, double* result) {
-  *result = atomicXor(1234.0, 1234.0);
-}
-
-__global__ void atomicXor_double_v4(Dummy* address, double* result) {
-  *result = atomicXor(address, 1234.0);
-}
-
-__global__ void atomicXor_double_v5(char* address, double* result) {
-  *result = atomicXor(address, 1234.0);
-}
-
-__global__ void atomicXor_double_v6(short* address, double* result) {
-  *result = atomicXor(address, 1234.0);
-}
-
-__global__ void atomicXor_double_v7(long* address, double* result) {
-  *result = atomicXor(address, 1234.0);
-}
-
-__global__ void atomicXor_double_v8(long long* address, double* result) {
-  *result = atomicXor(address, 1234.0);
+__global__ void atomicOr_ulonglong_v10(double* address, unsigned long long* result) {
+  *result = atomicOr(address, 1234);
 }

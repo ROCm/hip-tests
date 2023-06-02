c10cceedefff241d989e81d678b033bfcce9e415
Nives Vukovic <nives.vukovic@htecgroup.com>
2023-06-02 16:10:18 +0000

    EXSWHTEC-305 - Fix file with negative rtc kernels

--- catch/unit/math/casting_double_negative_kernels_rtc.hh
+++ catch/unit/math/casting_double_negative_kernels_rtc.hh
@@ -17,39 +17,141 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <hip_test_common.hh>
+#pragma once
 
-class Dummy {
- public:
-  __device__ Dummy() {}
-  __device__ ~Dummy() {}
-};
+/*
+Negative kernels used for the double type casting negative Test Cases that are using RTC.
+*/
 
-#define NEGATIVE_KERNELS_SHELL(T, func_name)                                                       \
-  __global__ void func_name##_kernel_v1(T* result, double* x) { *result = __##func_name(x); }      \
-  __global__ void func_name##_kernel_v2(T* result, Dummy x) { *result = __##func_name(x); }        \
-  __global__ void func_name##_kernel_v3(Dummy* result, double x) { *result = __##func_name(x); }
+static constexpr auto kDouble2Int{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2int_rd_kernel_v1(int* result, double* x) { *result = __double2int_rd(x); }
+  __global__ void double2int_rd_kernel_v2(int* result, Dummy x) { *result = __double2int_rd(x); }
+  __global__ void double2int_rd_kernel_v3(Dummy* result, double x) { *result = __double2int_rd(x); }
+  __global__ void double2int_rn_kernel_v1(int* result, double* x) { *result = __double2int_rn(x); }
+  __global__ void double2int_rn_kernel_v2(int* result, Dummy x) { *result = __double2int_rn(x); }
+  __global__ void double2int_rn_kernel_v3(Dummy* result, double x) { *result = __double2int_rn(x); }
+  __global__ void double2int_ru_kernel_v1(int* result, double* x) { *result = __double2int_ru(x); }
+  __global__ void double2int_ru_kernel_v2(int* result, Dummy x) { *result = __double2int_ru(x); }
+  __global__ void double2int_ru_kernel_v3(Dummy* result, double x) { *result = __double2int_ru(x); }
+  __global__ void double2int_rz_kernel_v1(int* result, double* x) { *result = __double2int_rz(x); }
+  __global__ void double2int_rz_kernel_v2(int* result, Dummy x) { *result = __double2int_rz(x); }
+  __global__ void double2int_rz_kernel_v3(Dummy* result, double x) { *result = __double2int_rz(x); }
+)"};
 
-NEGATIVE_KERNELS_SHELL(int, double2int_rd)
-NEGATIVE_KERNELS_SHELL(int, double2int_rn)
-NEGATIVE_KERNELS_SHELL(int, double2int_ru)
-NEGATIVE_KERNELS_SHELL(int, double2int_rz)
-NEGATIVE_KERNELS_SHELL(unsigned int, double2uint_rd)
-NEGATIVE_KERNELS_SHELL(unsigned int, double2uint_rn)
-NEGATIVE_KERNELS_SHELL(unsigned int, double2uint_ru)
-NEGATIVE_KERNELS_SHELL(unsigned int, double2uint_rz)
-NEGATIVE_KERNELS_SHELL(long long int, double2ll_rd)
-NEGATIVE_KERNELS_SHELL(long long int, double2ll_rn)
-NEGATIVE_KERNELS_SHELL(long long int, double2ll_ru)
-NEGATIVE_KERNELS_SHELL(long long int, double2ll_rz)
-NEGATIVE_KERNELS_SHELL(unsigned long long int, double2ull_rd)
-NEGATIVE_KERNELS_SHELL(unsigned long long int, double2ull_rn)
-NEGATIVE_KERNELS_SHELL(unsigned long long int, double2ull_ru)
-NEGATIVE_KERNELS_SHELL(unsigned long long int, double2ull_rz)
-NEGATIVE_KERNELS_SHELL(float, double2float_rd)
-NEGATIVE_KERNELS_SHELL(float, double2float_rn)
-NEGATIVE_KERNELS_SHELL(float, double2float_ru)
-NEGATIVE_KERNELS_SHELL(float, double2float_rz)
-NEGATIVE_KERNELS_SHELL(int, double2hiint)
-NEGATIVE_KERNELS_SHELL(int, double2loint)
-NEGATIVE_KERNELS_SHELL(long long int, double_as_longlong)
+static constexpr auto kDouble2Uint{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2uint_rd_kernel_v1(unsigned int* result, double* x) { *result = __double2uint_rd(x); }
+  __global__ void double2uint_rd_kernel_v2(unsigned int* result, Dummy x) { *result = __double2uint_rd(x); }
+  __global__ void double2uint_rd_kernel_v3(Dummy* result, double x) { *result = __double2uint_rd(x); }
+  __global__ void double2uint_rn_kernel_v1(unsigned int* result, double* x) { *result = __double2uint_rn(x); }
+  __global__ void double2uint_rn_kernel_v2(unsigned int* result, Dummy x) { *result = __double2uint_rn(x); }
+  __global__ void double2uint_rn_kernel_v3(Dummy* result, double x) { *result = __double2uint_rn(x); }
+  __global__ void double2uint_ru_kernel_v1(unsigned int* result, double* x) { *result = __double2uint_ru(x); }
+  __global__ void double2uint_ru_kernel_v2(unsigned int* result, Dummy x) { *result = __double2uint_ru(x); }
+  __global__ void double2uint_ru_kernel_v3(Dummy* result, double x) { *result = __double2uint_ru(x); }
+  __global__ void double2uint_rz_kernel_v1(unsigned int* result, double* x) { *result = __double2uint_rz(x); }
+  __global__ void double2uint_rz_kernel_v2(unsigned int* result, Dummy x) { *result = __double2uint_rz(x); }
+  __global__ void double2uint_rz_kernel_v3(Dummy* result, double x) { *result = __double2uint_rz(x); }
+)"};
+
+static constexpr auto kDouble2LL{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2ll_rd_kernel_v1(long long int* result, double* x) { *result = __double2ll_rd(x); }
+  __global__ void double2ll_rd_kernel_v2(long long int* result, Dummy x) { *result = __double2ll_rd(x); }
+  __global__ void double2ll_rd_kernel_v3(Dummy* result, double x) { *result = __double2ll_rd(x); }
+  __global__ void double2ll_rn_kernel_v1(long long int* result, double* x) { *result = __double2ll_rn(x); }
+  __global__ void double2ll_rn_kernel_v2(long long int* result, Dummy x) { *result = __double2ll_rn(x); }
+  __global__ void double2ll_rn_kernel_v3(Dummy* result, double x) { *result = __double2ll_rn(x); }
+  __global__ void double2ll_ru_kernel_v1(long long int* result, double* x) { *result = __double2ll_ru(x); }
+  __global__ void double2ll_ru_kernel_v2(long long int* result, Dummy x) { *result = __double2ll_ru(x); }
+  __global__ void double2ll_ru_kernel_v3(Dummy* result, double x) { *result = __double2ll_ru(x); }
+  __global__ void double2ll_rz_kernel_v1(long long int* result, double* x) { *result = __double2ll_rz(x); }
+  __global__ void double2ll_rz_kernel_v2(long long int* result, Dummy x) { *result = __double2ll_rz(x); }
+  __global__ void double2ll_rz_kernel_v3(Dummy* result, double x) { *result = __double2ll_rz(x); }
+)"};
+
+static constexpr auto kDouble2ULL{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2ull_rd_kernel_v1(unsigned long long int* result, double* x) { *result = __double2ull_rd(x); }
+  __global__ void double2ull_rd_kernel_v2(unsigned long long int* result, Dummy x) { *result = __double2ull_rd(x); }
+  __global__ void double2ull_rd_kernel_v3(Dummy* result, double x) { *result = __double2ull_rd(x); }
+  __global__ void double2ull_rn_kernel_v1(unsigned long long int* result, double* x) { *result = __double2ull_rn(x); }
+  __global__ void double2ull_rn_kernel_v2(unsigned long long int* result, Dummy x) { *result = __double2ull_rn(x); }
+  __global__ void double2ull_rn_kernel_v3(Dummy* result, double x) { *result = __double2ull_rn(x); }
+  __global__ void double2ull_ru_kernel_v1(unsigned long long int* result, double* x) { *result = __double2ull_ru(x); }
+  __global__ void double2ull_ru_kernel_v2(unsigned long long int* result, Dummy x) { *result = __double2ull_ru(x); }
+  __global__ void double2ull_ru_kernel_v3(Dummy* result, double x) { *result = __double2ull_ru(x); }
+  __global__ void double2ull_rz_kernel_v1(unsigned long long int* result, double* x) { *result = __double2ull_rz(x); }
+  __global__ void double2ull_rz_kernel_v2(unsigned long long int* result, Dummy x) { *result = __double2ull_rz(x); }
+  __global__ void double2ull_rz_kernel_v3(Dummy* result, double x) { *result = __double2ull_rz(x); }
+)"};
+
+static constexpr auto kDouble2Float{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2float_rd_kernel_v1(float* result, double* x) { *result = __double2float_rd(x); }
+  __global__ void double2float_rd_kernel_v2(float* result, Dummy x) { *result = __double2float_rd(x); }
+  __global__ void double2float_rd_kernel_v3(Dummy* result, double x) { *result = __double2float_rd(x); }
+  __global__ void double2float_rn_kernel_v1(float* result, double* x) { *result = __double2float_rn(x); }
+  __global__ void double2float_rn_kernel_v2(float* result, Dummy x) { *result = __double2float_rn(x); }
+  __global__ void double2float_rn_kernel_v3(Dummy* result, double x) { *result = __double2float_rn(x); }
+  __global__ void double2float_ru_kernel_v1(float* result, double* x) { *result = __double2float_ru(x); }
+  __global__ void double2float_ru_kernel_v2(float* result, Dummy x) { *result = __double2float_ru(x); }
+  __global__ void double2float_ru_kernel_v3(Dummy* result, double x) { *result = __double2float_ru(x); }
+  __global__ void double2float_rz_kernel_v1(float* result, double* x) { *result = __double2float_rz(x); }
+  __global__ void double2float_rz_kernel_v2(float* result, Dummy x) { *result = __double2float_rz(x); }
+  __global__ void double2float_rz_kernel_v3(Dummy* result, double x) { *result = __double2float_rz(x); }
+)"};
+
+static constexpr auto kDouble2Hiint{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2hiint_kernel_v1(int* result, double* x) { *result = __double2hiint(x); }
+  __global__ void double2hiint_kernel_v2(int* result, Dummy x) { *result = __double2hiint(x); }
+  __global__ void double2hiint_kernel_v3(Dummy* result, double x) { *result = __double2hiint(x); }
+)"};
+
+static constexpr auto kDouble2Loint{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double2loint_kernel_v1(int* result, double* x) { *result = __double2loint(x); }
+  __global__ void double2loint_kernel_v2(int* result, Dummy x) { *result = __double2loint(x); }
+  __global__ void double2loint_kernel_v3(Dummy* result, double x) { *result = __double2loint(x); }
+)"};
+
+static constexpr auto kDoubleAsLonglong{R"(
+  class Dummy {
+   public:
+    __device__ Dummy() {}
+    __device__ ~Dummy() {}
+  };
+  __global__ void double_as_longlong_kernel_v1(long long int* result, double* x) { *result = __double_as_longlong(x); }
+  __global__ void double_as_longlong_kernel_v2(long long int* result, Dummy x) { *result = __double_as_longlong(x); }
+  __global__ void double_as_longlong_kernel_v3(Dummy* result, double x) { *result = __double_as_longlong(x); }
+)"};

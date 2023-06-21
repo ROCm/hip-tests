5aca7970605e5bba27bf6b1926ca675733fbe542
ROCm CI Service Account <66695075+rocm-ci@users.noreply.github.com>
2023-06-21 15:59:58 +0530

    SWDEV-385838 - Don't run Unit_HMM_OverSubscriptionTst test on non xnack device. (#271)
    Change-Id: I342affb0cd62ce8fab4b35d6c9e0f1b6fb75bee9
    
    [ROCm/hip-tests commit: 9a40c9a6da8df09facfcaaef543268a5f1bb1674]

--- catch/unit/memory/hipHmmOvrSubscriptionTst.cc
+++ catch/unit/memory/hipHmmOvrSubscriptionTst.cc
@@ -140,6 +140,14 @@ static void DisplayHmmFlgs(int *Signal) {
 }
 
 TEST_CASE("Unit_HMM_OverSubscriptionTst") {
+  hipDeviceProp_t prop;
+  HIP_CHECK(hipGetDeviceProperties(&prop, 0));
+  char *p = nullptr;
+  p = strstr(prop.gcnArchName, "xnack+");
+  if (p == nullptr) {
+    INFO("Skipped due current device is non xnack device.");
+    return;
+  }
   int HmmEnabled = 0;
   // The following Shared Mem is to get Max GPU Mem
   // The size requested is for three ints

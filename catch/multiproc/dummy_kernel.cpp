a553a15ada2abc84310f3fdcd75c6a4694b89a6e
David Salinas <David.Salinas@amd.com>
2023-06-22 16:35:16 +0000

    change HIP header file inclusions to system include

--- catch/multiproc/dummy_kernel.cpp
+++ catch/multiproc/dummy_kernel.cpp
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "hip/hip_runtime.h"
+#include <hip/hip_runtime.h>
 
 extern "C" __global__ void dummy_ker() {
 }

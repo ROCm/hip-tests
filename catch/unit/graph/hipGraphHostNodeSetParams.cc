7d7486a3daa215ee930cf35f4c9dae27054d4b18
Mirza Halilčević <109971222+mirza-halilcevic@users.noreply.github.com>
2023-06-28 06:04:52 +0200

    EXSWHTEC-191 - Implement additional tests for Host Graph Node APIs  (#9)
    - Tidy up hipGraphAddHostNode tests
    - Tidy up hipGraphHostNodeGetParams tests
    - Tidy up hipGraphHostNodeSetParams tests
    - Tidy up hipGraphExecHostNodeSetParams tests.
    - Disable failing test sections on AMD.
    
    [ROCm/hip-tests commit: 9f5bb4219ac7c07b6b97da78dd34d7b4804344d0]

--- catch/unit/graph/hipGraphHostNodeSetParams.cc
+++ catch/unit/graph/hipGraphHostNodeSetParams.cc
@@ -6,24 +6,26 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANNTY OF ANY KIND, EXPRESS OR
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER INN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR INN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
 /**
-Testcase Scenarios of hipGraphHostNodeSetParams API:
+Test Case Scenarios of hipGraphHostNodeSetParams API:
 
 Functional:
 1. Creates graph, Adds HostNode, update hostNode params using hipGraphHostNodeSetParams API
    and validates the result
-2. Create graph, Add Graphnodes and clones the graph. Add Hostnode to the cloned graph, update
+2. Create graph, Add Graph nodes and clones the graph. Add Host node to the cloned graph, update
    hostNode params using hipGraphHostNodeSetParams API and validate the result
 
 Negative:
@@ -31,30 +33,30 @@ Negative:
 1) Pass pGraphNode as nullptr and verify api doesn’t crash, returns error code.
 2) Pass pNodeParams as nullptr and verify api doesn’t crash, returns error code.
 3) Pass hipHostNodeParams::hipHostFn_t as nullptr and verify api doesn't crash, returns error code.
-4) Pass unintialized host params
+4) Pass uninitialized host params
 */
 
-#include <hip_test_common.hh>
 #include <hip_test_checkers.hh>
+#include <hip_test_common.hh>
 
 #define SIZE 1024
 
-static void callbackfunc(void *A_h) {
-  int *A = reinterpret_cast<int *>(A_h);
+static void callbackfunc(void* A_h) {
+  int* A = reinterpret_cast<int*>(A_h);
   for (int i = 0; i < SIZE; i++) {
-     A[i] = i;
+    A[i] = i;
   }
 }
 
-static void callbackfunc_setparams(void *B_h) {
-  int *B = reinterpret_cast<int *>(B_h);
+static void callbackfunc_setparams(void* B_h) {
+  int* B = reinterpret_cast<int*>(B_h);
   for (int i = 0; i < SIZE; i++) {
-     B[i] = i * i;
+    B[i] = i * i;
   }
 }
 
 /*
-This testcase verifies the negative scenarios of
+This test case verifies the negative scenarios of
 hipGraphHostNodeSetParams API
 */
 TEST_CASE("Unit_hipGraphHostNodeSetParams_Negative") {
@@ -62,8 +64,7 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_Negative") {
   hipGraph_t graph;
   int *A_d{nullptr}, *C_d{nullptr};
   int *A_h{nullptr}, *C_h{nullptr};
-  HipTest::initArrays<int>(&A_d, nullptr, &C_d,
-                           &A_h, nullptr, &C_h, N, false);
+  HipTest::initArrays<int>(&A_d, nullptr, &C_d, &A_h, nullptr, &C_h, N, false);
 
   HIP_CHECK(hipGraphCreate(&graph, 0));
 
@@ -71,65 +72,63 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_Negative") {
   hipHostNodeParams hostParams;
   hostParams.fn = callbackfunc;
   hostParams.userData = A_h;
-  HIP_CHECK(hipGraphAddHostNode(&hostNode, graph,
-                                 nullptr,
-                                 0, &hostParams));
-#if HT_NVIDIA
+  HIP_CHECK(hipGraphAddHostNode(&hostNode, graph, nullptr, 0, &hostParams));
+
   SECTION("Passing nullptr to graph node") {
-    REQUIRE(hipGraphHostNodeSetParams(nullptr, &hostParams)
-                                      == hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphHostNodeSetParams(nullptr, &hostParams), hipErrorInvalidValue);
   }
 
   SECTION("Passing nullptr to hostParams") {
-    REQUIRE(hipGraphHostNodeSetParams(hostNode, nullptr)
-                                      == hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphHostNodeSetParams(hostNode, nullptr), hipErrorInvalidValue);
   }
-#endif
 
   SECTION("Passing nullptr to host func") {
     hostParams.fn = nullptr;
-    REQUIRE(hipGraphHostNodeSetParams(hostNode, &hostParams)
-                                      == hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphHostNodeSetParams(hostNode, &hostParams), hipErrorInvalidValue);
   }
 
-
-  SECTION("Passing unintialized hostParams") {
+  SECTION("Passing uninitialized hostParams") {
     hipHostNodeParams unintParams = {0, 0};
-    REQUIRE(hipGraphHostNodeSetParams(hostNode, &unintParams)
-                                      == hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphHostNodeSetParams(hostNode, &unintParams), hipErrorInvalidValue);
   }
+
+#if HT_NVIDIA // segfaults on AMD
+  SECTION("node is not a host node") {
+    hipGraphNode_t empty_node;
+    HIP_CHECK(hipGraphAddEmptyNode(&empty_node, graph, nullptr, 0));
+    HIP_CHECK_ERROR(hipGraphHostNodeSetParams(empty_node, &hostParams), hipErrorInvalidValue);
+  }
+#endif
+
+  HipTest::freeArrays<int>(A_d, nullptr, C_d, A_h, nullptr, C_h, false);
   HIP_CHECK(hipGraphDestroy(graph));
 }
+
 /*
-This testcase verifies hipGraphHostNodeSetParams API in cloned graph
+This test case verifies hipGraphHostNodeSetParams API in cloned graph
 Creates graph, Add graph nodes and clone the graph
 Add HostNode to the cloned graph,update the host params using
 hipGraphHostNodeSetParams API and validates the result
 */
-TEST_CASE("Unit_hipGraphHostNodeSetParams_ClonedGraphwithHostNode") {
+TEST_CASE("Unit_hipGraphHostNodeSetParams_ClonedGraphWithHostNode") {
   constexpr size_t N = 1024;
   constexpr size_t Nbytes = N * sizeof(int);
   hipGraph_t graph;
   hipGraphExec_t graphExec;
   int *A_d{nullptr}, *C_d{nullptr};
   int *A_h{nullptr}, *C_h{nullptr};
-  HipTest::initArrays<int>(&A_d, nullptr, &C_d,
-                           &A_h, nullptr, &C_h, N, false);
+  HipTest::initArrays<int>(&A_d, nullptr, &C_d, &A_h, nullptr, &C_h, N, false);
 
   HIP_CHECK(hipGraphCreate(&graph, 0));
-  hipGraphNode_t memcpyH2D_C,
-                 memcpyD2H_AC;
+  hipGraphNode_t memcpyH2D_C, memcpyD2H_AC;
   hipStream_t streamForGraph;
   HIP_CHECK(hipStreamCreate(&streamForGraph));
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyH2D_C, graph, nullptr,
-                                    0, C_d, C_h,
-                                    Nbytes, hipMemcpyHostToDevice));
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyD2H_AC, graph, nullptr,
-                                    0, A_h, C_d,
-                                    Nbytes, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyH2D_C, graph, nullptr, 0, C_d, C_h, Nbytes,
+                                    hipMemcpyHostToDevice));
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyD2H_AC, graph, nullptr, 0, A_h, C_d, Nbytes,
+                                    hipMemcpyDeviceToHost));
 
-  HIP_CHECK(hipGraphAddDependencies(graph, &memcpyH2D_C,
-                                    &memcpyD2H_AC, 1));
+  HIP_CHECK(hipGraphAddDependencies(graph, &memcpyH2D_C, &memcpyD2H_AC, 1));
 
   hipGraph_t clonedgraph;
   HIP_CHECK(hipGraphClone(&clonedgraph, graph));
@@ -138,16 +137,12 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_ClonedGraphwithHostNode") {
   hipHostNodeParams hostParams;
   hostParams.fn = callbackfunc;
   hostParams.userData = A_h;
-  HIP_CHECK(hipGraphAddHostNode(&hostNode, clonedgraph,
-                                 nullptr,
-                                 0, &hostParams));
-
+  HIP_CHECK(hipGraphAddHostNode(&hostNode, clonedgraph, nullptr, 0, &hostParams));
 
   hipHostNodeParams sethostParams;
   sethostParams.fn = callbackfunc_setparams;
   sethostParams.userData = C_h;
   HIP_CHECK(hipGraphHostNodeSetParams(hostNode, &sethostParams));
-
 
   // Instantiate and launch the cloned graph
   HIP_CHECK(hipGraphInstantiate(&graphExec, clonedgraph, nullptr, nullptr, 0));
@@ -157,7 +152,7 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_ClonedGraphwithHostNode") {
   // Verify execution result
   for (size_t i = 0; i < N; i++) {
     if (C_h[i] != static_cast<int>(i * i)) {
-      INFO("Validation failed i " <<  i  << "C_h[i] "<< C_h[i]);
+      INFO("Validation failed i " << i << "C_h[i] " << C_h[i]);
       REQUIRE(false);
     }
   }
@@ -169,7 +164,7 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_ClonedGraphwithHostNode") {
 }
 
 /*
-This testcase verifies the following scenario
+This test case verifies the following scenario
 Create graph, Adds host node to the graph,
 updates the host params using hipGraphHostNodeSetParams API
 and validates the result
@@ -181,31 +176,24 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_BasicFunc") {
   hipGraphExec_t graphExec;
   int *A_d{nullptr}, *C_d{nullptr};
   int *A_h{nullptr}, *C_h{nullptr};
-  HipTest::initArrays<int>(&A_d, nullptr, &C_d,
-                           &A_h, nullptr, &C_h, N, false);
+  HipTest::initArrays<int>(&A_d, nullptr, &C_d, &A_h, nullptr, &C_h, N, false);
 
   HIP_CHECK(hipGraphCreate(&graph, 0));
   hipGraphNode_t memcpyD2H_AC, memcpyH2D_C;
   hipStream_t streamForGraph;
   HIP_CHECK(hipStreamCreate(&streamForGraph));
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyH2D_C, graph, nullptr,
-                                    0, C_d, C_h,
-                                    Nbytes, hipMemcpyHostToDevice));
-  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyD2H_AC, graph, nullptr,
-                                    0, A_h, C_d,
-                                    Nbytes, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyH2D_C, graph, nullptr, 0, C_d, C_h, Nbytes,
+                                    hipMemcpyHostToDevice));
+  HIP_CHECK(hipGraphAddMemcpyNode1D(&memcpyD2H_AC, graph, nullptr, 0, A_h, C_d, Nbytes,
+                                    hipMemcpyDeviceToHost));
   hipGraphNode_t hostNode;
   hipHostNodeParams hostParams;
   hostParams.fn = callbackfunc;
   hostParams.userData = A_h;
-  HIP_CHECK(hipGraphAddHostNode(&hostNode, graph,
-                                 nullptr,
-                                 0, &hostParams));
+  HIP_CHECK(hipGraphAddHostNode(&hostNode, graph, nullptr, 0, &hostParams));
 
-  HIP_CHECK(hipGraphAddDependencies(graph, &memcpyH2D_C,
-                                    &memcpyD2H_AC, 1));
-  HIP_CHECK(hipGraphAddDependencies(graph, &memcpyD2H_AC,
-                                    &hostNode, 1));
+  HIP_CHECK(hipGraphAddDependencies(graph, &memcpyH2D_C, &memcpyD2H_AC, 1));
+  HIP_CHECK(hipGraphAddDependencies(graph, &memcpyD2H_AC, &hostNode, 1));
 
   hipHostNodeParams sethostParams;
   sethostParams.fn = callbackfunc_setparams;
@@ -220,7 +208,7 @@ TEST_CASE("Unit_hipGraphHostNodeSetParams_BasicFunc") {
   // Verify execution result
   for (size_t i = 0; i < N; i++) {
     if (C_h[i] != static_cast<int>(i * i)) {
-      INFO("Validation failed i " <<  i  << "C_h[i] "<< C_h[i]);
+      INFO("Validation failed i " << i << "C_h[i] " << C_h[i]);
       REQUIRE(false);
     }
   }

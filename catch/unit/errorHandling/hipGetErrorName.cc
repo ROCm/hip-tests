3fdfa56ba087aba51eed4d59c22742dd862453d9
milos-mozetic <118800401+milos-mozetic@users.noreply.github.com>
2023-06-28 06:15:53 +0200

    EXSWHTEC-224 - Test cases ID clean up and documentation for Error Handling (#72)
    - Test cases ID clean up and documentation for Error Handling
    
    [ROCm/hip-tests commit: 93751460f745688f2f04ee6c6d83802c341fd349]

--- catch/unit/errorHandling/hipGetErrorName.cc
+++ catch/unit/errorHandling/hipGetErrorName.cc
@@ -25,6 +25,26 @@ THE SOFTWARE.
 #include <hip_test_common.hh>
 #include <hip/hip_runtime_api.h>
 
+/**
+ * @addtogroup hipGetErrorName hipGetErrorName
+ * @{
+ * @ingroup ErrorTest
+ * `hipGetErrorName(hipError_t hip_error)` -
+ * Return hip error as text string form.
+ */
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Validate that a non-empty string is returned for each supported
+ *    device error enumeration.
+ * Test source
+ * ------------------------
+ *  - unit/errorHandling/hipGetErrorName.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.2
+ */
 TEST_CASE("Unit_hipGetErrorName_Positive_Basic") {
   const char* error_string = nullptr;
   const auto enumerator =
@@ -36,6 +56,20 @@ TEST_CASE("Unit_hipGetErrorName_Positive_Basic") {
   REQUIRE(strlen(error_string) > 0);
 }
 
+/**
+ * Test Description
+ * ------------------------
+ *  - Validate handling of invalid arguments:
+ *    -# When error enumerator is invalid (-1)
+ *      - AMD expected output: return "hipErrorUnknown"
+ *      - NVIDIA expected output: return "cudaErrorUnknown"
+ * Test source
+ * ------------------------
+ *  - unit/errorHandling/hipGetErrorName.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.2
+ */
 TEST_CASE("Unit_hipGetErrorName_Negative_Parameters") {
   const char* error_string = hipGetErrorName(static_cast<hipError_t>(-1));
   REQUIRE(error_string != nullptr);
--- catch/unit/errorHandling/hipGetErrorString.cc
+++ catch/unit/errorHandling/hipGetErrorString.cc
@@ -24,6 +24,26 @@ THE SOFTWARE.
 #include <hip_test_common.hh>
 #include <hip/hip_runtime_api.h>
 
+/**
+ * @addtogroup hipGetErrorString hipGetErrorString
+ * @{
+ * @ingroup ErrorTest
+ * `hipGetErrorString(hipError_t hipError)` -
+ * Return handy text string message to explain the error which occurred.
+ */
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Validate that a non-empty string is returned for each supported
+ *    device error enumeration.
+ * Test source
+ * ------------------------
+ *  - unit/errorHandling/hipGetErrorString.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.2
+ */
 TEST_CASE("Unit_hipGetErrorString_Positive_Basic") {
   const char* error_string = nullptr;
   const auto enumerator =
@@ -35,6 +55,19 @@ TEST_CASE("Unit_hipGetErrorString_Positive_Basic") {
   REQUIRE(strlen(error_string) > 0);
 }
 
+/**
+ * Test Description
+ * ------------------------
+ *  - Validate handling of invalid arguments:
+ *    -# When error enumerator is invalid (-1)
+ *      - Expected output: do not return `nullptr`
+ * Test source
+ * ------------------------
+ *  - unit/errorHandling/hipGetErrorString.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.2
+ */
 TEST_CASE("Unit_hipGetErrorString_Negative_Parameters") {
   const char* error_string = hipGetErrorString(static_cast<hipError_t>(-1));
   REQUIRE(error_string != nullptr);

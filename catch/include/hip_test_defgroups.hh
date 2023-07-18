38742f3ebecafe646331d94bb73faf5bac78472c
milos-mozetic <118800401+milos-mozetic@users.noreply.github.com>
2023-07-18 09:32:03 +0200

    EXSWHTEC-315 - Implement unit tests for dim3 (#298)
    - Implement unit tests for dim3
    - Add doxygen comments
    
    [ROCm/hip-tests commit: 8412bcb1b3d11c8854f1940347c8f6eaccfde178]

--- catch/include/hip_test_defgroups.hh
+++ catch/include/hip_test_defgroups.hh
@@ -37,6 +37,13 @@ THE SOFTWARE.
  */
 
 /**
+ * @defgroup VectorTypeTest Vector types
+ * @{
+ * This section describes tests for the Vector type functions and operators.
+ * @}
+ */
+
+/**
  * @defgroup DeviceTest Device Management
  * @{
  * This section describes tests for device management functions of HIP runtime API.

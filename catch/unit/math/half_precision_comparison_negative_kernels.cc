97ad251f353dea685c20888f399a1e6e3eb7fe8a
Nives Vukovic <nives.vukovic@htecgroup.com>
2023-08-30 13:10:50 +0000

    EXSWHTEC-331 - Implement positive and negative tests for half/half2 comparison functions

---
+++ catch/unit/math/half_precision_comparison_negative_kernels.cc
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+#include <hip/hip_fp16.h>
+
+class Dummy {
+ public:
+  __device__ Dummy() {}
+  __device__ ~Dummy() {}
+};
+
+
+#define UNARY_BOOL_HALF_NEGATIVE_KERNELS(func_name)                                                \
+  __global__ void func_name##_kernel_v1(__half* x) { bool result = func_name(x); }                 \
+  __global__ void func_name##_kernel_v2(Dummy x) { bool result = func_name(x); }
+
+#define BINARY_BOOL_HALF_NEGATIVE_KERNELS(func_name)                                               \
+  __global__ void func_name##_kernel_v1(__half* x, __half y) { bool result = func_name(x, y); }    \
+  __global__ void func_name##_kernel_v2(__half x, __half* y) { bool result = func_name(x, y); }    \
+  __global__ void func_name##_kernel_v3(Dummy x, __half y) { bool result = func_name(x, y); }      \
+  __global__ void func_name##_kernel_v4(__half x, Dummy y) { bool result = func_name(x, y); }
+
+
+#define BINARY_HALF_NEGATIVE_KERNELS(func_name)                                                    \
+  __global__ void func_name##_kernel_v1(__half* x, __half y) { __half result = func_name(x, y); }  \
+  __global__ void func_name##_kernel_v2(__half x, __half* y) { __half result = func_name(x, y); }  \
+  __global__ void func_name##_kernel_v3(Dummy x, __half y) { __half result = func_name(x, y); }    \
+  __global__ void func_name##_kernel_v4(__half x, Dummy y) { __half result = func_name(x, y); }
+
+
+UNARY_BOOL_HALF_NEGATIVE_KERNELS(__hisinf)
+UNARY_BOOL_HALF_NEGATIVE_KERNELS(__hisnan)
+
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__heq)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hequ)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hne)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hneu)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hge)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hgeu)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hgt)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hgtu)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hle)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hleu)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hlt)
+BINARY_BOOL_HALF_NEGATIVE_KERNELS(__hltu)
+
+BINARY_HALF_NEGATIVE_KERNELS(__hmax)
+BINARY_HALF_NEGATIVE_KERNELS(__hmax_nan)
+BINARY_HALF_NEGATIVE_KERNELS(__hmin)
+BINARY_HALF_NEGATIVE_KERNELS(__hmin_nan)

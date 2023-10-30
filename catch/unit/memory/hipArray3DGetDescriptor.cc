e3fbd1a8a8ef4f6bfe3f5782f53b5d4d645634bf
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2023-10-30 21:07:05 +0000

    Add tests for hipArrayGetInfo, hipArrayGetDescriptor, hipArray3DGetDescriptor.

---
+++ catch/unit/memory/hipArray3DGetDescriptor.cc
@@ -0,0 +1,94 @@
+/*
+Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * @addtogroup hipArray3DGetDescriptor hipArray3DGetDescriptor
+ * @{
+ * @ingroup MemoryTest
+ * `hipArray3DGetDescriptor(HIP_ARRAY3D_DESCRIPTOR* pArrayDescriptor, hipArray* array)` -
+ * Gets a 3D array descriptor.
+ */
+
+#include <hip_test_common.hh>
+#include <resource_guards.hh>
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Basic sanity test for `hipArray3DGetDescriptor`.
+ * Test source
+ * ------------------------
+ *  - unit/memory/hipArray3DGetDescriptor.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.7
+ */
+TEST_CASE("Unit_hipArray3DGetDescriptor_Positive_Basic") {
+  DrvArrayAllocGuard<float> array(make_hipExtent(1024, 4, 2));
+
+  HIP_ARRAY3D_DESCRIPTOR desc;
+  HIP_CHECK(hipArray3DGetDescriptor(&desc, array.ptr()));
+
+  using vec_info = vector_info<float>;
+  REQUIRE(desc.Format == vec_info::format);
+  REQUIRE(desc.NumChannels == vec_info::size);
+  REQUIRE(desc.Width == 1024 / sizeof(float));
+  REQUIRE(desc.Height == 4);
+  REQUIRE(desc.Depth == 2);
+  REQUIRE(desc.Flags == 0);
+}
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Validate handling of invalid arguments:
+ *    -# When descriptor is nullptr
+ *      - Expected output: return "hipErrorInvalidValue"
+ *    -# When array is nullptr
+ *      - Expected output: return "hipErrorInvalidHandle"
+ *    -# When array is freed
+ *      - Expected output: return "hipErrorInvalidHandle"
+ * Test source
+ * ------------------------
+ *  - unit/memory/hipArray3DGetDescriptor.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.7
+ */
+TEST_CASE("Unit_hipArray3DGetDescriptor_Negative_Parameters") {
+  DrvArrayAllocGuard<float> array(make_hipExtent(1024, 4, 2));
+
+  HIP_ARRAY3D_DESCRIPTOR desc;
+
+  SECTION("desc is nullptr") {
+    HIP_CHECK_ERROR(hipArray3DGetDescriptor(nullptr, array.ptr()), hipErrorInvalidValue);
+  }
+
+  SECTION("array is nullptr") {
+    HIP_CHECK_ERROR(hipArray3DGetDescriptor(&desc, nullptr), hipErrorInvalidHandle);
+  }
+
+  SECTION("array is freed") {
+    HIP_CHECK(hipArrayDestroy(array.ptr()));
+    HIP_CHECK_ERROR(hipArray3DGetDescriptor(&desc, array.ptr()), hipErrorInvalidHandle);
+  }
+}
---
+++ catch/unit/memory/hipArrayGetDescriptor.cc
@@ -0,0 +1,111 @@
+/*
+Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/**
+ * @addtogroup hipArrayGetDescriptor hipArrayGetDescriptor
+ * @{
+ * @ingroup MemoryTest
+ * `hipArrayGetDescriptor(HIP_ARRAY_DESCRIPTOR* pArrayDescriptor, hipArray* array)` -
+ * Gets a 1D or 2D array descriptor.
+ */
+
+#include <hip_test_common.hh>
+#include <resource_guards.hh>
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Basic sanity test for `hipArrayGetDescriptor`.
+ * Test source
+ * ------------------------
+ *  - unit/memory/hipArrayGetDescriptor.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.7
+ */
+TEST_CASE("Unit_hipArrayGetDescriptor_Positive_Basic") {
+  HIP_ARRAY_DESCRIPTOR expected_desc{};
+  using vec_info = vector_info<float>;
+  expected_desc.Format = vec_info::format;
+  expected_desc.NumChannels = vec_info::size;
+  expected_desc.Width = 1024 / sizeof(float);
+  expected_desc.Height = 4;
+
+  hipArray_t ptr;
+  HIP_CHECK(hipArrayCreate(&ptr, &expected_desc));
+
+  HIP_ARRAY_DESCRIPTOR desc;
+  HIP_CHECK(hipArrayGetDescriptor(&desc, ptr));
+
+  REQUIRE(desc.Format == expected_desc.Format);
+  REQUIRE(desc.NumChannels == expected_desc.NumChannels);
+  REQUIRE(desc.Width == expected_desc.Width);
+  REQUIRE(desc.Height == expected_desc.Height);
+
+  HIP_CHECK(hipArrayDestroy(ptr));
+}
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Validate handling of invalid arguments:
+ *    -# When descriptor is nullptr
+ *      - Expected output: return "hipErrorInvalidValue"
+ *    -# When array is nullptr
+ *      - Expected output: return "hipErrorInvalidHandle"
+ *    -# When array is freed
+ *      - Expected output: return "hipErrorInvalidHandle"
+ * Test source
+ * ------------------------
+ *  - unit/memory/hipArrayGetDescriptor.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.7
+ */
+TEST_CASE("Unit_hipArrayGetDescriptor_Negative_Parameters") {
+  HIP_ARRAY_DESCRIPTOR expected_desc{};
+  using vec_info = vector_info<float>;
+  expected_desc.Format = vec_info::format;
+  expected_desc.NumChannels = vec_info::size;
+  expected_desc.Width = 1024 / sizeof(float);
+  expected_desc.Height = 4;
+
+  hipArray_t ptr;
+  HIP_CHECK(hipArrayCreate(&ptr, &expected_desc));
+
+  HIP_ARRAY_DESCRIPTOR desc;
+
+  SECTION("desc is nullptr") {
+    HIP_CHECK_ERROR(hipArrayGetDescriptor(nullptr, ptr), hipErrorInvalidValue);
+  }
+
+  SECTION("array is nullptr") {
+    HIP_CHECK_ERROR(hipArrayGetDescriptor(&desc, nullptr), hipErrorInvalidHandle);
+  }
+
+  SECTION("array is freed") {
+    HIP_CHECK(hipArrayDestroy(ptr));
+    HIP_CHECK_ERROR(hipArrayGetDescriptor(&desc, ptr), hipErrorInvalidHandle);
+  }
+
+  static_cast<void>(hipArrayDestroy(ptr));
+}

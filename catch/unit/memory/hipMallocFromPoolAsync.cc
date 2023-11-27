a7e66ee83269c7c240bd604b1558c01e088781f3
Nives Vukovic <nives.vukovic@htecgroup.com>
2023-11-27 18:11:04 +0100

    EXSWHTEC-35 - Expand testing of hipMallocFromPoolAsync API

--- catch/unit/memory/hipMallocFromPoolAsync.cc
+++ catch/unit/memory/hipMallocFromPoolAsync.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
    in the Software without restriction, including without limitation the rights
@@ -18,14 +18,31 @@
  */
 
 #include <hip_test_common.hh>
+#include <resource_guards.hh>
+#include <utils.hh>
+
 
 #include <limits>
 
-constexpr hipMemPoolProps kPoolPropsForMalloc = {
-    hipMemAllocationTypePinned, hipMemHandleTypeNone, {hipMemLocationTypeDevice, 0}, nullptr, {0}};
+static inline hipMemPool_t CreateMemPool(const int device) {
+  hipMemPoolProps kPoolProps;
+  kPoolProps.allocType = hipMemAllocationTypePinned;
+  kPoolProps.handleTypes = hipMemHandleTypeNone;
+  kPoolProps.location.type = hipMemLocationTypeDevice;
+  kPoolProps.location.id = device;
+  kPoolProps.win32SecurityAttributes = nullptr;
+  memset(kPoolProps.reserved, 0, sizeof(kPoolProps.reserved));
 
-TEST_CASE("Unit_hipMallocFromPoolAsync_negative") {
-  HIP_CHECK(hipSetDevice(0));
+  hipMemPool_t mem_pool;
+  HIP_CHECK(hipMemPoolCreate(&mem_pool, &kPoolProps));
+
+  return mem_pool;
+}
+
+TEST_CASE("Unit_hipMallocFromPoolAsync_Basic") {
+  int device_id = 0;
+  HIP_CHECK(hipSetDevice(device_id));
+
   int mem_pool_support = 0;
   HIP_CHECK(hipDeviceGetAttribute(&mem_pool_support, hipDeviceAttributeMemoryPoolsSupported, 0));
   if (!mem_pool_support) {
@@ -33,33 +50,96 @@ TEST_CASE("Unit_hipMallocFromPoolAsync_negative") {
     return;
   }
 
-  hipMemPool_t mem_pool = nullptr;
-  void* p = nullptr;
-  size_t max_size = std::numeric_limits<size_t>::max();
-  hipStream_t stream{nullptr};
-  HIP_CHECK(hipStreamCreate(&stream));
-  HIP_CHECK(hipMemPoolCreate(&mem_pool, &kPoolPropsForMalloc));
+  const auto allocation_size = GENERATE(kPageSize / 2, kPageSize, kPageSize * 2);
+  LinearAllocGuard<int> host_alloc(LinearAllocs::hipHostMalloc, allocation_size);
+  hipMemPool_t mem_pool = CreateMemPool(device_id);
+  hipMemAccessDesc desc_list = {{hipMemLocationTypeDevice, 0}, hipMemAccessFlagsProtReadWrite};
+  int count = 1;
+  HIP_CHECK(hipMemPoolSetAccess(mem_pool, &desc_list, count));
 
-  SECTION("dev_ptr is nullptr") {
-    REQUIRE(hipMallocFromPoolAsync(nullptr, 100, mem_pool, stream) != hipSuccess);
+  int* alloc_mem;
+  StreamGuard stream(Streams::created);
+
+  HIP_CHECK(hipMallocFromPoolAsync(reinterpret_cast<void**>(&alloc_mem), allocation_size, mem_pool,
+                                   stream.stream()));
+  HIP_CHECK(hipStreamSynchronize(stream.stream()));
+
+  size_t used_mem = 0;
+  hipMemPoolAttr attr = hipMemPoolAttrUsedMemCurrent;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  REQUIRE(allocation_size == used_mem);
+
+  const auto element_count = allocation_size / sizeof(int);
+  constexpr auto thread_count = 1024;
+  const auto block_count = element_count / thread_count + 1;
+  constexpr int expected_value = 17;
+  VectorSet<<<block_count, thread_count, 0>>>(alloc_mem, expected_value, element_count);
+  HIP_CHECK(hipGetLastError());
+
+  HIP_CHECK(hipMemcpy(host_alloc.host_ptr(), alloc_mem, allocation_size, hipMemcpyDeviceToHost));
+
+  ArrayFindIfNot(host_alloc.host_ptr(), expected_value, element_count);
+
+  HIP_CHECK(hipFreeAsync(reinterpret_cast<void*>(alloc_mem), stream.stream()));
+
+  attr = hipMemPoolAttrReservedMemCurrent;
+  std::uint64_t res_before_sync = 0;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &res_before_sync));
+  HIP_CHECK(hipStreamSynchronize(stream.stream()));
+
+  std::uint64_t res_after_sync = 0;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &res_after_sync));
+  // Sync must release memory to OS
+  REQUIRE(res_after_sync <= res_before_sync);
+
+  attr = hipMemPoolAttrUsedMemCurrent;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  REQUIRE(0 == used_mem);
+
+  HIP_CHECK(hipMemPoolDestroy(mem_pool));
+}
+
+TEST_CASE("Unit_hipMallocFromPoolAsync_Negative") {
+  int device_id = 0;
+  HIP_CHECK(hipSetDevice(device_id));
+
+  int mem_pool_support = 0;
+  HIP_CHECK(hipDeviceGetAttribute(&mem_pool_support, hipDeviceAttributeMemoryPoolsSupported, 0));
+  if (!mem_pool_support) {
+    SUCCEED("Runtime doesn't support Memory Pool. Skip the test case.");
+    return;
   }
 
-  SECTION("invalid mempool handle") {
-    REQUIRE(hipMallocFromPoolAsync(static_cast<void**>(&p), 100, reinterpret_cast<hipMemPool_t>(-1),
-                                   stream) != hipSuccess);
+  void* p = nullptr;
+  size_t max_size = std::numeric_limits<size_t>::max();
+  size_t alloc_size = 1024;
+  hipMemPool_t mem_pool = CreateMemPool(device_id);
+  StreamGuard stream(Streams::created);
+
+  SECTION("dev_ptr is nullptr") {
+    HIP_CHECK_ERROR(hipMallocFromPoolAsync(nullptr, alloc_size, mem_pool, stream.stream()),
+                    hipErrorInvalidValue);
+  }
+
+  SECTION("Mempool not created") {
+    hipMemPool_t dummy_mem_pool = nullptr;
+    HIP_CHECK_ERROR(hipMallocFromPoolAsync(static_cast<void**>(&p), alloc_size, dummy_mem_pool,
+                                           stream.stream()),
+                    hipErrorInvalidValue);
   }
 
   SECTION("invalid stream handle") {
-    REQUIRE(hipMallocFromPoolAsync(static_cast<void**>(&p), 100, mem_pool,
-                                   reinterpret_cast<hipStream_t>(-1)) != hipSuccess);
+    HIP_CHECK_ERROR(hipMallocFromPoolAsync(static_cast<void**>(&p), alloc_size, mem_pool,
+                                           reinterpret_cast<hipStream_t>(-1)),
+                    hipErrorInvalidHandle);
   }
 
-  SECTION("out of memory") {
-    REQUIRE(hipMallocFromPoolAsync(static_cast<void**>(&p), max_size, mem_pool, stream) !=
-            hipSuccess);
+  SECTION("Size is max size_t") {
+    HIP_CHECK_ERROR(
+        hipMallocFromPoolAsync(static_cast<void**>(&p), max_size, mem_pool, stream.stream()),
+        hipErrorOutOfMemory);
   }
 
-  HIP_CHECK(hipStreamSynchronize(stream));
+  HIP_CHECK(hipStreamSynchronize(stream.stream()));
   HIP_CHECK(hipMemPoolDestroy(mem_pool));
-  HIP_CHECK(hipStreamDestroy(stream));
 }

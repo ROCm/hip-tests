72c3b113120a5843df1b2f8b53dffa29ab9f4f33
Nives Vukovic <nives.vukovic@htecgroup.com>
2023-11-27 18:38:46 +0100

    EXSWHTEC-35 - Add positive test with two hipMallocFromPoolAsync API calls

--- catch/unit/memory/hipMallocFromPoolAsync.cc
+++ catch/unit/memory/hipMallocFromPoolAsync.cc
@@ -39,7 +39,7 @@ static inline hipMemPool_t CreateMemPool(const int device) {
   return mem_pool;
 }
 
-TEST_CASE("Unit_hipMallocFromPoolAsync_Basic") {
+TEST_CASE("Unit_hipMallocFromPoolAsync_Basic_OneAlloc") {
   int device_id = 0;
   HIP_CHECK(hipSetDevice(device_id));
 
@@ -94,6 +94,92 @@ TEST_CASE("Unit_hipMallocFromPoolAsync_Basic") {
 
   attr = hipMemPoolAttrUsedMemCurrent;
   HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  REQUIRE(0 == used_mem);
+
+  HIP_CHECK(hipMemPoolDestroy(mem_pool));
+}
+
+TEST_CASE("Unit_hipMallocFromPoolAsync_Basic_TwoAllocs") {
+  int device_id = 0;
+  HIP_CHECK(hipSetDevice(device_id));
+
+  int mem_pool_support = 0;
+  HIP_CHECK(hipDeviceGetAttribute(&mem_pool_support, hipDeviceAttributeMemoryPoolsSupported, 0));
+  if (!mem_pool_support) {
+    SUCCEED("Runtime doesn't support Memory Pool. Skip the test case.");
+    return;
+  }
+
+  const auto allocation_size1 = GENERATE(kPageSize / 2, kPageSize, kPageSize * 2);
+  const auto allocation_size2 = GENERATE(kPageSize / 2, kPageSize, kPageSize * 2);
+
+  LinearAllocGuard<int> host_alloc1(LinearAllocs::hipHostMalloc, allocation_size1);
+  LinearAllocGuard<int> host_alloc2(LinearAllocs::hipHostMalloc, allocation_size2);
+
+  hipMemPool_t mem_pool = CreateMemPool(device_id);
+  hipMemAccessDesc desc_list = {{hipMemLocationTypeDevice, 0}, hipMemAccessFlagsProtReadWrite};
+  int count = 1;
+  HIP_CHECK(hipMemPoolSetAccess(mem_pool, &desc_list, count));
+
+  int* alloc_mem1;
+  int* alloc_mem2;
+  StreamGuard stream(Streams::created);
+
+  HIP_CHECK(hipMallocFromPoolAsync(reinterpret_cast<void**>(&alloc_mem1), allocation_size1,
+                                   mem_pool, stream.stream()));
+  HIP_CHECK(hipMallocFromPoolAsync(reinterpret_cast<void**>(&alloc_mem2), allocation_size2,
+                                   mem_pool, stream.stream()));
+
+  size_t used_mem = 0;
+  hipMemPoolAttr attr = hipMemPoolAttrUsedMemCurrent;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  // Make sure the current usage query works - both allocations are reported
+  REQUIRE((allocation_size1 + allocation_size2) == used_mem);
+
+  const auto element_count1 = allocation_size1 / sizeof(int);
+  const auto element_count2 = allocation_size2 / sizeof(int);
+  constexpr auto thread_count = 1024;
+  const auto block_count1 = element_count1 / thread_count + 1;
+  const auto block_count2 = element_count2 / thread_count + 1;
+  constexpr int expected_value = 17;
+  VectorSet<<<block_count1, thread_count, 0>>>(alloc_mem1, expected_value, element_count1);
+  VectorSet<<<block_count2, thread_count, 0>>>(alloc_mem2, expected_value + 1, element_count2);
+  HIP_CHECK(hipGetLastError());
+
+  HIP_CHECK(hipMemcpy(host_alloc1.host_ptr(), alloc_mem1, allocation_size1, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipMemcpy(host_alloc2.host_ptr(), alloc_mem2, allocation_size2, hipMemcpyDeviceToHost));
+
+  ArrayFindIfNot(host_alloc1.host_ptr(), expected_value, element_count1);
+  ArrayFindIfNot(host_alloc2.host_ptr(), expected_value + 1, element_count2);
+
+  HIP_CHECK(hipFreeAsync(reinterpret_cast<void*>(alloc_mem1), stream.stream()));
+
+  attr = hipMemPoolAttrReservedMemCurrent;
+  std::uint64_t res_before_sync = 0;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &res_before_sync));
+  HIP_CHECK(hipStreamSynchronize(stream.stream()));
+
+  std::uint64_t res_after_sync = 0;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &res_after_sync));
+  // Sync must release memory to OS
+  REQUIRE(res_after_sync <= res_before_sync);
+
+  attr = hipMemPoolAttrUsedMemCurrent;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  // Make sure the current usage query works - just second buffer is left
+  REQUIRE(allocation_size2 == used_mem);
+
+  attr = hipMemPoolAttrUsedMemHigh;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  // Make sure the high watermark usage works - both buffers must be reported
+  REQUIRE((allocation_size1 + allocation_size2) == used_mem);
+
+  HIP_CHECK(hipFreeAsync(reinterpret_cast<void*>(alloc_mem2), stream.stream()));
+  HIP_CHECK(hipStreamSynchronize(stream.stream()));
+
+  attr = hipMemPoolAttrUsedMemCurrent;
+  HIP_CHECK(hipMemPoolGetAttribute(mem_pool, attr, &used_mem));
+  // Make sure the current usage query works - none of the buffers are used
   REQUIRE(0 == used_mem);
 
   HIP_CHECK(hipMemPoolDestroy(mem_pool));

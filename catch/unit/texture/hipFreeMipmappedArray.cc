8f7e7437f27fb70cf30fc777331717beaf82436b
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2023-12-01 13:55:03 +0100

    EXSWHTEC-361 - Implement tests for Mipmapped Texture Management APIs (#436)
    Change-Id: Ia753ddb3bd38b78aca719d321bf33de76ec00ad6
    
    
    [ROCm/hip-tests commit: 127e056c227fef2669466eb46ab808fefa70dbda]

---
+++ catch/unit/texture/hipFreeMipmappedArray.cc
@@ -0,0 +1,60 @@
+/*
+Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+
+/**
+ * @addtogroup hipFreeMipmappedArray hipFreeMipmappedArray
+ * @{
+ * @ingroup TextureTest
+ */
+
+/**
+ * Test Description
+ * ------------------------
+ *    - Negative parameters test for `hipFreeMipmappedArray`.
+ * Test source
+ * ------------------------
+ *    - unit/texture/hipFreeMipmappedArray.cc
+ * Test requirements
+ * ------------------------
+ *    - HIP_VERSION >= 5.7
+ */
+TEST_CASE("Unit_hipFreeMipmappedArray_Negative_Parameters") {
+  CHECK_IMAGE_SUPPORT;
+
+  SECTION("array is nullptr") {
+    HIP_CHECK_ERROR(hipFreeMipmappedArray(nullptr), hipErrorInvalidValue);
+  }
+
+  SECTION("double free") {
+    hipMipmappedArray_t array;
+    hipChannelFormatDesc desc = hipCreateChannelDesc<float>();
+    hipExtent extent = make_hipExtent(4, 4, 6);
+    unsigned int levels = 4;
+
+    HIP_CHECK(hipMallocMipmappedArray(&array, &desc, extent, levels, 0));
+
+    HIP_CHECK(hipFreeMipmappedArray(array));
+    HIP_CHECK_ERROR(hipFreeMipmappedArray(array), hipErrorContextIsDestroyed);
+  }
+}

c4af6bda9f97e9a7a90bcfce26f15fa17d92e529
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2023-12-01 10:14:53 +0100

    Add CHECK_IMAGE_SUPPORT to texture fetching tests.

--- catch/unit/texture/tex1DLod.cc
+++ catch/unit/texture/tex1DLod.cc
@@ -50,6 +50,8 @@ THE SOFTWARE.
  */
 TEMPLATE_TEST_CASE("Unit_tex1DLod_Positive_ReadModeElementType", "", char, unsigned char, short,
                    unsigned short, int, unsigned int, float) {
+  CHECK_IMAGE_SUPPORT;
+
   TextureTestParams<TestType> params = {0};
   params.extent = make_hipExtent(1024, 0, 0);
   params.num_subdivisions = 4;
@@ -101,6 +103,8 @@ TEMPLATE_TEST_CASE("Unit_tex1DLod_Positive_ReadModeElementType", "", char, unsig
  */
 TEMPLATE_TEST_CASE("Unit_tex1DLod_Positive_ReadModeNormalizedFloat", "", char, unsigned char, short,
                    unsigned short) {
+  CHECK_IMAGE_SUPPORT;
+
   TextureTestParams<TestType> params = {0};
   params.extent = make_hipExtent(1024, 0, 0);
   params.num_subdivisions = 4;

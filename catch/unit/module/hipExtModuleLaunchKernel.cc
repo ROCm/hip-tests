2b4b804e9128f09dbdcfef14fe306e01fe3d6db2
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2023-12-28 14:03:02 +0000

    Remove conflict marker.

--- catch/unit/module/hipExtModuleLaunchKernel.cc
+++ catch/unit/module/hipExtModuleLaunchKernel.cc
@@ -1,10 +1,6 @@
 /*
-<<<<<<< HEAD
-Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
-=======
 Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
 
->>>>>>> upstream/develop
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights

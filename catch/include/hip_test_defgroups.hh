1c10b556a7cf94abb9dbeef17ebc8d67f3f70156
Mirza Halilčević <109971222+mirza-halilcevic@users.noreply.github.com>
2023-12-28 18:19:15 +0100

    EXSWHTEC-302 - Implement tests for memory fence device functions #276
    Change-Id: I327527288f90011f09262708dd6372a7c6fc4708
    
    
    [ROCm/hip-tests commit: 8ef0d724f548fb6c25dfe35a8c9e31f00ef19d57]

--- catch/include/hip_test_defgroups.hh
+++ catch/include/hip_test_defgroups.hh
@@ -117,6 +117,13 @@ THE SOFTWARE.
  */
 
 /**
+ * @defgroup ThreadfenceTest Memory Fence Functions
+ * @{
+ * This section describes tests for Memory Fence Functions.
+ * @}
+ */
+
+/**
  * @defgroup MemoryTest memory Management APIs
  * @{
  * This section describes the memory management types & functions of HIP runtime API.

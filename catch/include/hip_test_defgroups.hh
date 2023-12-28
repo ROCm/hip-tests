e3bac85a3cb9c026d6d61d889f17ac05c89fe355
Mirza Halilčević <109971222+mirza-halilcevic@users.noreply.github.com>
2023-12-28 18:30:45 +0100

    EXSWHTEC-249 - Implement Performance Tests for Memcpy APIs #119
    Change-Id: Ib04fe4dd3efce92d7c7bfc8f0c75abd8e9dfe7be

--- catch/include/hip_test_defgroups.hh
+++ catch/include/hip_test_defgroups.hh
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 - 2023 Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c) 2021 - 2024 Advanced Micro Devices, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -186,7 +186,8 @@ THE SOFTWARE.
  * @}
  */
 
- /**
+/**
+ * @defgroup ComplexTest Complex type
  * @{
  * This section describes tests for the Complex type functions.
  * @}
---
+++ catch/performance/memcpy/hipMemcpyAtoH.cc
@@ -0,0 +1,69 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "memcpy_performance_common.hh"
+
+/**
+ * @addtogroup memcpy memcpy
+ * @{
+ * @ingroup PerformanceTest
+ */
+
+class MemcpyAtoHBenchmark : public Benchmark<MemcpyAtoHBenchmark> {
+ public:
+  void operator()(void* dst, hipArray* src_array, size_t allocation_size) {
+    TIMED_SECTION(kTimerTypeCpu) {
+      HIP_CHECK(hipMemcpyAtoH(dst, src_array, 0, allocation_size));
+    }
+  }
+};
+
+static void RunBenchmark(LinearAllocs host_allocation_type, size_t width) {
+  MemcpyAtoHBenchmark benchmark;
+  benchmark.AddSectionName(std::to_string(width));
+  benchmark.AddSectionName(GetAllocationSectionName(host_allocation_type));
+
+  size_t allocation_size = width * sizeof(int);
+  LinearAllocGuard<int> host_allocation(host_allocation_type, allocation_size);
+  ArrayAllocGuard<int> array_allocation(make_hipExtent(width, 0, 0), hipArrayDefault);
+  benchmark.Run(host_allocation.ptr(), array_allocation.ptr(), allocation_size);
+}
+
+/**
+ * Test Description
+ * ------------------------
+ *  - Executes `hipMemcpyAtoH` from Device array to Host:
+ *    -# Allocation size
+ *      - Small: 512 B
+ *      - Medium: 1024 B
+ *      - Large: 4096 B
+ *    -# Allocation type
+ *      - Host: host pinned and pageable
+ * Test source
+ * ------------------------
+ *  - performance/memcpy/hipMemcpyAtoH.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 5.2
+ */
+TEST_CASE("Performance_hipMemcpyAtoH") {
+  const auto allocation_size = GENERATE(512, 1024, 4096);
+  const auto host_allocation_type = GENERATE(LinearAllocs::malloc, LinearAllocs::hipHostMalloc);
+  RunBenchmark(host_allocation_type, allocation_size);
+}

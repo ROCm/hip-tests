00c8bba479fb8a971ab9edd42cd00c7729586654
Todd tiantuo Li <Toddtiantuo.Li@amd.com>
2024-05-29 03:34:05 -0700

    SWDEV-448894 - Correct and align peer access related behavior for memcpy tests
    Change-Id: I31379836f9066a6b317efc26678302261e4c7a1b
    
    
    [ROCm/hip-tests commit: 4163c69fdc1198863e0e84072fc839dbf8979be1]

--- catch/include/memcpy1d_tests_common.hh
+++ catch/include/memcpy1d_tests_common.hh
@@ -141,7 +141,9 @@ void MemcpyDeviceToDeviceShell(F memcpy_func, const hipStream_t kernel_stream = 
     int can_access_peer = 0;
     HIP_CHECK(hipDeviceCanAccessPeer(&can_access_peer, src_device, dst_device));
     if (!can_access_peer) {
-      INFO("Peer access cannot be enabled between devices " << src_device << " " << dst_device);
+      std::string msg = "Skipped as peer access cannot be enabled between devices " +
+          std::to_string(src_device) + " " + std::to_string(dst_device);
+      HipTest::HIP_SKIP_TEST(msg.c_str());
       return;
     }
     HIP_CHECK(hipDeviceEnablePeerAccess(dst_device, 0));

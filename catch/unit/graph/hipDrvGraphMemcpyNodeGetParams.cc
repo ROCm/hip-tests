be3b8c4d7157fced02be341ef7ac64a601ac6601
Anavena Venkatesh <Anavena.Venkatesh@amd.com>
2024-12-09 13:14:37 +0530

    SWDEV-491363 - [catch2][dtest] Added test cases for missed graph APIs
    Change-Id: Ic740c52992b8d0a325f09b1db0adf599561e1a1d

--- catch/unit/graph/hipDrvGraphMemcpyNodeGetParams.cc
+++ catch/unit/graph/hipDrvGraphMemcpyNodeGetParams.cc
@@ -20,18 +20,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <hip_test_defgroups.hh>
 #include <hip_test_common.hh>
+#include <hip_test_defgroups.hh>
 #include <memcpy3d_tests_common.hh>
+#include <numeric>
 
-// hipDrvGraphAddMemcpyNode API is yet to be implemented in HIP runtime.
-#if 0
 /**
  * @addtogroup hipDrvGraphMemcpyNodeGetParams hipDrvGraphMemcpyNodeGetParams
  * @{
  * @ingroup GraphTest
- * `hipDrvGraphMemcpyNodeGetParams(hipGraphNode_t hNode, HIP_MEMCPY3D* nodeParams)` -
- * 	Gets a memcpy node's parameters
+ * `hipDrvGraphMemcpyNodeGetParams(hipGraphNode_t hNode, HIP_MEMCPY3D*
+ * nodeParams)` - Gets a memcpy node's parameters
  * ________________________
  * Test cases from other APIs:
  *  - @ref Unit_hipDrvGraphMemcpyNodeSetParams_Positive_Basic
@@ -63,37 +62,129 @@ TEST_CASE("Unit_hipDrvGraphMemcpyNodeGetParams_Negative_Parameters") {
   LinearAllocGuard3D<int> src_alloc(extent);
   LinearAllocGuard3D<int> dst_alloc(extent);
 
-  auto params =
-      GetDrvMemcpy3DParms(dst_alloc.pitched_ptr(), make_hipPos(0, 0, 0), src_alloc.pitched_ptr(),
-                          make_hipPos(0, 0, 0), dst_alloc.extent(), hipMemcpyDeviceToDevice);
+  auto params = GetDrvMemcpy3DParms(
+      dst_alloc.pitched_ptr(), make_hipPos(0, 0, 0), src_alloc.pitched_ptr(),
+      make_hipPos(0, 0, 0), dst_alloc.extent(), hipMemcpyDeviceToDevice);
 
   hipGraph_t graph = nullptr;
   hipGraphNode_t node = nullptr;
 
   SECTION("node == nullptr") {
-    HIP_CHECK_ERROR(hipDrvGraphMemcpyNodeGetParams(nullptr, &params), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipDrvGraphMemcpyNodeGetParams(nullptr, &params),
+                    hipErrorInvalidValue);
   }
 
   SECTION("pNodeParams == nullptr") {
     HIP_CHECK(hipGraphCreate(&graph, 0));
-    HIP_CHECK(hipDrvGraphAddMemcpyNode(&node, graph, nullptr, 0, &params, context));
-    HIP_CHECK_ERROR(hipDrvGraphMemcpyNodeGetParams(node, nullptr), hipErrorInvalidValue);
+    HIP_CHECK(
+        hipDrvGraphAddMemcpyNode(&node, graph, nullptr, 0, &params, context));
+    HIP_CHECK_ERROR(hipDrvGraphMemcpyNodeGetParams(node, nullptr),
+                    hipErrorInvalidValue);
     HIP_CHECK(hipGraphDestroy(graph));
   }
-
+#if HT_AMD
   SECTION("Node is destroyed") {
     HIP_CHECK(hipGraphCreate(&graph, 0));
-    HIP_CHECK(hipDrvGraphAddMemcpyNode(&node, graph, nullptr, 0, &params, context));
+    HIP_CHECK(
+        hipDrvGraphAddMemcpyNode(&node, graph, nullptr, 0, &params, context));
     HIP_CHECK(hipGraphDestroy(graph));
-    HIP_CHECK_ERROR(hipDrvGraphMemcpyNodeGetParams(node, &params), hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipDrvGraphMemcpyNodeGetParams(node, &params),
+                    hipErrorInvalidValue);
   }
+#endif
 
   HIP_CHECK(hipCtxPopCurrent(&context));
   HIP_CHECK(hipCtxDestroy(context));
 }
-#endif // if 0
-
 /**
-* End doxygen group GraphTest.
-* @}
-*/
+ * Test Description
+ * ------------------------
+ *    - Create graph node with memcopy parameters and add to graph,
+ *    get the parameters from the created graph node and compare
+ *    the all the values.
+ * Test source
+ * ------------------------
+ *    - unit/graph/hipDrvGraphMemcpyNodeGetParams.cc
+ * Test requirements
+ * ------------------------
+ *    - HIP_VERSION >= 6.4
+ */
+
+TEST_CASE("Unit_hipDrvGraphMemcpyNodeGetParams_Positive") {
+  size_t size = 10;
+  size_t numW = size * sizeof(int);
+  // Host Vectors
+  std::vector<int> A_h(numW);
+  std::vector<int> B_h(numW);
+  hipCtx_t context;
+  // Initialization
+  std::iota(A_h.begin(), A_h.end(), 1);
+  std::fill_n(B_h.begin(), size, 0);
+  int deviceid = 0;
+  hipGraph_t graph;
+  hipStream_t streamForGraph;
+  hipGraphExec_t graphExec;
+  hipGraphNode_t node;
+
+  HIP_CHECK(hipGraphCreate(&graph, 0));
+  HIP_CHECK(hipSetDevice(deviceid));
+  HIP_CHECK(hipStreamCreate(&streamForGraph));
+  HIP_CHECK(hipCtxCreate(&context, 0, deviceid));
+
+  HIP_MEMCPY3D memCpy_params{};
+  memset(&memCpy_params, 0x0, sizeof(HIP_MEMCPY3D));
+  memCpy_params.srcXInBytes = 0;
+  memCpy_params.srcY = 0;
+  memCpy_params.srcZ = 0;
+  memCpy_params.dstXInBytes = 0;
+  memCpy_params.dstY = 0;
+  memCpy_params.dstZ = 0;
+  memCpy_params.WidthInBytes = numW;
+  memCpy_params.Height = 1;
+  memCpy_params.Depth = 1;
+  memCpy_params.srcMemoryType = hipMemoryTypeHost;
+  memCpy_params.dstMemoryType = hipMemoryTypeHost;
+  memCpy_params.srcHost = A_h.data();
+  memCpy_params.srcPitch = numW;
+  memCpy_params.srcHeight = 1;
+  memCpy_params.dstHost = B_h.data();
+  memCpy_params.dstPitch = numW;
+  memCpy_params.dstHeight = 1;
+
+  HIP_CHECK(hipDrvGraphAddMemcpyNode(&node, graph, nullptr, 0, &memCpy_params,
+                                     context));
+  HIP_CHECK(hipGraphInstantiate(&graphExec, graph, nullptr, nullptr, 0));
+  HIP_CHECK(hipGraphLaunch(graphExec, streamForGraph));
+  HIP_CHECK(hipStreamSynchronize(streamForGraph));
+  REQUIRE(memcmp(A_h.data(), B_h.data(), numW) == 0);
+
+  HIP_MEMCPY3D memCpyGetParams{};
+  HIP_CHECK(hipDrvGraphMemcpyNodeGetParams(node, &memCpyGetParams));
+
+  REQUIRE(memCpy_params.srcXInBytes == memCpyGetParams.srcXInBytes);
+  REQUIRE(memCpy_params.srcY == memCpyGetParams.srcY);
+  REQUIRE(memCpy_params.srcZ == memCpyGetParams.srcZ);
+  REQUIRE(memCpy_params.dstXInBytes == memCpyGetParams.dstXInBytes);
+  REQUIRE(memCpy_params.dstY == memCpyGetParams.dstY);
+  REQUIRE(memCpy_params.dstZ == memCpyGetParams.dstZ);
+  REQUIRE(memCpy_params.WidthInBytes == memCpyGetParams.WidthInBytes);
+  REQUIRE(memCpy_params.Height == memCpyGetParams.Height);
+  REQUIRE(memCpy_params.Depth == memCpyGetParams.Depth);
+  REQUIRE(memCpy_params.srcMemoryType == memCpyGetParams.srcMemoryType);
+  REQUIRE(memCpy_params.dstMemoryType == memCpyGetParams.dstMemoryType);
+  REQUIRE(memCpy_params.srcHost == memCpyGetParams.srcHost);
+  REQUIRE(memCpy_params.srcPitch == memCpyGetParams.srcPitch);
+  REQUIRE(memCpy_params.srcHeight == memCpyGetParams.srcHeight);
+  REQUIRE(memCpy_params.dstHost == memCpyGetParams.dstHost);
+  REQUIRE(memCpy_params.dstPitch == memCpyGetParams.dstPitch);
+  REQUIRE(memCpy_params.dstHeight == memCpyGetParams.dstHeight);
+
+  HIP_CHECK(hipGraphDestroy(graph));
+  HIP_CHECK(hipStreamDestroy(streamForGraph));
+  HIP_CHECK(hipCtxPopCurrent(&context));
+  HIP_CHECK(hipCtxDestroy(context));
+}
+/**
+ * End doxygen group GraphTest.
+ * @}
+ */
--- catch/unit/graph/hipGraphExecMemcpyNodeSetParamsToSymbol.cc
+++ catch/unit/graph/hipGraphExecMemcpyNodeSetParamsToSymbol.cc
@@ -19,12 +19,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#include <hip_test_checkers.hh>
+#include <hip_test_common.hh>
 #include <functional>
 #include <vector>
-
-#include <hip_test_common.hh>
-#include <hip_test_checkers.hh>
-
 #include "graph_memcpy_to_from_symbol_common.hh"
 
 HIP_GRAPH_MEMCPY_FROM_SYMBOL_NODE_DEFINE_GLOBALS(char)
@@ -38,24 +36,27 @@ HIP_GRAPH_MEMCPY_FROM_SYMBOL_NODE_DEFINE_ALTERNATE_GLOBALS(float)
 HIP_GRAPH_MEMCPY_FROM_SYMBOL_NODE_DEFINE_ALTERNATE_GLOBALS(double)
 
 template <typename T>
-void GraphExecMemcpyToSymbolSetParamsShell(const void* symbol, const void* alt_symbol,
-                                           size_t offset, const std::vector<T> set_values) {
-  const auto f = [alt_symbol, is_arr = set_values.size() > 1](const void* symbol, void* src,
-                                                              size_t count, size_t offset,
-                                                              hipMemcpyKind direction) {
+void GraphExecMemcpyToSymbolSetParamsShell(const void *symbol,
+                                           const void *alt_symbol,
+                                           size_t offset,
+                                           const std::vector<T> set_values) {
+  const auto f = [alt_symbol, is_arr = set_values.size() > 1](
+                     const void *symbol, void *src, size_t count, size_t offset,
+                     hipMemcpyKind direction) {
     hipGraph_t graph = nullptr;
     HIP_CHECK(hipGraphCreate(&graph, 0));
 
     hipGraphNode_t node = nullptr;
     HIP_CHECK(hipGraphAddMemcpyNodeToSymbol(
-        &node, graph, nullptr, 0, alt_symbol, reinterpret_cast<T*>(src) + is_arr,
-        count - is_arr * sizeof(T), offset + is_arr * sizeof(T), direction));
+        &node, graph, nullptr, 0, alt_symbol,
+        reinterpret_cast<T *>(src) + is_arr, count - is_arr * sizeof(T),
+        offset + is_arr * sizeof(T), direction));
 
     hipGraphExec_t graph_exec = nullptr;
     HIP_CHECK(hipGraphInstantiate(&graph_exec, graph, nullptr, nullptr, 0));
 
-    HIP_CHECK(hipGraphExecMemcpyNodeSetParamsToSymbol(graph_exec, node, symbol, src, count, offset,
-                                                      direction));
+    HIP_CHECK(hipGraphExecMemcpyNodeSetParamsToSymbol(
+        graph_exec, node, symbol, src, count, offset, direction));
 
     HIP_CHECK(hipGraphLaunch(graph_exec, hipStreamPerThread));
     HIP_CHECK(hipStreamSynchronize(hipStreamPerThread));
@@ -70,26 +71,30 @@ void GraphExecMemcpyToSymbolSetParamsShell(const void* symbol, const void* alt_s
 }
 
 /**
- * @addtogroup hipGraphExecMemcpyNodeSetParamsToSymbol hipGraphExecMemcpyNodeSetParamsToSymbol
+ * @addtogroup hipGraphExecMemcpyNodeSetParamsToSymbol
+ * hipGraphExecMemcpyNodeSetParamsToSymbol
  * @{
  * @ingroup GraphTest
- * `hipGraphExecMemcpyNodeSetParamsToSymbol(hipGraphExec_t hGraphExec, hipGraphNode_t node,
- * const void *symbol, void *src, size_t count, size_t offset, hipMemcpyKind kind)` -
- * Sets the parameters for a memcpy node in the given graphExec to copy to a symbol on the device
+ * `hipGraphExecMemcpyNodeSetParamsToSymbol(hipGraphExec_t hGraphExec,
+ * hipGraphNode_t node, const void *symbol, void *src, size_t count, size_t
+ * offset, hipMemcpyKind kind)` - Sets the parameters for a memcpy node in the
+ * given graphExec to copy to a symbol on the device
  */
 
 /**
  * Test Description
  * ------------------------
- *    - Verify that data is correctly copied to a symbol after node parameters are set following
- * node addition. A graph is constructed to which a MemcpyToSymbol node is added with valid but
- * incorrect parameters. After the graph is instantiated the parameters are updated to correct
- * values and the graph executed. After graph execution, a MemcpyFromSymbol is performed and the
- * copied values are compared against values known to have been copied to symbol memory previously.
- * The test is run for scalar, const scalar, array, and const array symbols of types char, int,
- * float and double. For array symbols, the test is repeated for zero and non-zero offset values.
- * Verification is performed for destination memory allocated on host and device.
- * Test source
+ *    - Verify that data is correctly copied to a symbol after node parameters
+ * are set following node addition. A graph is constructed to which a
+ * MemcpyToSymbol node is added with valid but incorrect parameters. After the
+ * graph is instantiated the parameters are updated to correct values and the
+ * graph executed. After graph execution, a MemcpyFromSymbol is performed and
+ * the copied values are compared against values known to have been copied to
+ * symbol memory previously. The test is run for scalar, const scalar, array,
+ * and const array symbols of types char, int, float and double. For array
+ * symbols, the test is repeated for zero and non-zero offset values.
+ * Verification is performed for destination memory allocated on host and
+ * device. Test source
  * ------------------------
  *    - unit/graph/hipGraphExecMemcpyNodeSetParamsToSymbol.cc
  * Test requirements
@@ -98,23 +103,23 @@ void GraphExecMemcpyToSymbolSetParamsShell(const void* symbol, const void* alt_s
  */
 TEST_CASE("Unit_hipGraphExecMemcpyNodeSetParamsToSymbol_Positive_Basic") {
   SECTION("char") {
-    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(GraphExecMemcpyToSymbolSetParamsShell, 10,
-                                                         char);
+    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(
+        GraphExecMemcpyToSymbolSetParamsShell, 10, char);
   }
 
   SECTION("int") {
-    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(GraphExecMemcpyToSymbolSetParamsShell, 10,
-                                                         int);
+    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(
+        GraphExecMemcpyToSymbolSetParamsShell, 10, int);
   }
 
   SECTION("float") {
-    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(GraphExecMemcpyToSymbolSetParamsShell, 10,
-                                                         float);
+    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(
+        GraphExecMemcpyToSymbolSetParamsShell, 10, float);
   }
 
   SECTION("double") {
-    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(GraphExecMemcpyToSymbolSetParamsShell, 10,
-                                                         double);
+    HIP_GRAPH_MEMCPY_NODE_SET_PARAMS_TO_FROM_SYMBOL_TEST(
+        GraphExecMemcpyToSymbolSetParamsShell, 10, double);
   }
 }
 
@@ -132,8 +137,8 @@ TEST_CASE("Unit_hipGraphExecMemcpyNodeSetParamsToSymbol_Positive_Basic") {
  *      -# kind is illogical (hipMemcpyDeviceToHost)
  *      -# kind is an invalid enum value
  *      -# Changing memcpy direction
- *      -# Changing src to memory allocated on a different device than the original src
- * Test source
+ *      -# Changing src to memory allocated on a different device than the
+ * original src Test source
  * ------------------------
  *    - unit/graph/hipGraphExecMemcpyNodeSetParamsToSymbol.cc
  * Test requirements
@@ -147,35 +152,37 @@ TEST_CASE("Unit_hipGraphExecMemcpyNodeSetParamsToSymbol_Negative_Parameters") {
 
   LinearAllocGuard<int> var(LinearAllocs::hipMalloc, sizeof(int));
   hipGraphNode_t node = nullptr;
-  HIP_CHECK(hipGraphAddMemcpyNodeToSymbol(&node, graph, nullptr, 0, SYMBOL(int_device_var),
-                                          var.ptr(), sizeof(*var.ptr()), 0, hipMemcpyDefault));
+  HIP_CHECK(hipGraphAddMemcpyNodeToSymbol(
+      &node, graph, nullptr, 0, SYMBOL(int_device_var), var.ptr(),
+      sizeof(*var.ptr()), 0, hipMemcpyDefault));
 
   hipGraphExec_t graph_exec = nullptr;
   HIP_CHECK(hipGraphInstantiate(&graph_exec, graph, nullptr, nullptr, 0));
 
   SECTION("hGraphExec == nullptr") {
-    HIP_CHECK_ERROR(
-        hipGraphExecMemcpyNodeSetParamsToSymbol(nullptr, node, SYMBOL(int_device_var), var.ptr(),
-                                                sizeof(*var.ptr()), 0, hipMemcpyDefault),
-        hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphExecMemcpyNodeSetParamsToSymbol(
+                        nullptr, node, SYMBOL(int_device_var), var.ptr(),
+                        sizeof(*var.ptr()), 0, hipMemcpyDefault),
+                    hipErrorInvalidValue);
   }
 
   SECTION("node == nullptr") {
-    HIP_CHECK_ERROR(
-        hipGraphExecMemcpyNodeSetParamsToSymbol(graph_exec, nullptr, SYMBOL(int_device_var),
-                                                var.ptr(), sizeof(*var.ptr()), 0, hipMemcpyDefault),
-        hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphExecMemcpyNodeSetParamsToSymbol(
+                        graph_exec, nullptr, SYMBOL(int_device_var), var.ptr(),
+                        sizeof(*var.ptr()), 0, hipMemcpyDefault),
+                    hipErrorInvalidValue);
   }
 
   MemcpyToSymbolCommonNegative(
-      std::bind(hipGraphExecMemcpyNodeSetParamsToSymbol, graph_exec, node, _1, _2, _3, _4, _5),
+      std::bind(hipGraphExecMemcpyNodeSetParamsToSymbol, graph_exec, node, _1,
+                _2, _3, _4, _5),
       SYMBOL(int_device_var), var.ptr(), sizeof(*var.ptr()));
 
   SECTION("Changing memcpy direction") {
-    HIP_CHECK_ERROR(
-        hipGraphExecMemcpyNodeSetParamsToSymbol(graph_exec, node, SYMBOL(int_device_var), var.ptr(),
-                                                sizeof(*var.ptr()), 0, hipMemcpyHostToDevice),
-        hipErrorInvalidValue);
+    HIP_CHECK_ERROR(hipGraphExecMemcpyNodeSetParamsToSymbol(
+                        graph_exec, node, SYMBOL(int_device_var), var.ptr(),
+                        sizeof(*var.ptr()), 0, hipMemcpyHostToDevice),
+                    hipErrorInvalidValue);
   }
 
   SECTION("Changing src allocation device") {
@@ -185,9 +192,9 @@ TEST_CASE("Unit_hipGraphExecMemcpyNodeSetParamsToSymbol_Negative_Parameters") {
       HIP_CHECK(hipSetDevice(1));
       LinearAllocGuard<int> new_var(LinearAllocs::hipMalloc, sizeof(int));
       HIP_CHECK_ERROR(hipGraphExecMemcpyNodeSetParamsToSymbol(
-                          graph_exec, node, SYMBOL(int_device_var), new_var.ptr(),
-                          sizeof(*new_var.ptr()), 0, static_cast<hipMemcpyKind>(-1)),
-                      hipErrorInvalidValue);
+                          graph_exec, node, SYMBOL(int_device_var),
+                          new_var.ptr(), sizeof(*new_var.ptr()),
+                          0, hipMemcpyDefault), hipErrorInvalidValue);
     }
   }
 
@@ -196,6 +203,6 @@ TEST_CASE("Unit_hipGraphExecMemcpyNodeSetParamsToSymbol_Negative_Parameters") {
 }
 
 /**
-* End doxygen group GraphTest.
-* @}
-*/
+ * End doxygen group GraphTest.
+ * @}
+ */

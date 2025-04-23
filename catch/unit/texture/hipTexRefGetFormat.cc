00c3baf03a23d4297f300f565d958e32bb3004c9
Sang, Tao <Tao.Sang@amd.com>
2025-04-23 11:50:13 -0400

    SWDEV-507388 - Check image support. (#6)
    Change-Id: Ieda106374eb066cd22ddd1194a9f9b1352c26d16
    
    [ROCm/hip-tests commit: 7bc6eda321e92161bda7a56b67e6ea692ec4f69b]

--- catch/unit/texture/hipTexRefGetFormat.cc
+++ catch/unit/texture/hipTexRefGetFormat.cc
@@ -24,6 +24,7 @@ THE SOFTWARE.
 texture<float, 2, hipReadModeElementType> tex;
 
 TEST_CASE("Unit_hipTexRefGetFormat_Basic") {
+  CHECK_IMAGE_SUPPORT
   hipModule_t module = nullptr;
   hipTexRef tex_ref = nullptr;
   int num_channels = 0;
@@ -44,6 +45,7 @@ TEST_CASE("Unit_hipTexRefGetFormat_Basic") {
 }
 
 TEST_CASE("Unit_hipTexRefGetFormat_Positive") {
+  CHECK_IMAGE_SUPPORT
   hipModule_t module = nullptr;
   hipTexRef tex_ref = nullptr;
   int num_channels = 1;
@@ -74,6 +76,7 @@ TEST_CASE("Unit_hipTexRefGetFormat_Positive") {
 }
 
 TEST_CASE("Unit_hipTexRefGetFormat_Negative") {
+  CHECK_IMAGE_SUPPORT
   hipModule_t module = nullptr;
   hipTexRef tex_ref = nullptr;
   int num_channels = 1;
--- catch/unit/texture/hipTexRefSetFormat.cc
+++ catch/unit/texture/hipTexRefSetFormat.cc
@@ -24,6 +24,7 @@ THE SOFTWARE.
 texture<float, 2, hipReadModeElementType> tex;
 
 TEST_CASE("Unit_hipTexRefSetFormat_Positive") {
+  CHECK_IMAGE_SUPPORT
   hipModule_t module = nullptr;
   hipTexRef tex_ref = nullptr;
   int num_channels = 0;
@@ -44,6 +45,7 @@ TEST_CASE("Unit_hipTexRefSetFormat_Positive") {
 }
 
 TEST_CASE("Unit_hipTexRefSetFormat_Negative") {
+  CHECK_IMAGE_SUPPORT
   hipModule_t module = nullptr;
   hipTexRef tex_ref = nullptr;
   int num_channels = 1;

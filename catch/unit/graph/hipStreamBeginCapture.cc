556cdc1858cad6dd966866800073271ad069ce77
Rahul Manocha <rmanocha@amd.com>
2025-04-30 22:30:49 +0000

    SWDEV-525244 - Additional graph validation test

--- catch/unit/graph/hipStreamBeginCapture.cc
+++ catch/unit/graph/hipStreamBeginCapture.cc
@@ -33,6 +33,8 @@ THE SOFTWARE.
 
 static int gCbackIter = 0;
 
+__device__ int kernel_cnt = 0;
+
 static __global__ void dummyKernel() { return; }
 
 static __global__ void incrementKernel(int* data) {
@@ -1566,6 +1568,77 @@ TEST_CASE("Unit_hipStreamBeginCapture_StreamSync_OngoingCapture_MThread") {
     HIP_CHECK(hipGraphExecDestroy(graphExec));
     HIP_CHECK(hipGraphDestroy(graph));
   }
+}
+
+TEST_CASE("Unit_hipStreamBeginCapture_MultipleStreams_ReuseEvent") {
+  // Allocate streams
+  hipStream_t str0, str1, str2;
+  hipEvent_t ev0, ev1, ev2;
+  // Instantiate graph for str0
+  hipGraph_t graph0;
+  hipGraphExec_t graphExec0;
+  // Instantiate graph for str1
+  hipGraph_t graph1;
+  hipGraphExec_t graphExec1;
+ 
+  // Create streams
+  HIP_CHECK(hipStreamCreate(&str0));
+  HIP_CHECK(hipStreamCreate(&str1));
+  HIP_CHECK(hipStreamCreate(&str2));
+ 
+  // Create events
+  HIP_CHECK(hipEventCreate(&ev0));
+  HIP_CHECK(hipEventCreate(&ev1));
+  HIP_CHECK(hipEventCreate(&ev2));
+
+  // Enable capture on streams str0 and str1
+  HIP_CHECK(hipStreamBeginCapture(str0, hipStreamCaptureModeGlobal));
+  // str1 in relaxed mode so hipGraphInstantiate can be called on cuda without error
+  HIP_CHECK(hipStreamBeginCapture(str1, hipStreamCaptureModeGlobal));
+
+  dummyKernel<<<1, 1, 0, str0>>>();
+  HIP_CHECK(hipEventRecord(ev0, str0));
+ 
+  HIP_CHECK(hipEventRecord(ev1, str1));
+ 
+  HIP_CHECK(hipStreamWaitEvent(str2, ev0, 0));
+  dummyKernel<<<1, 1, 0, str2>>>();
+  HIP_CHECK(hipEventRecord(ev2, str2));
+
+  HIP_CHECK(hipStreamWaitEvent(str0, ev2, 0));
+  dummyKernel<<<1, 1, 0, str0>>>();
+ 
+  // Instantiate graph for str0
+  HIP_CHECK(hipStreamEndCapture(str0, &graph0));
+  HIP_CHECK(hipGraphInstantiate(&graphExec0, graph0, nullptr, nullptr, 0));
+  HIP_CHECK(hipGraphDestroy(graph0));
+ 
+  HIP_CHECK(hipStreamWaitEvent(str2, ev1, 0));
+  HIP_CHECK(hipEventRecord(ev2, str2));
+
+  HIP_CHECK(hipStreamWaitEvent(str1, ev2, 0));
+  dummyKernel<<<1, 1, 0, str1>>>();
+  HIP_CHECK(hipGetLastError());
+ 
+  HIP_CHECK(hipStreamEndCapture(str1, &graph1));
+
+  HIP_CHECK(hipGraphLaunch(graphExec0, str0));
+  HIP_CHECK(hipStreamSynchronize(str0));
+  HIP_CHECK(hipGraphExecDestroy(graphExec0));
+
+  HIP_CHECK(hipGraphInstantiate(&graphExec1, graph1, nullptr, nullptr, 0));
+  HIP_CHECK(hipGraphLaunch(graphExec1, str1));
+  HIP_CHECK(hipStreamSynchronize(str1));
+  HIP_CHECK(hipGraphExecDestroy(graphExec1));
+  HIP_CHECK(hipGraphDestroy(graph1));
+ 
+  // Clean up resources
+  HIP_CHECK(hipEventDestroy(ev0));
+  HIP_CHECK(hipEventDestroy(ev1));
+  HIP_CHECK(hipEventDestroy(ev2));
+  HIP_CHECK(hipStreamDestroy(str0));
+  HIP_CHECK(hipStreamDestroy(str1));
+  HIP_CHECK(hipStreamDestroy(str2));
 }
 
 /**

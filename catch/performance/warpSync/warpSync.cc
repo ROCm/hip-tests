b6e0bb62f53937f4578d2b5bc4943cbd3d88568e
Hernandez, Gerardo <Gerardo.Hernandez@amd.com>
2025-05-14 11:57:05 +0100

    SWDEV-420237 - Add tests for reduce sync operations (#102)
    [ROCm/hip-tests commit: 001affc6ae5194b5bdd5f5218855c1be69538d66]

---
+++ catch/performance/warpSync/warpSync.cc
@@ -0,0 +1,395 @@
+/*
+Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#define HIP_ENABLE_WARP_SYNC_BUILTINS
+#define HIP_ENABLE_EXTRA_WARP_SYNC_TYPES
+
+#include "warp_common.hh"
+#include <hip/hip_runtime.h>
+#include <hip/hip_fp16.h>
+#include <hip_test_common.hh>
+#include <performance_common.hh>
+#include <hip/amd_detail/amd_hip_atomic.h>
+#include <resource_guards.hh>
+#include <cmd_options.hh>
+#include <algorithm>
+#include <type_traits>
+#include <limits>
+#include <map>
+
+/**
+* @addtogroup __reduce_op_sync __reduce_op_sync
+* @{
+* @ingroup WarpSyncPerformance
+* __reduce_op_sync(MaskT mask, T val)
+* Reduces the val as per the lanes described in mask and calculates the
+* aggregated result
+*/
+
+static constexpr int kBlockDim = 1024;
+
+template <class T>
+struct AtomicAddOp {
+  __device__ T operator()(T* lhs, const T& rhs)
+  {
+    return atomicAdd(lhs, rhs);
+  }
+};
+
+template <class T>
+struct AtomicMinOp {
+  __device__ T operator()(T* lhs, const T& rhs)
+  {
+    return atomicMin(lhs, rhs);
+  }
+};
+
+template <class T>
+struct AtomicMaxOp {
+  __device__ T operator()(T* lhs, const T& rhs)
+  {
+    return atomicMax(lhs, rhs);
+  }
+};
+
+template <class T>
+struct AtomicAndOp {
+  __device__ T operator()(T* lhs, const T& rhs)
+  {
+    return atomicAnd(lhs, rhs);
+  }
+};
+
+template <class T>
+struct AtomicOrOp {
+  __device__ T operator()(T* lhs, const T& rhs)
+  {
+    return atomicOr(lhs, rhs);
+  }
+};
+
+template <class T>
+struct AtomicXorOp {
+  __device__ T operator()(T* lhs, const T& rhs)
+  {
+    return atomicXor(lhs, rhs);
+  }
+};
+
+// uses atomics to reduce the whole warp; depending on the mask our reduce should be faster
+// @output   to store the result, one per warp
+// @numItems must be a multiple of warpSize
+template <class T, template <typename> class Op>
+__global__ void reduceAllAtomics(T* __restrict__ output, const T* __restrict__ input, unsigned long long mask)
+{
+  int idx = threadIdx.x + blockIdx.x * kBlockDim;
+  __shared__ T result[kBlockDim / warpSize]; // one per warp
+  Op<T> op;
+  int numWarp = threadIdx.x / warpSize;
+
+  // initialize result[numWarp] to the "identity" element for Op
+  if constexpr (std::is_same<Op<T>, AtomicMinOp<T>>::value)
+    result[numWarp] = std::numeric_limits<T>::max();
+  else if constexpr (std::is_same<Op<T>, AtomicMaxOp<T>>::value)
+    result[numWarp] = std::numeric_limits<T>::lowest();
+  else if constexpr (std::is_same<Op<T>, AtomicAndOp<T>>::value)
+    result[numWarp] = 1;
+  else
+    result[numWarp] = 0;
+
+  __syncthreads();
+
+  if (mask & (1ul << __ockl_lane_u32()))
+    op(&result[numWarp], input[idx]);
+
+  __syncthreads();
+
+  if (__ockl_lane_u32() == 0)
+    output[idx / warpSize] = result[numWarp];
+}
+
+template <class T, template<typename> class Op>
+__global__ void reduceOpSync(T* __restrict__ output, const T* __restrict__ input, unsigned long long mask)
+{
+  int idx = threadIdx.x + blockIdx.x * kBlockDim;
+  T result;
+
+  if (mask & (1ul << __ockl_lane_u32())) {
+    if constexpr (std::is_same<Op<T>, std::plus<T>>::value)
+      result = __reduce_add_sync(mask, input[idx]);
+    else if constexpr (std::is_same<Op<T>, MinOp<T>>::value)
+      result = __reduce_min_sync(mask, input[idx]);
+    else if constexpr (std::is_same<Op<T>, MaxOp<T>>::value)
+      result = __reduce_max_sync(mask, input[idx]);
+    else if constexpr (std::is_same<Op<T>, std::logical_and<T>>::value)
+      result = __reduce_and_sync(mask, input[idx]);
+    else if constexpr (std::is_same<Op<T>, std::logical_or<T>>::value)
+      result = __reduce_or_sync(mask, input[idx]);
+    else if constexpr (std::is_same<Op<T>, XorOp<T>>::value)
+      result = __reduce_xor_sync(mask, input[idx]);
+    else
+      static_assert(std::is_void<T>::value, "Unsupported operator");
+
+    if (__ockl_activelane_u32() == 0)
+      output[idx / warpSize] = result;
+  }
+}
+
+template <class T, template <typename> class Op>
+class AtomicBenchmark : public Benchmark<AtomicBenchmark<T, Op>> {
+public:
+  void operator()(T* output, const T* input, int numItems, unsigned long long mask)
+  {
+    dim3 blockDim = { kBlockDim };
+    dim3 gridDim = { static_cast<uint32_t>(std::ceil(numItems / static_cast<float>(blockDim.x))) };
+
+    TIMED_SECTION(kTimerTypeEvent) {
+      if constexpr (std::is_same<Op<T>, std::plus<T>>::value)
+        reduceAllAtomics<T, AtomicAddOp><<<gridDim, blockDim>>>(output, input, mask);
+      else if constexpr (std::is_same<Op<T>, MinOp<T>>::value)
+        reduceAllAtomics<T, AtomicMinOp><<<gridDim, blockDim>>>(output, input, mask);
+      else if constexpr (std::is_same<Op<T>, MaxOp<T>>::value)
+        reduceAllAtomics<T, AtomicMaxOp><<<gridDim, blockDim>>>(output, input, mask);
+      else if constexpr (std::is_same<Op<T>, std::logical_and<T>>::value)
+        reduceAllAtomics<T, AtomicAndOp><<<gridDim, blockDim>>>(output, input, mask);
+      else if constexpr (std::is_same<Op<T>, std::logical_or<T>>::value)
+        reduceAllAtomics<T, AtomicOrOp><<<gridDim, blockDim>>>(output, input, mask);
+      else if constexpr (std::is_same<Op<T>, XorOp<T>>::value)
+        reduceAllAtomics<T, AtomicXorOp><<<gridDim, blockDim>>>(output, input, mask);
+      else
+        static_assert(std::is_void<T>::value, "Unsupported operator");
+
+      HIP_CHECK(hipDeviceSynchronize());
+    }
+  }
+};
+
+template <class T, template <typename> class Op>
+class ReduceSyncBenchmark : public Benchmark<ReduceSyncBenchmark<T, Op>> {
+public:
+  void operator()(T* output, T* input, int numItems, unsigned long long mask)
+  {
+    dim3 blockDim = { kBlockDim };
+    dim3 gridDim = { static_cast<uint32_t>(std::ceil(numItems / static_cast<float>(blockDim.x))) };
+
+
+    TIMED_SECTION(kTimerTypeEvent) {
+      reduceOpSync<T, Op><<<gridDim, blockDim>>>(output, input, mask);
+      HIP_CHECK(hipDeviceSynchronize());
+    }
+  }
+};
+
+template <class T, template <typename> class Op>
+void checkResults(T* d_atomicsResult, T* d_reduceResult, size_t numBytes, unsigned long long mask)
+{
+  using namespace Catch::Matchers;
+  LinearAllocGuard<T> outputAtomic(LinearAllocs::malloc, numBytes);
+  LinearAllocGuard<T> outputReduce(LinearAllocs::malloc, numBytes);
+  bool memcmpResult = std::memcmp(outputAtomic.ptr(), outputReduce.ptr(), numBytes);
+
+  assert(numBytes % sizeof(T) == 0 && "numBytes needs to be a multiple of sizeof(T)");
+  HIP_CHECK(hipMemcpy(outputAtomic.ptr(), d_atomicsResult, numBytes, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipMemcpy(outputReduce.ptr(), d_reduceResult, numBytes, hipMemcpyDeviceToHost));
+
+  if (memcmpResult) {
+    for (int i = 0; i < numBytes / sizeof(T); i++) {
+      auto& atomicResult = outputAtomic.ptr()[i];
+      auto& reduceResult = outputReduce.ptr()[i];
+
+      if constexpr (std::is_integral<T>::value || std::is_same<Op<T>, MinOp<T>>::value ||
+                    std::is_same<Op<T>, MaxOp<T>>::value)
+        // for integral types or min/max the result should match exactly
+        REQUIRE(atomicResult == reduceResult);
+      else
+        // floating point types or operations which are lossy in terms of precision
+        REQUIRE_THAT(reduceResult, WithinRel(atomicResult));
+    }
+  }
+}
+
+template <class T, template <typename> class Op>
+struct IsLogicalOp {
+  static constexpr bool value = false;
+};
+
+template <class T>
+struct IsLogicalOp<T, std::logical_and> {
+  static constexpr bool value = true;
+};
+
+template <class T>
+struct IsLogicalOp<T, std::logical_or> {
+  static constexpr bool value = true;
+};
+
+template <class T>
+struct IsLogicalOp<T, XorOp> {
+  static constexpr bool value = true;
+};
+
+// Neither long long or fp16 have atomic operations. In those cases
+// we only benchmark reduce sync operations, we cannot compare with native atomics
+template <class T>
+struct HasAtomicOps {
+  static constexpr bool value = true;
+};
+
+template <>
+struct HasAtomicOps<half> {
+  static constexpr bool value = false;
+};
+
+template <>
+struct HasAtomicOps<long long> {
+  static constexpr bool value = false;
+};
+
+template <class T, template <typename> class Op>
+struct ReduceBenchmark {
+  void Run()
+  {
+    static constexpr int numMasks = 6;
+    using distribution = typename DistributionType<T>::type;
+    ReduceSyncBenchmark<T, Op> benchmarkReduce;
+    uint64_t inputSize = cmd_options.reduce_input_size * 1_MB;
+    int numItems = inputSize / sizeof(T);
+    int wavefrontSize = getWarpSize();
+    int outputNumBytes = inputSize / wavefrontSize;
+    LinearAllocGuard<T> input(LinearAllocs::malloc, inputSize);
+    LinearAllocGuard<T> d_input(LinearAllocs::hipMalloc, inputSize);
+    LinearAllocGuard<T> d_outputsAtomic[numMasks];
+    LinearAllocGuard<T> d_outputsReduce[numMasks];
+    LinearAllocGuard<T>* d_outputAtomic = &d_outputsAtomic[0];
+    LinearAllocGuard<T>* d_outputReduce = &d_outputsReduce[0];
+    std::mt19937_64 gen(123);
+    distribution dist;
+    int halfWaveSize = wavefrontSize / 2;
+    unsigned long long halfBitsOn = (1ul << (wavefrontSize / 2)) - 1;
+    unsigned long long fullMask = -1ul,
+                       halfHighBitsOn = halfBitsOn << halfWaveSize,
+                       high16BitsOn = halfBitsOn << (wavefrontSize - 16),
+                       high8BitsOn = halfBitsOn << (wavefrontSize - 8),
+                       high4BitsOn = halfBitsOn << (wavefrontSize - 4),
+                       allButOne = -1 & ~1;
+    const char* typeStr = typeToString<T>();
+    const char* opStr = opToString<T, Op>();
+    std::map<std::string, unsigned long long> masks;
+    std::pair<std::string, unsigned long long> masksPairs[] = { { "full mask", fullMask },
+                                                                { "high order 32 bits on", halfHighBitsOn },
+                                                                { "high order 16 bits on", high16BitsOn },
+                                                                { "high order 8 bits on", high8BitsOn },
+                                                                { "high order 4 bits on", high4BitsOn },
+                                                                { "all but one", allButOne } };
+    int pos = 0, numMask = 0;
+
+    for (auto& mask : masksPairs) {
+      // don't use 'halfHighBitsOn' on warp size 32; it's the same as high16BitsOn
+      if (wavefrontSize != 32 || mask.second != halfHighBitsOn) {
+        masks.emplace(std::to_string(numMask) + " - " + mask.first, wavefrontSize == 64? mask.second : mask.second & 0xFFFFFFFF);
+        numMask++;
+      }
+    }
+
+    // avoid generating values different than 1 or 0 for logical operators;
+    // otherwise the atomic version of the kernels would produce different results as
+    // atomicAnd/Or() are bitwise operations, not logical
+    if constexpr (IsLogicalOp<T, Op>::value)
+      dist = distribution(0, 1);
+
+    for (int i = 0; i < numItems; i++) {
+      input.ptr()[i] = dist(gen);
+    }
+
+    for (auto& buffer : d_outputsAtomic) {
+      buffer = LinearAllocGuard<T>(LinearAllocs::hipMalloc, outputNumBytes);
+    }
+
+    for (auto& buffer : d_outputsReduce) {
+      buffer = LinearAllocGuard<T>(LinearAllocs::hipMalloc, outputNumBytes);
+    }
+
+    HIP_CHECK(hipMemcpy(d_input.ptr(), input.ptr(), inputSize, hipMemcpyHostToDevice));
+
+    if constexpr (HasAtomicOps<T>::value) {
+      AtomicBenchmark<T, Op> benchmarkAtomics;
+
+      printf("\n--- atomics %s %s---\n", opStr, typeStr);
+
+      for (auto& mask : masks) {
+        printf("%s %llx\n", mask.first.c_str(), mask.second);
+        benchmarkAtomics.Run((d_outputAtomic++)->ptr(), d_input.ptr(), numItems, mask.second);
+      }
+    }
+
+    printf("\n--- reduce %s %s--- \n", opStr, typeStr);
+
+    for (const auto& mask : masks) {
+      printf("%s %llx\n", mask.first.c_str(), mask.second);
+      benchmarkReduce.Run((d_outputReduce++)->ptr(), d_input.ptr(), numItems, mask.second);
+    }
+
+    printf("\n");
+
+    if constexpr (HasAtomicOps<T>::value) {
+      printf("Checking results...\n");
+
+      for (const auto& mask : masks) {
+        checkResults<T, Op>(d_outputsAtomic[pos].ptr(), d_outputsReduce[pos].ptr(), outputNumBytes, mask.second);
+        pos++;
+      }
+    }
+  }
+};
+
+TEMPLATE_TEST_CASE("Performance_Reduce_Sync_Add", "", int, unsigned int, unsigned long long,
+                   long long, float, half, double) {
+  ReduceBenchmark<TestType, std::plus> benchmark;
+
+  benchmark.Run();
+}
+
+TEMPLATE_TEST_CASE("Performance_Reduce_Sync_Min", "", int, unsigned int, unsigned long long, long long, float, half, double) {
+  ReduceBenchmark<TestType, MinOp> benchmark;
+
+  benchmark.Run();
+}
+
+TEMPLATE_TEST_CASE("Performance_Reduce_Sync_Max", "", int, unsigned int, unsigned long long, long long, float, half, double) {
+  ReduceBenchmark<TestType, MaxOp> benchmark;
+
+  benchmark.Run();
+}
+
+TEMPLATE_TEST_CASE("Performance_Reduce_Sync_And", "", int, unsigned int, unsigned long long, long long) {
+  ReduceBenchmark<TestType, std::logical_and> benchmark;
+
+  benchmark.Run();
+}
+
+TEMPLATE_TEST_CASE("Performance_Reduce_Sync_Or", "", int, unsigned int, unsigned long long, long long) {
+  ReduceBenchmark<TestType, std::logical_or> benchmark;
+
+  benchmark.Run();
+}
+
+TEMPLATE_TEST_CASE("Performance_Reduce_Sync_Xor", "", int, unsigned int, unsigned long long, long long) {
+  ReduceBenchmark<TestType, XorOp> benchmark;
+
+  benchmark.Run();
+}
---
+++ catch/unit/warp/warp_reduce.cc
@@ -0,0 +1,230 @@
+/*
+Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#define HIP_ENABLE_WARP_SYNC_BUILTINS
+#define HIP_ENABLE_EXTRA_WARP_SYNC_TYPES
+
+#include <hip_test_common.hh>
+#include "warp_common.hh"
+#include <hip/hip_runtime.h>
+#include <hip/hip_fp16.h>
+#include <resource_guards.hh>
+#include <memory>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <cstdlib>
+#include <cmd_options.hh>
+#include <tuple>
+
+#define NELEMS(array) (sizeof(array) / sizeof(array[0]))
+
+template <class T>
+// @input an array containing one value per lane to be used as input for the reduction
+// @masks a list of masks, none of them sharing bits
+__global__ void multipleMasksKernel(T* output, const T* input, const unsigned long long* masks, int numMasks)
+{
+  bool isInAnyOfTheMasks = false;
+  int numMask = 0;
+  unsigned long long mask;
+
+  while (numMask < numMasks && !isInAnyOfTheMasks) {
+    mask = masks[numMask];
+    if ((1ul << threadIdx.x) & mask)
+      isInAnyOfTheMasks = true;
+
+    numMask++;
+  }
+
+  if (!isInAnyOfTheMasks)
+    return;
+
+  output[threadIdx.x] = __reduce_add_sync<decltype(mask)>(mask, input[threadIdx.x]);
+}
+
+template <class T, class Op, class MaskType>
+__global__ void reduceOp(T* output, const T* input, const MaskType* masks, int numReduces, Op)
+{
+  int tid = threadIdx.x;
+
+  for (int i = 0; i < numReduces; i++) {
+    if (masks[i] & (1ul << tid)) {
+      // call the operator only if the lane is mentioned in the mask
+      T& result = output[warpSize * i + tid];
+
+      if constexpr (std::is_same<Op, std::plus<T>>::value)
+        result = __reduce_add_sync(masks[i], input[tid]);
+      else if constexpr (std::is_same<Op, MinOp<T>>::value)
+        result = __reduce_min_sync(masks[i], input[tid]);
+      else if constexpr (std::is_same<Op, MaxOp<T>>::value)
+        result = __reduce_max_sync(masks[i], input[tid]);
+      else if constexpr (std::is_same<Op, std::logical_and<T>>::value)
+        result = __reduce_and_sync(masks[i], input[tid]);
+      else if (std::is_same<Op, std::logical_or<T>>::value)
+        result = __reduce_or_sync(masks[i], input[tid]);
+      else if (std::is_same<Op, XorOp<T>>::value)
+        result = __reduce_xor_sync(masks[i], input[tid]);
+      else
+        assert(false && "Unsupported operator");
+    }
+  }
+}
+
+template <class T>
+void runTestMultipleMasks(unsigned long long masks[], int numMasks)
+{
+  using namespace Catch::Matchers;
+  using distribution = typename DistributionType<T>::type;
+  unsigned int wavefrontSize = getWarpSize();
+  LinearAllocGuard<unsigned long long> d_masks(LinearAllocs::hipMalloc, numMasks * sizeof(decltype(masks[0])));
+  LinearAllocGuard<T> d_input, input;
+  LinearAllocGuard<T> output(LinearAllocs::malloc, wavefrontSize * sizeof(T));
+  LinearAllocGuard<T> d_output(LinearAllocs::hipMalloc, wavefrontSize * sizeof(T));
+  std::plus<T> op;
+  std::mt19937_64 gen(123);
+  T a = std::is_same<T, half>::value? std::numeric_limits<unsigned short>::lowest() : -1023;
+  T b = std::is_same<T, half>::value? std::numeric_limits<unsigned short>::max() : 1023;
+  distribution distInput(a, b);
+  dim3 blkDim { wavefrontSize };
+  dim3 grdDim { 1u };
+
+  HIP_CHECK(hipMemcpy(d_masks.ptr(), &masks[0], d_masks.size_bytes(), hipMemcpyHostToDevice));
+  genRandomBuffers(d_input, input, distInput, gen, wavefrontSize);
+  multipleMasksKernel<T><<<grdDim, blkDim>>>(d_output.ptr(), d_input.ptr(), d_masks.ptr(), numMasks);
+  HIP_CHECK(hipMemcpy(output.ptr(), d_output.ptr(), d_output.size_bytes(), hipMemcpyDeviceToHost));
+
+  for (int numMask = 0; numMask < numMasks; numMask++) {
+    unsigned long long mask = masks[numMask];
+    T expected = calculateExpected<T>(input.ptr(), op, mask);
+    int lane = 0;
+
+    while (lane < wavefrontSize) {
+      if ((1ul << lane) & mask) {
+        T result = output.ptr()[lane];
+
+        if constexpr (std::is_integral<T>::value) {
+          // for integral types the result should match exactly
+          if (result != expected) {
+            printMismatch(result, expected, input.ptr(), mask);
+            REQUIRE(result == expected);
+          }
+        } else
+          compareFloatingPoint(result, expected, mask, input.ptr());
+      }
+
+      lane++;
+    }
+  }
+}
+
+TEMPLATE_TEST_CASE("Unit_hipReduceSingleMasks", "", int, unsigned int, long long, unsigned long long, float, half, double) {
+  unsigned long long fullMask = getWarpSize() == 64? ~0ul : 0xFFFFFFFF;
+  unsigned long long oneBitMasks[] = { 0b1 & fullMask};
+  unsigned long long everyFifthMasks[] = { Every5thBit & fullMask };
+  unsigned long long everyNinethMasks[] = { Every9thBit & fullMask };
+  unsigned long long everyFifthButNinethMasks[] = { Every5thBut9th & fullMask};
+
+  runTestMultipleMasks<TestType>(oneBitMasks, NELEMS(oneBitMasks));
+  runTestMultipleMasks<TestType>(everyFifthMasks, NELEMS(everyFifthMasks));
+  runTestMultipleMasks<TestType>(everyNinethMasks, NELEMS(everyNinethMasks));
+  runTestMultipleMasks<TestType>(everyFifthButNinethMasks, NELEMS(everyFifthButNinethMasks));
+}
+
+TEMPLATE_TEST_CASE("Unit_hipReduceMultipleMasks", "",
+                   int, unsigned int, long long, unsigned long long, float, half, double) {
+  if (getWarpSize() == 64) {
+    unsigned long long masks[] = { 0b0110011, 0x0F0F0F0F00000000, 0xF0F0F0F000000000,
+                                 0x000000000F0F0F00, 0b0000100};
+    // these divergent masks, when combined, occupy the whole set of lanes
+    unsigned long long fullMasks[] = { 0xFFFF000000000000, 0x0000FFFFFFFF0000, 0x000000000000FFFF};
+    unsigned long long fullMasksEvenOdd[] = { 0x5555555555555555, // even lanes
+                                              0xAAAAAAAAAAAAAAAA }; // odd lanes
+
+    runTestMultipleMasks<TestType>(masks, NELEMS(masks));
+    runTestMultipleMasks<TestType>(fullMasks, NELEMS(fullMasks));
+    runTestMultipleMasks<TestType>(fullMasksEvenOdd, NELEMS(fullMasksEvenOdd));
+  } else {
+    unsigned long long masks1[] = { 0x0F0F0F0F, 0xF0F0F0F0 };
+    unsigned long long masks2[] = { 0b0110011, 0x0F0F0F00, 0b0000100};
+    runTestMultipleMasks<TestType>(masks1, NELEMS(masks1));
+    runTestMultipleMasks<TestType>(masks2, NELEMS(masks2));
+  }
+}
+
+template <template <typename> class Op, class Type = void>
+void runTestReduceForTypes(const std::tuple<>)
+{
+}
+
+template <template <typename> class Op, class T, typename... Types>
+void runTestReduceForTypes(const std::tuple<T, Types...>)
+{
+  unsigned int wavefrontSize = getWarpSize();
+  dim3 blkDim { wavefrontSize };
+  dim3 grdDim { 1u };
+  std::tuple<Types...> remainingTypes;
+  int iteration = 0;
+  auto reduceFunc = [&](T* d_output, const T* d_input, const unsigned long long* d_masks, int numReduces, Op<T> op) {
+    reduceOp<T><<<grdDim, blkDim>>>(d_output, d_input, d_masks, numReduces, op);
+  };
+  bool customNumIterations = cmd_options.reduce_iterations != 1;
+
+  if (customNumIterations)
+    std::cout << "\n" << opToString<T, Op>() << " - " << typeToString<T>() << "\n";
+
+  while (iteration < cmd_options.reduce_iterations) {
+    runTestReduce<T, decltype(reduceFunc), Op>(iteration, reduceFunc);
+    iteration++;
+
+    if (customNumIterations) {
+      std::cout << "\rIteration: " << iteration;
+      std::flush(std::cout);
+    }
+  }
+
+  runTestReduceForTypes<Op>(remainingTypes);
+}
+
+TEST_CASE("Unit_hipReduceRandom") {
+  const std::tuple<int, unsigned int, long long, unsigned long long, float, half, double> allTypes;
+  const std::tuple<int, unsigned int, long long, unsigned long long> integralTypes;
+
+  SECTION("add") {
+    runTestReduceForTypes<std::plus>(allTypes);
+  }
+
+  SECTION("min") {
+    runTestReduceForTypes<MinOp>(allTypes);
+  }
+
+  SECTION("max") {
+    runTestReduceForTypes<MaxOp>(allTypes);
+  }
+
+  SECTION("and") {
+    runTestReduceForTypes<std::logical_and>(integralTypes);
+  }
+
+  SECTION("or") {
+    runTestReduceForTypes<std::logical_or>(integralTypes);
+  }
+
+  SECTION("xor") {
+    runTestReduceForTypes<XorOp>(integralTypes);
+  }
+}

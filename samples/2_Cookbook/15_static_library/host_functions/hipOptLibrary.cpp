62c6a0e853fd8b0b9211929bb66dfd55d72292d8
Patel, Jaydeepkumar <Jaydeepkumar.Patel@amd.com>
2025-07-01 09:58:10 +0530

    SWDEV-539967 - Include missing assert header. (#337)
    [ROCm/hip-tests commit: 2c7c7128eabd31eaa7f0f9c447d1d28b2ec37efd]

--- samples/2_Cookbook/15_static_library/host_functions/hipOptLibrary.cpp
+++ samples/2_Cookbook/15_static_library/host_functions/hipOptLibrary.cpp
@@ -24,6 +24,7 @@
 #include <hip/hip_runtime_api.h>
 #include <iostream>
 #include <stdexcept>
+#include <cassert>
 
 #define HIP_ASSERT(status)                                                                         \
   {                                                                                                \

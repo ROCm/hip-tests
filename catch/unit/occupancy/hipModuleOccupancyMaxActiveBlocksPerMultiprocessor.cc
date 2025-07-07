8ef80bf177e9adc084c6e748781e1139b0d83d37
Swargam, Rambabu <Rambabu.Swargam@amd.com>
2025-07-07 17:42:45 +0530

    SWDEV-491288 - [catch2][dtest] Stream Capture tests for Occupancy API's (#157)
    * SWDEV-491288 - [catch2][dtest] Stream Capture tests for Occupancy API's
    
    * Modified Cmake file
    
    [ROCm/hip-tests commit: ea366c7aa5a8bd2b24e13a12bc06b364740d63eb]

--- catch/unit/occupancy/hipModuleOccupancyMaxActiveBlocksPerMultiprocessor.cc
+++ catch/unit/occupancy/hipModuleOccupancyMaxActiveBlocksPerMultiprocessor.cc
@@ -23,6 +23,7 @@ execution of hipModuleOccupancyMaxActiveBlocksPerMultiprocessor for diffrent par
 Unit_hipModuleOccupancyMaxActiveBlocksPerMultiprocessor_Negative_Parameters - Test unsuccessful
 execution of hipModuleOccupancyMaxActiveBlocksPerMultiprocessor api when parameters are invalid
 */
+#include <hip_test_kernels.hh>
 #include "occupancy_common.hh"
 
 TEST_CASE("Unit_hipModuleOccupancyMaxActiveBlocksPerMultiprocessor_Negative_Parameters") {
@@ -90,3 +91,74 @@ TEST_CASE("Unit_hipModuleOccupancyMaxActiveBlocksPerMultiprocessor_Positive_Rang
 
   HIP_CHECK(hipModuleUnload(module));
 }
+
+/**
+ * Test Description
+ * ------------------------
+ *  - This test case tests the behaviour of all Occupancy APIs
+ *  - during the the stream capture.
+ * Test source
+ * ------------------------
+ *  - unit/occupancy/hipModuleOccupancyMaxActiveBlocksPerMultiprocessor.cc
+ */
+TEST_CASE("Unit_OccupancyAPIs_StreamCapture") {
+  GENERATE_CAPTURE();
+
+  hipStream_t stream;
+  HIP_CHECK(hipStreamCreate(&stream));
+
+  hipModule_t module;
+  HIP_CHECK(hipModuleLoad(&module, "simple_kernel.code"));
+  REQUIRE(module != nullptr);
+  hipFunction_t function;
+  HIP_CHECK(hipModuleGetFunction(&function, module, "SimpleKernel"));
+  REQUIRE(function != nullptr);
+
+  BEGIN_CAPTURE(stream);
+
+  int gridSize = 0, blockSize = 0, numBlocks = 0;
+
+  HIP_CHECK(hipOccupancyMaxPotentialBlockSize(&gridSize, &blockSize,
+            reinterpret_cast<const void *>(HipTest::vectorADD<int>), 0, 0));
+  REQUIRE(gridSize > 0);
+  REQUIRE(blockSize > 0);
+
+  HIP_CHECK(hipOccupancyMaxActiveBlocksPerMultiprocessor(
+            &numBlocks, reinterpret_cast<const void *>(HipTest::vectorADD<int>),
+            blockSize, 0));
+  REQUIRE(numBlocks > 0);
+
+  numBlocks = 0;
+  HIP_CHECK(hipOccupancyMaxActiveBlocksPerMultiprocessorWithFlags(
+            &numBlocks, reinterpret_cast<const void *>(HipTest::vectorADD<int>),
+            blockSize, 0, 0));
+  REQUIRE(numBlocks > 0);
+
+  gridSize = 0, blockSize = 0;
+  HIP_CHECK(hipModuleOccupancyMaxPotentialBlockSize(&gridSize, &blockSize,
+                                                    function, 0, 0));
+  REQUIRE(gridSize > 0);
+  REQUIRE(blockSize > 0);
+
+  gridSize = 0; blockSize = 0;
+  HIP_CHECK(hipModuleOccupancyMaxPotentialBlockSizeWithFlags(
+            &gridSize, &blockSize, function, 0, 0, 0));
+  REQUIRE(gridSize > 0);
+  REQUIRE(blockSize > 0);
+
+  numBlocks = 0;
+  HIP_CHECK(hipModuleOccupancyMaxActiveBlocksPerMultiprocessor(
+            &numBlocks, function, blockSize, 0));
+  REQUIRE(numBlocks > 0);
+
+  numBlocks = 0;
+  HIP_CHECK(hipModuleOccupancyMaxActiveBlocksPerMultiprocessorWithFlags(
+            &numBlocks, function, blockSize, 0, 0));
+  REQUIRE(numBlocks > 0);
+
+  END_CAPTURE(stream);
+
+  HIP_CHECK(hipModuleUnload(module));
+  HIP_CHECK(hipStreamDestroy(stream));
+}
+

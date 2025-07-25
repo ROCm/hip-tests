f51ea2aa6bbdd4b918da1a92336c2cc9707647b9
Ioannis Assiouras <Ioannis.Assiouras@amd.com>
2025-07-25 09:30:39 +0100

    SWDEV-544502 - Skip opengl tests on devices with no image support

--- catch/unit/gl_interop/gl_interop_common.hh
+++ catch/unit/gl_interop/gl_interop_common.hh
@@ -214,6 +214,12 @@ class GLContextScopeGuard {
   static constexpr char kEnvarName[] = "GL_CONTEXT_TYPE";
 
   GLContextScopeGuard() {
+
+    if(!HipTest::isImageSupported()) {
+      HipTest::HIP_SKIP_TEST("Image is not supported on the device. Skipped.");
+      exit(1);
+    }
+
     char* val = std::getenv(kEnvarName);
     std::string val_str = val == NULL ? "" : val;
 

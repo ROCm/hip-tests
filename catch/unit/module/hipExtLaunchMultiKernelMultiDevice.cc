d10ce000565fec138329efe19fdf45928a406c9a
Vladana Stojiljkovic <Vladana.Stojiljkovic@amd.com>
2025-08-07 14:26:23 +0000

    SWDEV-547526 - Add missing free calls

--- catch/unit/module/hipExtLaunchMultiKernelMultiDevice.cc
+++ catch/unit/module/hipExtLaunchMultiKernelMultiDevice.cc
@@ -121,6 +121,7 @@ TEST_CASE("Unit_hipExtLaunchMultiKernelMultiDevice_Functional") {
 
   for (int j = 0; j < nGpu; j++) {
     HIP_CHECK(hipStreamSynchronize(stream[j]));
+    HIP_CHECK(hipStreamDestroy(stream[j]));
 
     hipDeviceProp_t props;
     HIP_CHECK(hipGetDeviceProperties(&props, j));
@@ -132,5 +133,12 @@ TEST_CASE("Unit_hipExtLaunchMultiKernelMultiDevice_Functional") {
     for (size_t i = 0; i < N; i++) {
       REQUIRE(fabs(C_h[i] - (A_h[i] * A_h[i])) < 0.00000000001);
     }
+
+    HIP_CHECK(hipFree(A_d[j]));
+    HIP_CHECK(hipFree(C_d[j]));
   }
+
+  free(A_h);
+  free(C_h);
+  free(launchParamsList);
 }

f54db139a49520e3f35dd48a3e416c2d197b4240
Marko Arandjelovic <Marko.Arandjelovic@amd.com>
2025-08-14 14:58:10 +0000

    SWDEV-548482 - Run clang-format

--- catch/unit/memory/hipMallocManagedFlagsTst.cc
+++ catch/unit/memory/hipMallocManagedFlagsTst.cc
@@ -32,13 +32,11 @@ __global__ void MallcMangdFlgTst(int n, float* x, float* y) {
 
 // The following section tests working of hipMallocManaged with flag parameters
 TEST_CASE("Unit_hipMallocManaged_FlgParam") {
-
   auto managed = HmmAttrPrint();
   if (managed != 1) {
     HipTest::HIP_SKIP_TEST("GPU doesn't support managed memory so skipping test.");
     return;
   }
-
   std::atomic<int> DataMismatch{0};
   bool IfTestPassed = true;
   float *HmmAG = NULL, *HmmAH1 = NULL, *HmmAH2 = NULL, INIT_VAL = 2.5;
@@ -126,7 +124,6 @@ TEST_CASE("Unit_hipMallocManaged_AccessMultiStream") {
     HipTest::HIP_SKIP_TEST("GPU doesn't support managed memory so skipping test.");
     return;
   }
-
   std::atomic<int> DataMismatch{0};
   bool IfTestPassed = true;
   float *HmmAG = NULL, *HmmAH1 = NULL, *HmmAH2 = NULL, INIT_VAL = 2.5;

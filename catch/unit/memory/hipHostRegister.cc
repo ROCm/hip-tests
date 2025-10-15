afe4069e2c338ea336dda26f7786440af6c1898a
JeniferC99 <150404595+JeniferC99@users.noreply.github.com>
2025-10-15 13:32:41 -0700

    Revert "SWDEV-554608 - Add hipHostRegisterIoMemory for hipHostRegister (#1266…" (#1397)
    This reverts commit 089015967613b933668e92108f210e805f83510f.

--- catch/unit/memory/hipHostRegister.cc
+++ catch/unit/memory/hipHostRegister.cc
@@ -122,12 +122,10 @@ TEMPLATE_TEST_CASE("Unit_hipHostRegister_ReferenceFromKernelandhipMemset", "", i
     SECTION("hipExtHostRegisterUncached") {
       HIP_CHECK(hipHostRegister(A, sizeBytes, hipExtHostRegisterUncached));
     }
-    SECTION("hipHostRegisterPortable | hipHostRegisterMapped | "
-            "hipExtHostRegisterUncached | hipHostRegisterIoMemory") {
+    SECTION("hipHostRegisterPortable | hipHostRegisterMapped | hipExtHostRegisterUncached") {
       HIP_CHECK(hipHostRegister(
           A, sizeBytes,
-          hipHostRegisterPortable | hipHostRegisterMapped | hipExtHostRegisterUncached | 
-          hipHostRegisterIoMemory));
+          hipHostRegisterPortable | hipHostRegisterMapped | hipExtHostRegisterUncached));
     }
   }
 #endif
@@ -927,7 +925,6 @@ TEMPLATE_TEST_CASE("Unit_hipHostRegister_Flags", "", int, float, double) {
       FlagType{0x08, true}, FlagType{hipHostRegisterPortable | hipHostRegisterMapped, true},
       FlagType{hipHostRegisterPortable | hipHostRegisterMapped | 0x08, true},
 #if (HT_AMD == 1) && (HT_LINUX == 1)
-      FlagType{hipHostRegisterIoMemory, true},
       FlagType{hipExtHostRegisterUncached, true},
       FlagType{hipHostRegisterPortable | hipHostRegisterMapped | hipExtHostRegisterUncached, true},
 #endif
